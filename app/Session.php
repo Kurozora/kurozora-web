b10cfa1ecc10ad4ccabc16f984d2b6776f42b85b
unknown <>
2018-08-14 17:02:18 +0200

    Fixed timezone and added expiration date

--- app/Session.php
+++ app/Session.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Session extends Model
 {
-    protected $fillable = ['user_id', 'device', 'secret'];
+    protected $fillable = ['user_id', 'device', 'secret', 'expiration_date'];
 }
