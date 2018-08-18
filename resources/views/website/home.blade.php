aa495022f97e33374a19f46aa7af6cc42ba4b724
musa11971 <mussesemou99@gmail.com>
2018-08-18 14:29:09 +0200

    Added site icon

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" type="text/css" href="{{ asset('img/static/logo_sm.png') }}">
 
 		<title>Kurozora App</title>
 
