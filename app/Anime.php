e6ed4bf6bdd0493f548af25eb2dda82c60df8013
unknown <>
2018-08-20 14:08:21 +0200

    Added TVDB support, added PHP doc, optimized anime explore

--- app/Anime.php
+++ app/Anime.php
@@ -3,7 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use TVDB;
 
+/**
+ * Class Anime
+ *
+ * @package App
+ */
 class Anime extends Model
 {
     protected $fillable = [
@@ -23,4 +29,123 @@ class Anime extends Model
     const ANIME_TYPE_SPECIAL    = 3;
     const ANIME_TYPE_OVA        = 4;
     const ANIME_TYPE_ONA        = 5;
+
+    /**
+     * Retrieves the poster image URL for an Anime item
+     *
+     * @param bool $thumbnail
+     * @return string
+     */
+    public function getPoster($thumbnail = false) {
+        // Try to retrieve the poster from the cache
+        if(!$thumbnail && $this->cached_poster != null)
+            return $this->cached_poster;
+        else if($thumbnail && $this->cached_poster_thumbnail != null)
+            return $this->cached_poster_thumbnail;
+
+        // Check if there is a TVDB ID set
+        if($this->tvdb_id == null)
+            return '';
+
+        // Get the poster from TVDB
+        $tvdb = new TVDB();
+        $retrievedPoster = $tvdb->getAnimePoster($this->tvdb_id, $thumbnail);
+
+        // Cache the poster
+        if($thumbnail)
+            $this->cached_poster_thumbnail = $retrievedPoster;
+        else
+            $this->cached_poster = $retrievedPoster;
+
+        $this->save();
+
+        // Return the poster
+        return $retrievedPoster;
+    }
+
+    /**
+     * Retrieves the background image URL for an Anime item
+     *
+     * @param bool $thumbnail
+     * @return string
+     */
+    public function getBackground($thumbnail = false) {
+        // Try to retrieve the background from the cache
+        if(!$thumbnail && $this->cached_background != null)
+            return $this->cached_background;
+        else if($thumbnail && $this->cached_background_thumbnail != null)
+            return $this->cached_background_thumbnail;
+
+        // Check if there is a TVDB ID set
+        if($this->tvdb_id == null)
+            return '';
+
+        // Get the background from TVDB
+        $tvdb = new TVDB();
+        $retrievedBg = $tvdb->getAnimeBackground($this->tvdb_id, $thumbnail);
+
+        // Cache the background
+        if($thumbnail)
+            $this->cached_background_thumbnail = $retrievedBg;
+        else
+            $this->cached_background = $retrievedBg;
+
+        $this->save();
+
+        // Return the background
+        return $retrievedBg;
+    }
+
+    /**
+     * Formats an array of Anime items as a category
+     *
+     * @param string $categoryTitle
+     * @param string $type
+     * @param Anime[] $animeArray
+     * @return array
+     */
+    public static function formatAnimesAsCategory($categoryTitle, $type, $animeArray) {
+        $retArr = [
+            'title' => $categoryTitle,
+            'type'  => $type,
+            'shows' => []
+        ];
+
+        // Add all Anime items to the shows array
+        foreach($animeArray as $anime)
+            $retArr['shows'][] = self::formatAnimeAsThumbnail($anime);
+
+        // Return the category
+        return $retArr;
+    }
+
+    /**
+     * Formats a list of Anime items into an array of thumbnail data
+     *
+     * @param Anime[] $animeArray
+     * @return array
+     */
+    public static function formatAnimesAsThumbnail($animeArray) {
+        $retArr = [];
+
+        foreach($animeArray as $anime)
+            $retArr[] = self::formatAnimeAsThumbnail($anime);
+
+        return $retArr;
+    }
+
+    /**
+     * Returns minimal data of an Anime item to display it as a thumbnail
+     *
+     * @param Anime $anime
+     * @return array
+     */
+    public static function formatAnimeAsThumbnail($anime) {
+        return [
+            'id'                => $anime->id,
+            'title'             => $anime->title,
+            'poster_url'        => $anime->getPoster(true),
+            'background_url'    => $anime->getBackground(true)
+        ];
+    }
 }
--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -4,54 +4,46 @@ namespace App\Http\Controllers;
 
 use App\Anime;
 use App\Helpers\JSONResult;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class AnimeController extends Controller
 {
     /**
-        /api/v1/anime/explore
-
-        expects:
-        - n/a
-    **/
-    public function exploreAnime(Request $request) {
+     * Returns the necessary data for the Anime explore page
+     *
+     * URL: /api/v1/anime/explore
+     */
+    public function exploreAnime() {
         // Settings for queries
-        $columnsToSelect = ['id', 'title', 'cached_poster_thumbnail as poster_url', 'cached_background_thumbnail as background_url'];
         $maxAnimePerCategory    = 10;
         $maxAnimeForBanners     = 5;
 
+        // Top anime of all time
+        $query_TAOAT = Anime::where('nsfw', false)
+            ->orderBy('title', 'asc')
+            ->limit($maxAnimePerCategory)
+            ->get();
+
+        // Top new movies
+        $query_TNM = Anime::where('nsfw', false)
+            ->orderBy('title', 'asc')
+            ->limit($maxAnimePerCategory)
+            ->get();
+
+
         // Add all the categories together
         $categoryArray = [
-            [
-                'title' => 'Top Anime of All Time',
-                'type'  => 'normal',
-                'shows' => Anime::select($columnsToSelect)
-                                    ->where('nsfw', false)
-                                    ->orderBy('title', 'asc')
-                                    ->limit($maxAnimePerCategory)
-                                    ->get()
-            ],
-            [
-                'title' => 'Top New Movies',
-                'type'  => 'normal',
-                'shows' => Anime::select($columnsToSelect)
-                                    ->where('nsfw', false)
-                                    ->orderBy('title', 'asc')
-                                    ->limit($maxAnimePerCategory)
-                                    ->get()
-            ],
+            Anime::formatAnimesAsCategory('Top Anime of All time', 'normal', $query_TAOAT),
+            Anime::formatAnimesAsCategory('Top New Movies', 'normal', $query_TNM)
         ];
 
         // Retrieve banner section
-        $bannerArray = Anime::select($columnsToSelect)
-                        ->where('nsfw', false)
+        $query_banners = Anime::where('nsfw', false)
                         ->limit($maxAnimeForBanners)
                         ->get();
 
         (new JSONResult())->setData([
             'categories'    => $categoryArray,
-            'banners'       => $bannerArray
+            'banners'       => Anime::formatAnimesAsThumbnail($query_banners)
         ])->show();
     }
 }
