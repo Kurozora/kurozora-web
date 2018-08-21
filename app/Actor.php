a0c061e600f9cfe02421f8346c77fd28dd684e25
unknown <>
2018-08-21 13:28:01 +0200

    Actor response

--- app/Actor.php
+++ app/Actor.php
@@ -14,4 +14,17 @@ class Actor extends Model
         'role',
         'image'
     ];
+
+    /**
+     * Formats the actor with the minimal data needed for a response
+     *
+     * @return array
+     */
+    public static function formatForResponse($actor) {
+        return [
+            'name'  => $actor->name,
+            'role'  => $actor->role,
+            'image' => $actor->image
+        ];
+    }
 }
