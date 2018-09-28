d2900279c2c4da5789350ea16e3c7b5bb257c333
musa11971 <mussesemou99@gmail.com>
2018-09-28 16:58:20 +0200

    Prevent indexing pages

--- resources/views/website/password_reset_page.blade.php
+++ resources/views/website/password_reset_page.blade.php
@@ -1,13 +1,20 @@
-@if ($success)
-    <h1>Your password has been reset.</h1>
-    <p>We have sent you an email with your new password.</p>
-@else
-    Oops! We were unable to reset your password. <br><br>
+<html>
+    <head>
+        <meta name="robots" content="noindex,nofollow" />
+    </head>
+    <body>
+        @if ($success)
+            <h1>Your password has been reset.</h1>
+            <p>We have sent you an email with your new password.</p>
+        @else
+            Oops! We were unable to reset your password. <br><br>
 
-    This could mean:
-    <ul>
-        <li>... this link has already been used.</li>
-        <li>... this link never existed in the first place.</li>
-        <li>... something really weird is going on and you should <a href="https://twitter.com/{{ env('APP_TWITTER_HANDLE') }}" target="_blank">Tweet us</a>.</li>
-    </ul>
-@endif
+            This could mean:
+            <ul>
+                <li>... this link has already been used.</li>
+                <li>... this link never existed in the first place.</li>
+                <li>... something really weird is going on and you should <a href="https://twitter.com/{{ env('APP_TWITTER_HANDLE') }}" target="_blank">Tweet us</a>.</li>
+            </ul>
+        @endif
+    </body>
+</html>
