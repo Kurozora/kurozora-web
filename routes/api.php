c96804236ae765a0a17f15310bcd9a73cfd7bbfd
musa11971 <mussesemou99@gmail.com>
2018-09-29 14:12:41 +0200

    Actor response changes

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,8 @@ Route::prefix('/v1')->group(function() {
         Route::post('/reset_password', 'UserController@resetPassword');
         Route::post('/get_sessions', 'UserController@getSessions');
         Route::post('/delete_session', 'UserController@deleteSession');
+        Route::post('/update_account', 'UserController@updateAccount');
+
         Route::post('/{id}/profile', 'UserController@profile')->where('id', '[0-9]*');
     });
 
@@ -32,7 +34,7 @@ Route::prefix('/v1')->group(function() {
         Route::get('/explore', 'AnimeController@exploreAnime');
 
         Route::post('/{id}/details', 'AnimeController@detailsAnime')->where('id', '[0-9]*');
-        Route::get('/{id}/actors', 'AnimeController@actorsAnime')->where('id', '[0-9]*');
+        Route::post('/{id}/actors', 'AnimeController@actorsAnime')->where('id', '[0-9]*');
         Route::get('/{id}/episodes', 'AnimeController@episodesAnime')->where('id', '[0-9]*');
         Route::post('/{id}/rate', 'AnimeController@rateAnime')->where('id', '[0-9]*');
     });
