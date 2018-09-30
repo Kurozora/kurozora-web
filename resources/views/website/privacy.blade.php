375cc8d7c8cd7faebfceccf8b7e1d446eac9a8b9
musa11971 <mussesemou99@gmail.com>
2018-09-30 18:36:36 +0200

    Privacy policy page

---
+++ resources/views/website/privacy.blade.php
@@ -0,0 +1,10 @@
+@extends('website.main_template')
+
+@section('content')
+    <div class="container">
+        <h5 class="white-text">Privacy policy</h5>
+        <h6 class="grey-text" id="privacyLastMod"></h6>
+
+        <p class="grey-text" id="privacyText">Loading...</p>
+    </div>
+@endsection
