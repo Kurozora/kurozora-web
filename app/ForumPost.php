cfe0b319c1b63535b9564da082f6f0c87c66ab77
musa11971 <mussesemou99@gmail.com>
2018-10-02 20:57:21 +0200

    Forum post schema

---
+++ app/ForumPost.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ForumPost extends Model
+{
+    // Table name
+    protected $table = 'forum_post';
+}
---
+++ database/migrations/2018_10_02_203307_create_forum_posts_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateForumPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forum_post', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->timestamp('edited_at')->nullable()->default(null);
+
+            $table->integer('section_id')->unsigned();
+            $table->foreign('section_id')->references('id')->on('forum_section')->onDelete('cascade');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+
+            $table->integer('parent_post')->unsigned()->nullable()->default(null);
+            $table->foreign('parent_post')->references('id')->on('forum_post')->onDelete('cascade');
+
+            $table->string('ip');
+
+            $table->string('title')->nullable()->default('null');
+            $table->text('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forum_post');
+    }
+}
