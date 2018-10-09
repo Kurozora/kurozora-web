2a607644c15514af9f170105936c0d06e4f50ea2
musa11971 <mussesemou99@gmail.com>
2018-10-09 16:45:35 +0200

    Update script changes

--- update.sh
+++ update.sh
@@ -1,2 +1,10 @@
-echo "Updating from repository..."
+# Colours
+ORANGE='\033[0;33m'
+BLACK='\033[0;30m'
+LIME='\033[1;32m'
+NC='\033[0m'
+
+# Git pull
+echo "\n${BLACK}[${ORANGE}Kurozora${BLACK}] ${NC}Updating source from repository..."
 git pull origin master --quiet
+echo "\n${BLACK}[${ORANGE}Kurozora${BLACK}] ${LIME}✔ ${NC}Source updated!"
