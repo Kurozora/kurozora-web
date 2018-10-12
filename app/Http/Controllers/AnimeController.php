41703cfd1a50a69a116745315094a7eb31e0a992
musa11971 <mussesemou99@gmail.com>
2018-10-12 14:09:24 +0200

    Details update

--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -106,6 +106,8 @@ class AnimeController extends Controller
             'id'                    => $anime->id,
             'title'                 => $anime->title,
             'type'                  => $anime->getType(),
+            'imdb_id'               => $anime->imdb_id,
+            'network'               => $anime->network,
             'episodes'              => $anime->episode_count,
             'seasons'               => $anime->season_count,
             'average_rating'        => $anime->average_rating,
