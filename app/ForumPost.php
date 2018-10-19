7fe0dd448400e7cce5f4cc2da0c3022d1db474c7
musa11971 <mussesemou99@gmail.com>
2018-10-19 10:38:43 +0200

    Started post endpoint

--- app/ForumPost.php
+++ app/ForumPost.php
@@ -8,4 +8,19 @@ class ForumPost extends Model
 {
     // Table name
     protected $table = 'forum_post';
+
+    /**
+     * Formats the post for a response
+     *
+     * @return array
+     */
+    public function formatForResponse() {
+        return [
+            'id'            => $this->id,
+            'user_id'       => $this->user_id,
+            'parent_post'   => $this->parent_post,
+            'title'         => $this->title,
+            'content'       => $this->content,
+        ];
+    }
 }
--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\ForumPost;
 use App\ForumSection;
 use App\Helpers\JSONResult;
 use Illuminate\Http\Request;
+use Validator;
 
 class ForumController extends Controller
 {
@@ -20,5 +22,63 @@ class ForumController extends Controller
             $sections[] = $rawSection->formatForResponse();
 
         (new JSONResult())->setData(['sections' => $sections])->show();
+    }
+
+    /**
+     * Returns the posts of a section
+     */
+    public function getPosts(Request $request) {
+        // Validate the inputs
+        $validator = Validator::make($request->all(), [
+            'section_id'    => 'bail|required|numeric|exists:forum_section,id',
+            'order'         => 'bail|required|in:top,recent',
+            'page'          => 'bail|numeric|min:0'
+        ]);
+
+        // Fetch the variables
+        $givenSection = $request->input('section_id');
+        $givenOrder = $request->input('order');
+        $givenPage = $request->input('page');
+
+        // Check validator
+        if($validator->fails())
+            (new JSONResult())->setError($validator->errors()->first())->show();
+
+        // Create where clauses
+        $whereClauses = [
+            ['section_id', '=', $givenSection]
+        ];
+
+        // Add where clauses
+        $rawPosts = ForumPost::where($whereClauses);
+
+        // Add order
+        if($givenOrder == 'top')
+            $rawPosts->orderBy('score', 'DESC');
+        else if($givenOrder == 'recent')
+            $rawPosts->orderBy('created_at', 'DESC');
+
+        // Add page/offset
+        $resultsPerPage = 10;
+
+        if($givenPage == null)
+            $givenPage = 0;
+
+        $rawPosts->offset($givenPage * $resultsPerPage);
+        $rawPosts->limit($resultsPerPage);
+
+        // Get the results
+        $rawPosts = $rawPosts->get();
+
+        $posts = [];
+
+        foreach($rawPosts as $rawPost)
+            $posts[] = $rawPost->formatForResponse();
+
+        // Show posts in response
+        (new JSONResult())->setData([
+            'page'  => (int) $givenPage,
+            'posts' => $posts
+        ])->show();
     }
 }
