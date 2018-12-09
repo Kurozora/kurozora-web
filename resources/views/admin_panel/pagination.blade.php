ed4e5632f71286f1234b1a205c4f2b88763324b0
musa11971 <mussesemou99@gmail.com>
2018-12-09 13:26:38 +0100

    Paginate

--- resources/views/admin_panel/pagination.blade.php
+++
@@ -1,9 +0,0 @@
-<ul class="pagination center">
-    <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-    <li class="active"><a href="#!">1</a></li>
-    <li class="waves-effect"><a href="#!">2</a></li>
-    <li class="waves-effect"><a href="#!">3</a></li>
-    <li class="waves-effect"><a href="#!">4</a></li>
-    <li class="waves-effect"><a href="#!">5</a></li>
-    <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-</ul>
