2fa819472527a0674b54d44010564b5fc283b94c
musa11971 <mussesemou99@gmail.com>
2018-12-20 16:58:27 +0100

    Changed location of controllers

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,21 +2,89 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\NewUserSessionEvent;
 use App\Events\UserSessionKilledEvent;
 use App\Helpers\JSONResult;
+use App\LoginAttempt;
 use App\Session;
 use App\User;
+use KuroAuthToken;
 use Validator;
 use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
     /**
+     * Creates a new session
+     *
+     * @param Request $request
+     */
+    public function create(Request $request) {
+        // Validate the inputs
+        $validator = Validator::make($request->all(), [
+            'username'  => 'bail|required|exists:' . User::TABLE_NAME . ',username',
+            'password'  => 'bail|required',
+            'device'    => 'bail|required|max:50'
+        ]);
+
+        // Display an error if validation failed
+        if($validator->fails())
+            (new JSONResult())->setError($validator->errors()->first())->show();
+
+        // Check if the request IP is not banned from logging in
+        if(!LoginAttempt::isIPAllowedToLogin($request->ip()))
+            (new JSONResult())->setError('Oops. You have failed to login too many times. Please grab yourself a snack and try again in a bit.')->show();
+
+        // Fetch the variables and sanitize them
+        $username       = $request->input('username');
+        $rawPassword    = $request->input('password');
+        $device         = $request->input('device');
+
+        // Find the user
+        $foundUser = User::where('username', $username)->first();
+
+        // Compare the passwords
+        if(!User::checkPassHash($rawPassword, $foundUser->password)) {
+            // Register the login attempt
+            LoginAttempt::registerFailedLoginAttempt($request->ip());
+
+            // Show error message
+            (new JSONResult())->setError('The entered password does not match.')->show();
+        }
+
+        // Check if email is confirmed
+        if(!$foundUser->hasConfirmedEmail())
+            (new JSONResult())->setError('You have not confirmed your email address yet. Please check your email inbox or spam folder.')->show();
+
+        // Create a new session
+        $loginIPAddress = $request->ip();
+
+        $newSession = Session::create([
+            'user_id'           => $foundUser->id,
+            'device'            => $device,
+            'secret'            => str_random(128),
+            'expiration_date'   => date('Y-m-d H:i:s', strtotime('90 days')),
+            'ip'                => $loginIPAddress
+        ]);
+
+        // Fire event
+        event(new NewUserSessionEvent($newSession));
+
+        // Show a successful response
+        (new JSONResult())->setData([
+            'kuro_auth_token'   => KuroAuthToken::generate($foundUser->id, $newSession->secret),
+            'session_id'        => $newSession->id,
+            'user_id'           => $foundUser->id,
+            'role'              => $foundUser->role
+        ])->show();
+    }
+
+    /**
      * Checks whether or not a session_secret/user_id combination is valid
      *
      * @param Request $request
      */
-    public function validateSession(Request $request) {
+    public function validate(Request $request) {
         // Find the session
         $foundSession = Session::where([
             ['user_id', '=', $request->user_id],
@@ -43,7 +111,7 @@ class SessionController extends Controller
      * @param Request $request
      * @param $sessionID
      */
-    public function deleteSession(Request $request, $sessionID) {
+    public function delete(Request $request, $sessionID) {
         // Fetch the variables
         $delSessionID = $sessionID;
 
@@ -71,7 +139,7 @@ class SessionController extends Controller
      * @param Request $request
      * @param $sessionID
      */
-    public function getSessionDetails(Request $request, $sessionID) {
+    public function details(Request $request, $sessionID) {
         // Find the session
         $foundSession = Session::where([
             ['id'       , '=', $sessionID],
