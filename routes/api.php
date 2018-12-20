ee8f874cd719f360ba6c886af2d863290e9f07b2
musa11971 <mussesemou99@gmail.com>
2018-12-20 20:09:58 +0100

    Added seaons

--- routes/api.php
+++ routes/api.php
@@ -77,7 +77,9 @@ Route::group([/*'middleware' => ['kurozora.useragent'],*/ 'prefix' => 'v1'], fun
         Route::get('/{animeID}/actors', [AnimeController::class, 'actorsAnime'])
             ->where('animeID', '[0-9]*');
 
-        Route::get('/{id}/seasons', 'AnimeController@seasonsAnime')->where('id', '[0-9]*');
+        Route::get('/{animeID}/seasons', [AnimeController::class, 'seasonsAnime'])
+            ->where('animeID', '[0-9]*');
+
         Route::get('/{id}/episodes', 'AnimeController@episodesAnime')->where('id', '[0-9]*');
         Route::post('/{id}/rate', 'AnimeController@rateAnime')->where('id', '[0-9]*')->middleware('kurozora.userauth');
     });
