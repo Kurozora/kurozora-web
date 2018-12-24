48f7eeabeaf9d96dfad2e8c21e0c729a11196dab
musa11971 <mussesemou99@gmail.com>
2018-12-24 20:41:57 +0100

    Returning watched status

--- app/Http/Controllers/AnimeSeasonController.php
+++ app/Http/Controllers/AnimeSeasonController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\AnimeEpisode;
 use App\AnimeSeason;
 use App\Helpers\JSONResult;
+use App\UserWatchedEpisode;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AnimeSeasonController extends Controller
 {
@@ -31,7 +35,7 @@ class AnimeSeasonController extends Controller
      *
      * @param $seasonID
      */
-    public function episodes($seasonID) {
+    public function episodes(Request $request, $seasonID) {
         // Get the season
         $season = AnimeSeason::find($seasonID);
 
@@ -39,12 +43,49 @@ class AnimeSeasonController extends Controller
         if(!$seasonID)
             (new JSONResult())->setError(JSONResult::ERROR_ANIME_SEASON_NON_EXISTENT)->show();
 
-        $episodes = $season->getEpisodes();
+        // Determine columns to select
+        $columnsToSelect = [
+            AnimeEpisode::TABLE_NAME . '.id AS episode_id',
+            AnimeEpisode::TABLE_NAME . '.number AS number',
+            AnimeEpisode::TABLE_NAME . '.name AS name',
+            AnimeEpisode::TABLE_NAME . '.first_aired AS first_aired',
+            AnimeEpisode::TABLE_NAME . '.overview AS overview',
+            AnimeEpisode::TABLE_NAME . '.verified AS verified',
+            // Select the user's "watched" status via subquery
+            DB::raw('(SELECT COUNT(*) FROM ' . UserWatchedEpisode::TABLE_NAME . ' WHERE episode_id = ' . AnimeEpisode::TABLE_NAME . '.id AND user_id = ' . $request->user_id . ' LIMIT 1) AS watched')
+        ];
+
+        // Get the episodes
+        $episodeInfo = DB::table(AnimeEpisode::TABLE_NAME)
+            ->select($columnsToSelect)
+            ->where([
+                [AnimeEpisode::TABLE_NAME . '.season_id', '=', $seasonID]
+            ])
+            ->orderBy('number', 'ASC');
+
+        $rawEpisodes = $episodeInfo->get();
+
+        // Format the episodes
         $endEpisodes = [];
 
-        foreach($episodes as $episode)
-            $endEpisodes[] = $episode->formatEpisodeData();
+        foreach($rawEpisodes as $rawEpisode) {
+            $firstAiredUnix = (new Carbon($rawEpisode->first_aired))->timestamp;
+            $formattedFirstAired = date('j M, Y', $firstAiredUnix);
 
+            $endEpisodes[] = [
+                'id'            => $rawEpisode->episode_id,
+                'number'        => $rawEpisode->number,
+                'name'          => $rawEpisode->name,
+                'first_aired'   => $formattedFirstAired,
+                'overview'      => $rawEpisode->overview,
+                'verified'      => (bool) $rawEpisode->verified,
+                'user_details'  => [
+                    'watched'   => (bool) $rawEpisode->watched
+                ]
+            ];
+        }
+
+        // Show the response
         (new JSONResult())->setData([
             'season'        => $season->formatForInfoResponse(),
             'episodes'      => $endEpisodes
