b5d36f8be77f60a55f73637d26f5c8af811ed69e
musa11971 <mussesemou99@gmail.com>
2019-01-03 21:26:44 +0100

    Modified confirm email to mailable

---
+++ app/Mail/ConfirmEmail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ConfirmEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $user;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->subject('Please confirm your email address')
+            ->view('email.confirmation_email')
+            ->with([
+                'title' => 'Your Kurozora account registration',
+                'username'          => $this->user->username,
+                'confirmation_url'  => url('/confirmation/' . $this->user->email_confirmation_id)
+            ]);
+    }
+}
--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => 'no-reply@kurozora.app',
+        'name' => 'Kurozora Team',
     ],
 
     /*
