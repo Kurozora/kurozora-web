7ef1bf13a0af3ef9056cbbe2043e796d9ff20b51
musa11971 <mussesemou99@gmail.com>
2019-01-03 22:02:14 +0100

    Password reset email to Mailable

---
+++ app/Mail/ResetPassword.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use App\PasswordReset;
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ResetPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $user;
+    protected $passwordReset;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param User $user
+     * @param PasswordReset $passwordReset
+     */
+    public function __construct(User $user, PasswordReset $passwordReset)
+    {
+        $this->user = $user;
+        $this->passwordReset = $passwordReset;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->subject('Request to reset your Kurozora password')
+            ->view('email.password_reset_notification')
+            ->with([
+                'title'     => 'Password reset',
+                'username'  => $this->user->username,
+                'ip'        => $this->passwordReset->ip,
+                'reset_url' => url('/reset/' . $this->passwordReset->token)
+            ]);
+    }
+}
