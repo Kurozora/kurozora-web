e0f88660c645322fede723101d0265eb476bbb89
musa11971 <mussesemou99@gmail.com>
2019-01-10 15:30:27 +0100

    Up down changes

--- down.sh
+++ down.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# Colours
+ORANGE='\033[0;33m'
+BLACK='\033[0;30m'
+LIME='\033[1;32m'
+NC='\033[0m'
+
 # Kills Laravel queue background process
 pkill -f KurozoraQueue
+echo -e "\n${BLACK}[${ORANGE}Kurozora${BLACK}] ${LIME}✔ ${NC}Laravel queue worker stopped!"
--- up.sh
+++ up.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# Colours
+ORANGE='\033[0;33m'
+BLACK='\033[0;30m'
+LIME='\033[1;32m'
+NC='\033[0m'
+
 # Starts Laravel queue background process
-(trap "" SIGINT; exec -a KurozoraQueue php artisan queue:work &)
+(trap "" SIGINT; exec -a KurozoraQueue nohup php artisan queue:work &>/dev/null &)
+echo -e "\n${BLACK}[${ORANGE}Kurozora${BLACK}] ${LIME}✔ ${NC}Laravel queue worker started!"
