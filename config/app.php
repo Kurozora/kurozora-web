5794b6398fd898c4a0efa002fd9c87f6ad501dcd
musa11971 <mussesemou99@gmail.com>
2019-01-12 12:04:00 +0100

    Removed useless comment

--- config/app.php
+++ config/app.php
@@ -181,10 +181,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
-         * Package Service Providers...
-         */
-
-        /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
