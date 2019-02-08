c9f0558fb1fe283765f480bae086d5a157d64ac6
musa11971 <mussesemou99@gmail.com>
2019-02-08 13:51:48 +0100

    Caching keys function for each model

--- app/KModel.php
+++ app/KModel.php
@@ -8,4 +8,23 @@ class KModel extends Model
 {
     // Remove column guards
     protected $guarded = [];
+
+    /**
+     * Generates a key to be used for caching
+     *
+     * @param array $options
+     * @return string
+     */
+    static function cacheKey($options = []) {
+        // Start with the table name
+        $key = get_called_class()::TABLE_NAME;
+
+        // Add a name
+        if(isset($options['name'])) $key .= '-' . $options['name'];
+
+        // Add an ID
+        if(isset($options['id'])) $key .= '-' . $options['id'];
+
+        return $key;
+    }
 }
