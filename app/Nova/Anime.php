1cee837cc9976a8a0c4014dfd3dd07913c4602b2
musa11971 <mussesemou99@gmail.com>
2019-05-12 16:20:05 +0200

    Anime columns in nova

--- app/Nova/Anime.php
+++ app/Nova/Anime.php
@@ -5,10 +5,14 @@ namespace App\Nova;
 use App\Nova\Actions\FetchAnimeImages;
 use Chaseconey\ExternalImage\ExternalImage;
 use Laravel\Nova\Fields\BelongsToMany;
+use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\Heading;
 use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
 
 class Anime extends Resource
 {
@@ -46,9 +50,83 @@ class Anime extends Resource
         return [
             ID::make()->sortable(),
 
-            ExternalImage::make('Poster', 'cached_poster_thumbnail'),
+            Heading::make('Basic information')
+                ->onlyOnForms(),
 
-            Text::make('Title')->sortable(),
+            ExternalImage::make('Thumbnail', 'cached_poster_thumbnail')
+                ->onlyOnIndex(),
+
+            Text::make('Title')
+                ->rules('required')
+                ->sortable(),
+
+            Text::make('Slug')
+                ->rules('required')
+                ->onlyOnForms()
+                ->help('Used to identify the Anime in a URL: https://kurozora.app/anime/<strong>wolf-children</strong>'),
+
+            Textarea::make('Synopsis')
+                ->onlyOnForms()
+                ->help('A short description about the Anime.'),
+
+            Heading::make('Meta information')
+                ->onlyOnForms(),
+
+            Text::make('Network')
+                ->onlyOnForms()
+                ->help('The network that airs the Anime.'),
+
+            Number::make('TVDB ID', 'tvdb_id')
+                ->help('The ID of the Anime as noted on The TVDB.'),
+
+            Text::make('IMDB ID', 'imdb_id')
+                ->onlyOnForms()
+                ->help('The ID of the Anime as noted on IMDB.'),
+
+            Number::make('Runtime in minutes', 'runtime')
+                ->onlyOnForms()
+                ->help('For series: The average runtime in minutes of a single episode.<br />For movies: The amount of minutes the movie takes.'),
+
+            Boolean::make('NSFW')
+                ->help('NSFW: Not Safe For Work (not suitable for watchers under the age of 18).'),
+
+            Text::make('Watch rating', 'watch_rating')
+                ->onlyOnForms()
+                ->help('for example: TV-PG.'),
+
+            Heading::make('Images')
+                ->onlyOnForms(),
+
+            ExternalImage::make('Poster image URL', 'cached_poster')
+                ->onlyOnForms(),
+
+            ExternalImage::make('Poster Thumbnail image URL', 'cached_poster_thumbnail')
+                ->onlyOnForms(),
+
+            ExternalImage::make('Banner image URL', 'cached_background')
+                ->onlyOnForms(),
+
+            ExternalImage::make('Banner Thumbnail image URL', 'cached_background_thumbnail')
+                ->onlyOnForms(),
+
+            Heading::make('Flags')
+                ->onlyOnForms(),
+
+            Boolean::make('Actors Fetched?', 'fetched_actors')
+                ->onlyOnForms()
+                ->help('Whether or not the actors were retrieved from TVDB.<br />Untick and the system will do so once the Anime gets visited the next time.'),
+
+            Boolean::make('Base Episodes Fetched?', 'fetched_base_episodes')
+                ->onlyOnForms()
+                ->help('Whether or not the base episodes were retrieved from TVDB.<br />Untick and the system will do so once the Anime gets visited the next time.'),
+
+            Boolean::make('Images Fetched?', 'fetched_images')
+                ->onlyOnForms()
+                ->help('Whether or not the images were retrieved from TVDB.<br />Untick and the system will do so once the Anime gets visited the next time.'),
+
+            Boolean::make('Details Fetched?', 'fetched_details')
+                ->onlyOnForms()
+                ->help('Whether or not the details (information_ were retrieved from TVDB.<br />Untick and the system will do so once the Anime gets visited the next time.'),
 
             BelongsToMany::make('Genres')
                 ->searchable(),
