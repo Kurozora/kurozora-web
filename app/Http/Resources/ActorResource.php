4e16724aaf2ac52f5efb95bc36ffd1c99de61d88
musa11971 <mussesemou99@gmail.com>
2019-06-27 13:30:04 +0200

    Added ActorResource

---
+++ app/Http/Resources/ActorResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ActorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'    => $this->id,
+            'name'  => $this->name,
+            'role'  => $this->role,
+            'image' => $this->image
+        ];
+    }
+}
