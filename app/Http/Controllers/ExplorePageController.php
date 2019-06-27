33b3af69985a1069aa67f383fe0a93c4a1b668f8
musa11971 <mussesemou99@gmail.com>
2019-06-27 14:19:11 +0200

    Converted ExplorePageController to new JSONResult syntax

--- app/Http/Controllers/ExplorePageController.php
+++ app/Http/Controllers/ExplorePageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\ExplorePageCategory;
 use App\Helpers\JSONResult;
 use App\Http\Resources\ExplorePageCategoryResource;
+use Illuminate\Http\JsonResponse;
 
 class ExplorePageController extends Controller
 {
@@ -13,14 +14,16 @@ class ExplorePageController extends Controller
 
     /**
      * Returns the necessary data for the Anime explore page
+     *
+     * @return JsonResponse
      */
     function explore() {
         // Retrieve the categories
         $categories = ExplorePageCategory::all();
 
         // Return the response
-        (new JSONResult())->setData([
+        return JSONResult::success([
             'categories' => ExplorePageCategoryResource::collection(($categories))
-        ])->show();
+        ]);
     }
 }
