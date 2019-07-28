0c9756e133e9cef2abb6d11f086364845b39504c
musa11971 <mussesemou99@gmail.com>
2019-07-28 17:06:07 +0200

    Added new actions to anime (nova) and added actor support in Nova

--- app/Actor.php
+++ app/Actor.php
@@ -12,4 +12,13 @@ class Actor extends KModel
     // Table name
     const TABLE_NAME = 'actors';
     protected $table = self::TABLE_NAME;
+
+    /**
+     * Returns the Anime the actor belongs to.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    function anime() {
+        return $this->belongsTo(Anime::class);
+    }
 }
