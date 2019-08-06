99f7dd4756d79055a04ce795c8ad619e1f45b8fa
musa11971 <mussesemou99@gmail.com>
2019-08-07 00:18:57 +0200

    Added icons to nova

--- app/Nova/UserNotification.php
+++ app/Nova/UserNotification.php
@@ -36,6 +36,13 @@ class UserNotification extends Resource
     ];
 
     /**
+     * The logical group associated with the resource.
+     *
+     * @var string
+     */
+    public static $group = 'Users';
+
+    /**
      * Get the fields displayed by the resource.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -130,4 +137,15 @@ class UserNotification extends Resource
     {
         return [];
     }
+
+    /**
+     * The icon of the resource.
+     *
+     * @var string
+     */
+    public static $icon = '
+        <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path fill="var(--sidebar-icon)" d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 10v6h14v-6h-2.38l-1.45 2.9a2 2 0 0 1-1.79 1.1h-2.76a2 2 0 0 1-1.8-1.1L7.39 13H5zm14-2V5H5v6h2.38a2 2 0 0 1 1.8 1.1l1.44 2.9h2.76l1.45-2.9a2 2 0 0 1 1.79-1.1H19z"/>
+        </svg>
+    ';
 }
