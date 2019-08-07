c072c9e2fb075f432ebd766604b0fbe416efe2a5
musa11971 <mussesemou99@gmail.com>
2019-08-07 11:22:32 +0200

    update gitignore

---
+++ resources/views/vendor/nova/resources/navigation.blade.php
@@ -0,0 +1,56 @@
+@if (count(Nova::availableResources(request())))
+<ul class="sidemenu">
+    @foreach($navigation as $group => $resources)
+      @if (count($groups) > 1)
+      <li class="mb-2">
+        <span class="ml-8 mb-4 text-xs text-white-50% uppercase tracking-wide">{{ $group }}</span>
+
+        <ul class="list-reset">
+          @foreach($resources as $resource)
+          <li>
+            <router-link :to="{
+                name: 'index',
+                params: {
+                    resourceName: '{{ $resource::uriKey() }}'
+                }
+            }" class="flex items-center font-normal text-white mb-4 text-base no-underline dim">
+                @if(property_exists($resource, 'icon'))
+                    {!! $resource::$icon !!}
+                @else
+                <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path fill="var(--sidebar-icon)" d="M3 1h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3h-4zM3 11h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4h-4z"
+                    />
+                </svg>
+                @endif
+                <span class="sidebar-label">{{ $resource::label() }}</span>
+            </router-link>
+          </li>
+          @endforeach
+        </ul>
+      </li>
+
+      @else
+        @foreach($resources as $resource)
+        <li class="sidebar-dropdown">
+            <router-link :to="{
+                name: 'index',
+                params: {
+                    resourceName: '{{ $resource::uriKey() }}'
+                }
+            }" class="flex items-center font-normal text-white mb-6 text-base no-underline dim">
+                @if(property_exists($resource, 'icon'))
+                    {!! $resource::$icon !!}
+                @else
+                <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                    <path fill="var(--sidebar-icon)" d="M3 1h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2V3c0-1.1045695.8954305-2 2-2zm0 2v4h4V3h-4zM3 11h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2H3c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4H3zm10-2h4c1.1045695 0 2 .8954305 2 2v4c0 1.1045695-.8954305 2-2 2h-4c-1.1045695 0-2-.8954305-2-2v-4c0-1.1045695.8954305-2 2-2zm0 2v4h4v-4h-4z"
+                    />
+                </svg>
+                @endif
+                <span class="sidebar-label">{{ $resource::label() }}</span>
+            </router-link>
+        </li>
+        @endforeach
+      @endif
+    @endforeach
+</ul>
+@endif
