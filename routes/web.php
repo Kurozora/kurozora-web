bc164c0efa73a3b33a5182804ca983fea03e92b7
musa11971 <mussesemou99@gmail.com>
2019-09-01 20:22:03 +0200

    Added privacy policy page

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,17 @@
 
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WebControllers\PrivacyPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('website.home');
 })->name('home');
 
+// Privacy page
+Route::get('/privacy', [PrivacyPageController::class, 'show']);
+
+// Landing pages
 Route::get('/anime/{animeID}', [PageController::class, 'anime']);
 Route::get('/profile/{userID}', [PageController::class, 'userProfile']);
 Route::get('/thread/{threadID}', [PageController::class, 'thread']);
