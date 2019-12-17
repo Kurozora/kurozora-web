d0e3d8d45e3db8152890cdc83339930a2630e805
kirito <casillaskhoren1@yahoo.com>
2019-12-17 13:17:41 +0100

    - Get episodes is now optional

--- app/Http/Controllers/AnimeSeasonController.php
+++ app/Http/Controllers/AnimeSeasonController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\AnimeEpisode;
 use App\AnimeSeason;
 use App\Helpers\JSONResult;
+use App\Http\Resources\AnimeEpisodeResource;
 use App\Http\Resources\AnimeSeasonResource;
 use App\UserWatchedEpisode;
 use Carbon\Carbon;
@@ -35,52 +36,11 @@ class AnimeSeasonController extends Controller
      * @throws \Exception
      */
     public function episodes(AnimeSeason $season) {
-        // Determine columns to select
-        $columnsToSelect = [
-            AnimeEpisode::TABLE_NAME . '.id AS episode_id',
-            AnimeEpisode::TABLE_NAME . '.number AS number',
-            AnimeEpisode::TABLE_NAME . '.name AS name',
-            AnimeEpisode::TABLE_NAME . '.first_aired AS first_aired',
-            AnimeEpisode::TABLE_NAME . '.overview AS overview',
-            AnimeEpisode::TABLE_NAME . '.verified AS verified',
-            // Select the user's "watched" status via subquery
-            DB::raw('(SELECT COUNT(*) FROM ' . UserWatchedEpisode::TABLE_NAME . ' WHERE episode_id = ' . AnimeEpisode::TABLE_NAME . '.id AND user_id = ' . Auth::id() . ' LIMIT 1) AS watched')
-        ];
-
         // Get the episodes
-        $episodeInfo = DB::table(AnimeEpisode::TABLE_NAME)
-            ->select($columnsToSelect)
-            ->where([
-                [AnimeEpisode::TABLE_NAME . '.season_id', '=', $season->id]
-            ])
-            ->orderBy('number', 'ASC');
+        $episodes = $season->getEpisodes();
 
-        $rawEpisodes = $episodeInfo->get();
-
-        // Format the episodes
-        $endEpisodes = [];
-
-        foreach($rawEpisodes as $rawEpisode) {
-            $firstAiredUnix = (new Carbon($rawEpisode->first_aired))->timestamp;
-            $formattedFirstAired = date('j M, Y', $firstAiredUnix);
-
-            $endEpisodes[] = [
-                'id'            => $rawEpisode->episode_id,
-                'number'        => $rawEpisode->number,
-                'name'          => $rawEpisode->name,
-                'first_aired'   => $formattedFirstAired,
-                'overview'      => $rawEpisode->overview,
-                'verified'      => (bool) $rawEpisode->verified,
-                'user_details'  => [
-                    'watched'   => (bool) $rawEpisode->watched
-                ]
-            ];
-        }
-
-        // Show the response
         return JSONResult::success([
-            'season'        => AnimeSeasonResource::make($season),
-            'episodes'      => $endEpisodes
+            'episodes'  => AnimeEpisodeResource::collection($episodes)
         ]);
     }
 }
