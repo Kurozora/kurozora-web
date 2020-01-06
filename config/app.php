0c2eea51b4cc928aeb7bee14f85676dca00de38f
kirito <casillaskhoren1@yahoo.com>
2020-01-06 02:21:21 +0100

    - Added new "redownload" option to redownload the file separately from the "seed" option. - Added new MigrateCommand to add signature of the new option - Added new MigrationServiceProvider to register the custom MigrateCommand - Added MigrationServiceProvider to the providers list in app.php - Added new function in AnimesTableDummySeeder to download and return the downloaded json.

--- config/app.php
+++ config/app.php
@@ -187,6 +187,7 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\MigrationServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\UserModelServiceProvider::class,
