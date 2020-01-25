18cd6ebf229072613a74f185187672e2b399d909
musa11971 <mussesemou99@gmail.com>
2020-01-25 22:07:56 +0100

    Added some more FollowTests

--- tests/API/FollowTest.php
+++ tests/API/FollowTest.php
@@ -33,4 +33,95 @@ class FollowTest extends TestCase
         // Check that the other user is now being followed by one person
         $this->assertEquals($anotherUser->followers()->count(), 1);
     }
+
+    /**
+     * Test if a user can unfollow another user.
+     *
+     * @return void
+     * @test
+     */
+    function a_user_can_unfollow_another_user()
+    {
+        /** @var User $anotherUser */
+        $anotherUser = factory(User::class)->create();
+
+        // Add the other user to our following list
+        $this->user->following()->attach($anotherUser);
+
+        // Send the unfollow API request
+        $this->auth()->json('POST', '/api/v1/users/' . $anotherUser->id . '/follow', [
+            'follow' => 0
+        ])->assertSuccessfulAPIResponse();
+
+        // Check that the user is now following no-one
+        $this->assertEquals($this->user->following()->count(), 0);
+
+        // Check that the other user is now being followed by no-one
+        $this->assertEquals($anotherUser->followers()->count(), 0);
+    }
+
+    /**
+     * Test if a user cannot follow a non-existing user.
+     *
+     * @return void
+     * @test
+     */
+    function a_user_cannot_follow_an_invalid_user()
+    {
+        $this->auth()->json('POST', '/api/v1/users/' . 99999 . '/follow', [
+            'follow' => 1
+        ])->assertNotFound();
+
+        // Check that the user is still following no-one
+        $this->assertEquals($this->user->following()->count(), 0);
+    }
+
+    /**
+     * Test if a user cannot follow a user they already follow.
+     *
+     * @return void
+     * @test
+     */
+    function a_user_cannot_follow_a_user_they_already_follow()
+    {
+        /** @var User $anotherUser */
+        $anotherUser = factory(User::class)->create();
+
+        // Add the other user to our following list
+        $this->user->following()->attach($anotherUser);
+
+        // Send the follow API request
+        $this->auth()->json('POST', '/api/v1/users/' . $anotherUser->id . '/follow', [
+            'follow' => 1
+        ])->assertUnsuccessfulAPIResponse();
+
+        // Check that the user is now following one person
+        $this->assertEquals($this->user->following()->count(), 1);
+
+        // Check that the other user is now being followed by one person
+        $this->assertEquals($anotherUser->followers()->count(), 1);
+    }
+
+    /**
+     * Test if a user cannot unfollow a user they do not follow.
+     *
+     * @return void
+     * @test
+     */
+    function a_user_cannot_unfollow_a_user_they_do_not_follow()
+    {
+        /** @var User $anotherUser */
+        $anotherUser = factory(User::class)->create();
+
+        // Send the unfollow API request
+        $this->auth()->json('POST', '/api/v1/users/' . $anotherUser->id . '/follow', [
+            'follow' => 0
+        ])->assertUnsuccessfulAPIResponse();
+
+        // Check that the user is now following one person
+        $this->assertEquals($this->user->following()->count(), 0);
+
+        // Check that the other user is now being followed by one person
+        $this->assertEquals($anotherUser->followers()->count(), 0);
+    }
 }
