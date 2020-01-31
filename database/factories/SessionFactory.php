dbfb43799ae3ff46c47a8bf079e0065bd31617d2
musa11971 <mussesemou99@gmail.com>
2020-01-31 15:33:57 +0100

    Added Session Factory

---
+++ database/factories/SessionFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Session;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+$factory->define(Session::class, function (Faker $faker) {
+    return [
+        'user_id'           => factory(\App\User::class)->create()->id,
+        'expiration_date'   => now()->addDays(90),
+        'last_validated'    => now(),
+        'ip'                => $faker->ipv4,
+        'device'            => 'Faker Factory',
+        'secret'            => Str::random(128),
+        'city'              => $faker->city,
+        'region'            => $faker->state,
+        'country'           => $faker->country,
+        'latitude'          => $faker->latitude,
+        'longitude'         => $faker->longitude
+    ];
+});
