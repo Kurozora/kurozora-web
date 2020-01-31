1ba3771326745d5465aa0f719e9b876d95f34e9b
musa11971 <mussesemou99@gmail.com>
2020-01-31 12:39:56 +0100

    Removed unused method in UserLibrary model

--- app/UserLibrary.php
+++ app/UserLibrary.php
@@ -7,16 +7,4 @@ class UserLibrary extends KModel
     // Table name
     const TABLE_NAME = 'user_libraries';
     protected $table = self::TABLE_NAME;
-
-    /**
-     * Formats this library item for a response
-     *
-     * @return array
-     */
-    public function formatForResponse() {
-        return [
-            'id'        => $this->id,
-            'anime_id'  => $this->anime_id
-        ];
-    }
 }
