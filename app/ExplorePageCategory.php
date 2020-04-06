df38527736672423641d1086adba770ec81fe9b6
musa11971 <mussesemou99@gmail.com>
2020-04-06 19:51:59 +0200

    Auto generated explore page filtered by genres

--- app/ExplorePageCategory.php
+++ app/ExplorePageCategory.php
@@ -2,9 +2,7 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-
-class ExplorePageCategory extends Model
+class ExplorePageCategory extends KModel
 {
     // Table name
     const TABLE_NAME = 'explore_page_categories';
