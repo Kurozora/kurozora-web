6bb29ef7f74717048f493b29ff9015dc63de4e61
musa11971 <mussesemou99@gmail.com>
2020-04-10 18:32:55 +0200

    Remove all before install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,3 @@
 # Remove all files and folders in the deployment folder
-rm -R /var/www/html/* -f
-rm -R /var/www/html/.* -f
+rm -R /var/www/html/* -f > /dev/null 2>&1
+rm -R /var/www/html/.* -f > /dev/null 2>&1
