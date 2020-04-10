fb5bf4ec159947a1346a52a4c2d9e234b80eda74
musa11971 <mussesemou99@gmail.com>
2020-04-10 18:54:52 +0200

    Laravel setup

---
+++ scripts/after_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Enter html directory
+cd /var/www/html/
+
+# Create cache and chmod folders
+mkdir -p /var/www/html/bootstrap/cache
+mkdir -p /var/www/html/storage/framework/sessions
+mkdir -p /var/www/html/storage/framework/views
+mkdir -p /var/www/html/storage/framework/cache
+
+# Install dependencies
+export COMPOSER_ALLOW_SUPERUSER=1
+composer install -d /var/www/html/
+
+# Migrate all tables
+php /var/www/html/artisan migrate
+
+# Clear any previous cached views
+php /var/www/html/artisan config:clear
+php /var/www/html/artisan cache:clear
+php /var/www/html/artisan view:clear
+
+# Optimize the application
+php /var/www/html/artisan config:cache
+php /var/www/html/artisan optimize
+
+# Change rights
+chmod 777 -R /var/www/html/bootstrap/cache
+chmod 777 -R /var/www/html/storage
+chmod 777 -R /var/www/html/public/files/
+
+# Bring up application
+php /var/www/html/artisan up
