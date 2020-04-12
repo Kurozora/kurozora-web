03cab4c5adcdc952a87040a5565202099fc33e93
musa11971 <mussesemou99@gmail.com>
2020-04-12 17:48:57 +0200

    Update

--- .platform/hooks/predeploy/06-set-laravel-permissions.sh
+++ .platform/hooks/predeploy/06-set-laravel-permissions.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Set nginx as owner of the application
-chown -R nginx:nginx .
+#chown -R nginx:nginx .
 
 # All files to 644
-sudo find . -type f -exec chmod 644 {} \;
+#sudo find . -type f -exec chmod 644 {} \;
 
 # All directories to 755
-sudo find . -type d -exec chmod 755 {} \;
+#sudo find . -type d -exec chmod 755 {} \;
+exit 0
