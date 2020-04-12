0418930f23b7c4296aead0ecbec8fcfed663dfe2
musa11971 <mussesemou99@gmail.com>
2020-04-12 15:48:20 +0200

    Hooks

---
+++ .platform/hooks/predeploy/03-symlink-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Symlink .env configuration
+ln -s ~/.env /var/www/html/.env
---
+++ .platform/hooks/predeploy/05-optimize-laravel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clear any previous cached views
+php /var/www/html/artisan config:clear
+php /var/www/html/artisan cache:clear
+php /var/www/html/artisan view:clear
+
+# Optimize the application
+php /var/www/html/artisan config:cache
+php /var/www/html/artisan optimize
