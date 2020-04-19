a54b96d8359d27bae50b691ae4cd243bdab97aba
musa11971 <mussesemou99@gmail.com>
2020-04-20 00:40:02 +0200

    WIP: Using policy for marking episodes as watched

--- app/Http/Requests/MarkEpisodeAsWatchedRequest.php
+++ app/Http/Requests/MarkEpisodeAsWatchedRequest.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Requests;
 
-use App\Rules\ValidateAnimeInLibrary;
+use App\AnimeEpisode;
 use Illuminate\Foundation\Http\FormRequest;
 
 class MarkEpisodeAsWatchedRequest extends FormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $episode = AnimeEpisode::find($this->route('episode'));
+
+        return $this->user()->can('mark_as_watched', $episode);
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
