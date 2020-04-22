9582a73d2dc2028c8eaf0636c58ba0c7280a8fb0
musa11971 <mussesemou99@gmail.com>
2020-04-22 13:33:02 +0200

    Posting to feed tests

--- database/factories/FeedMessageFactory.php
+++ database/factories/FeedMessageFactory.php
@@ -8,7 +8,8 @@ use Faker\Generator as Faker;
 
 $factory->define(FeedMessage::class, function (Faker $faker) {
     return [
-        'user_id'   => factory(User::class)->create()->id,
-        'body'      => $faker->sentence
+        'user_id'                   => factory(User::class)->create()->id,
+        'parent_feed_message_id'    => null,
+        'body'                      => $faker->sentence
     ];
 });
