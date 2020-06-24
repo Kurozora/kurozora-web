b4483afaa070b5fb55fd1217990249633c7c8f80
kirito <casillaskhoren1@yahoo.com>
2020-06-24 18:23:58 +0200

    [Add] Optional authentication - Added optional authentication middleware to the studios APIs

--- routes/API/Studios.php
+++ routes/API/Studios.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/studios')->group(function() {
-    Route::get('/', [StudioController::class, 'overview']);
+    Route::get('/', [StudioController::class, 'overview'])
+        ->middleware('kurozora.userauth:optional');;
 
-    Route::get('/{studio}', [StudioController::class, 'details']);
+    Route::get('/{studio}', [StudioController::class, 'details'])
+        ->middleware('kurozora.userauth:optional');;
 });
