25a4c444da11b33a2f9cd7c3340982019ef22a7b
kirito <casillaskhoren1@yahoo.com>
2020-06-24 18:26:34 +0200

    [Remove] Comment - Removed unnecessary comment

--- app/Http/Requests/GetStudio.php
+++ app/Http/Requests/GetStudio.php
@@ -6,16 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class GetStudio extends FormRequest
 {
-//    /**
-//     * Determine if the user is authorized to make this request.
-//     *
-//     * @return bool
-//     */
-//    public function authorize()
-//    {
-//        return false;
-//    }
-
     /**
      * Get the validation rules that apply to the request.
      *
