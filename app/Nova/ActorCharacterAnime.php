731459c519ede78656c4c8d47ae656669388b62f
kirito <casillaskhoren1@yahoo.com>
2020-06-28 02:20:24 +0200

    [Add] ActorCharacter and Anime - Added ActorCharacter and Anime migration - Added ActorCharacter and Anime relations - Added ActorCharacter Nova resource - Added ActorCharacter Nova fields - Added ActorCharacterAnime pivot - Added ActorCharacterAnime Nova fields - Added ActorCharacterAnimeFactory.php - Added ActorCharacterAnimeSeeder.php

---
+++ app/Nova/ActorCharacterAnime.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\ID;
+
+class ActorCharacterAnime extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\ActorCharacterAnime';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * The logical group associated with the resource.
+     *
+     * @var string
+     */
+    public static $group = 'Anime Pivot';
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            BelongsTo::make('Actor Character')
+                ->searchable()
+                ->sortable(),
+
+            BelongsTo::make('Anime')
+                ->searchable()
+                ->sortable(),
+
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
