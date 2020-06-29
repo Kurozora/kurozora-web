f0a8c5f4cc6883b2ae48a4dbd37b8998dd241bde
kirito <casillaskhoren1@yahoo.com>
2020-06-29 02:00:30 +0200

    [Fix] ActorCharacterAnime factory - Fixed error when seeding ActorCharacterAnime

--- database/factories/ActorCharacterAnimeFactory.php
+++ database/factories/ActorCharacterAnimeFactory.php
@@ -13,7 +13,7 @@ $factory->define(ActorCharacterAnime::class, function (Faker $faker) {
     return [
         'actor_character_id'    => factory(ActorCharacter::class)->create()->id,
         'anime_id'              => factory(Anime::class)->create()->id,
-        'actor_role'            => array_rand(PersonRole::toArray()),
-        'character_role'        => array_rand(CharacterRole::toArray()),
+        'actor_role'            => array_rand(PersonRole::getValues()),
+        'character_role'        => array_rand(CharacterRole::getValues()),
     ];
 });
