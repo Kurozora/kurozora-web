d93d8c1d0818fe41a1123dfc639e5e5e831bf46e
kirito <casillaskhoren1@yahoo.com>
2020-07-15 23:03:56 +0200

    [Improvement] Landing page structure - Added header.blade.php - Added footer.blade.php - Included header and footer in landing.blade.php - Removed script section in favor of global script tag

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -1,22 +1,9 @@
 @extends('website.layouts.landing')
 
 @section('content')
-    <div class="container mx-auto">
-        <div id="middle-div" class="relative h-screen hidden">
-            <div class="absolute inset-0 flex flex-col items-center justify-center">
-                <img src="{{ asset('img/static/logo_sm.png') }}" width="256" alt="Kurozora Logo"/>
-                <br>
-                <h1 class="text-white text-4xl font-bold">Kurozora</h1>
-            </div>
-        </div>
+    <div class="absolute inset-0 flex flex-col items-center justify-center">
+        <img src="{{ asset('img/static/logo_sm.png') }}" width="256" alt="Kurozora Logo"/>
+        <br />
+        <h1 class="text-white text-4xl font-bold">Kurozora</h1>
     </div>
-@endsection
-
-@section('script')
-    <script>
-        $('html').css('background-image', `url({{ asset('img/static/star_bg_lg.jpg') }})`);
-        $(document).ready(function() {
-            $('#middle-div').fadeIn(1000);
-        });
-    </script>
 @endsection
