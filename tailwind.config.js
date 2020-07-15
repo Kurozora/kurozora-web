a80a8e03a6633f3b5a74a6eaf4e7d14667d63cfa
kirito <casillaskhoren1@yahoo.com>
2020-07-15 19:47:03 +0200

    [Fix] Color palette - Fixed Tailwind CSS color palette not working correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,21 @@ module.exports = {
         whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
     },
     theme: {
-        extend: {},
-        colors: {
-            orange: '#FF9300',
-        },
+        extend: {
+            colors: {
+                orange: {
+                    '100': '#FFFAF0',
+                    '200': '#FEEBC8',
+                    '300': '#FBD38D',
+                    '400': '#F6AD55',
+                    '500': '#FF9300',
+                    '600': '#DD6B20',
+                    '700': '#C05621',
+                    '800': '#9C4221',
+                    '900': '#7B341E',
+                }
+            }
+        }
     },
     variants: {},
     plugins: [
