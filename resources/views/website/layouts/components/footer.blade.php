9f69b0a4de7cc5699a26713546f3fb93720cea68
kirito <casillaskhoren1@yahoo.com>
2020-07-15 23:03:56 +0200

    [Improvement] Landing page structure - Added header.blade.php - Added footer.blade.php - Included header and footer in landing.blade.php - Removed script section in favor of global script tag

---
+++ resources/views/website/layouts/components/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="absolute w-full text-gray-300 text-center p-2 inset-x-0 bottom-0">
+    <p>Copyright © 2020 Kurozora B.V. All rights reserved</p>
+</footer>
--- resources/views/website/layouts/landing.blade.php
+++ resources/views/website/layouts/landing.blade.php
@@ -1,39 +1,21 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="text/css" href="{{ asset('img/static/logo_sm.png') }}">
+@include('website.layouts.components.header')
 
-    @if(isset($page['title']))
-        <title>{{ $page['title'] }}</title>
-        <meta property="og:title" content="{{ $page['title'] }}" />
-    @else
-        <title>Kurozora App</title>
-    @endif
-
-    @if(isset($page['image']) && $page['image'] !== null)
-        <meta property="og:image" content="{{ $page['image'] }}" />
-    @endif
-
-    @if(isset($page['type']) && $page['type'] !== null)
-        <meta property="og:type" content="{{ $page['type'] }}" />
-    @endif
-
-    @if(isset($page['no_index']) && $page['no_index'])
-        <meta name="robots" content="noindex,nofollow" />
-    @endif
-
-    {{-- jQuery --}}
-    <script type="text/javascript" src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
-
-    {{-- Landing stylesheet --}}
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
 <body>
-    @yield('content')
+    <div class="container mx-auto px-4">
+        <div id="middle-div" class="relative hidden">
+            @yield('content')
+        </div>
+    </div>
 
-    @yield('script')
+    @include('website.layouts.components.footer')
+
+    <script>
+        $('html').css('background-image', `url({{ asset('img/static/star_bg_lg.jpg') }})`);
+        $(document).ready(function() {
+            $('#middle-div').fadeIn(1000);
+        });
+    </script>
 </body>
 </html>
