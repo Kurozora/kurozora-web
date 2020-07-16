c5ec51284ee2e9f07751fa771ab52c16510fae41
kirito <casillaskhoren1@yahoo.com>
2020-07-16 02:02:13 +0200

    [Improvement] Landing pages and design consistency - Fixed deprecated use of getURLToAvatar in PageController.php - Moved anime landing routes to Web folder - Moved profile landing routes to Web folder - Moved thread landing routes to Web folder - Refactored anime-page.blade.php for consistency - Refactored thread-page.blade.php for consistency - Refactored user-profile-page.blade.php for consistency

--- resources/views/website/thread-page.blade.php
+++ resources/views/website/thread-page.blade.php
@@ -1,10 +1,12 @@
 @extends('website.layouts.landing')
 
 @section('content')
-    <h1 class="kurozora-title">{{ $threadData['title'] }}</h1>
-    <h2 class="kurozora-under-title">thread posted {{ $threadData['date'] }}</h2>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-white">{{ $threadData['title'] }}</h1>
+        <h2 class="text-white">Posted {{ $threadData['date'] }}</h2>
 
-    <a href="{{ ios_app_url('thread/' . $threadData['id']) }}" class="kurozora-btn">
-        Open in Kurozora App
-    </a>
+        <a href="{{ ios_app_url('thread/' . $threadData['id']) }}" class="kurozora-btn">
+            Open in Kurozora App
+        </a>
+    </div>
 @endsection
