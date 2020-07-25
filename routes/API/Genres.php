40c28380647a7800a5da8eb3230c729b93ee5d3f
kirito <casillaskhoren1@yahoo.com>
2020-07-25 20:29:25 +0200

    [Improvement] Genre resource - Added name to Genre routes - Standardised Genre resource - Standardised Genre response

--- routes/API/Genres.php
+++ routes/API/Genres.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/genres')->group(function() {
-    Route::get('/', [GenreController::class, 'overview']);
+Route::prefix('/genres')
+    ->name('genres.')
+    ->group(function() {
+        Route::get('/', [GenreController::class, 'overview'])
+            ->name('overview');
 
-    Route::get('/{genre}', [GenreController::class, 'details']);
-});
+        Route::get('/{genre}', [GenreController::class, 'details'])
+            ->name('details');
+    });
