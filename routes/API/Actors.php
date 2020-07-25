ae6af772c51c0b93e2160ba64038f922c37bbafe
kirito <casillaskhoren1@yahoo.com>
2020-07-25 20:24:08 +0200

    [Improvement] Actors resource - Added name to actors routes - Added `href` key to ActorResource.php - Standardised actors response

--- routes/API/Actors.php
+++ routes/API/Actors.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/actors')->group(function() {
-    Route::get('/', [ActorController::class, 'overview']);
+Route::prefix('/actors')
+    ->name('actors.')
+    ->group(function() {
+        Route::get('/', [ActorController::class, 'overview'])
+            ->name('overview');
 
-    Route::get('/{actor}', [ActorController::class, 'details']);
-});
+        Route::get('/{actor}', [ActorController::class, 'details'])
+            ->name('details');
+    });
