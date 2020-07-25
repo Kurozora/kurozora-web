e929ecd5dc86d153f491e13bbaa5601289487c13
kirito <casillaskhoren1@yahoo.com>
2020-07-25 03:15:03 +0200

    [Improvement] Themes - Changed simple pagination to a full pagination - Refactored theme template.blade.php - Refactored theme-thumbnail.blade.php - Refactored themes view to use Tailwind CSS - Refactored ThemeRoller to use Tailwind CSS

--- resources/views/website/themes/create.blade.php
+++ resources/views/website/themes/create.blade.php
@@ -1,25 +1,23 @@
 @extends('website.themes.template')
 
 @section('content')
-    <div class="section">
-        <div class="container">
-            <div class="has-text-centered">
-                <h1 class="title white-text">Create your own theme</h1>
-                <p class="subtitle white-text">Take your time and make something great, we'll leave you to it.</p>
-            </div>
-
-            <theme-roller submit_url="{{ route('themes.create') }}" />
+    <section class="container mx-auto px-4">
+        <div class="text-center">
+            <h1 class="text-white">Create your own theme</h1>
+            <p class="text-white">Take your time and make something great, we'll leave you to it.</p>
         </div>
-    </div>
 
-    <div class="section has-background-light">
-        <h1 class="title is-text has-text-centered">Finished designing your theme?</h1>
-        <p class="subtitle has-text-centered">Use the button below to submit your theme to the Kurozora team for approval.</p>
+        <theme-roller submit_url="{{ route('themes.create') }}"></theme-roller>
+    </section>
 
-        <div class="has-text-centered">
-            <a class="button is-primary is-large" id="submit-theme">
+    <section class="bg-white text-center">
+        <h1>Finished designing your theme?</h1>
+        <p>Use the button below to submit your theme to the Kurozora team for approval.</p>
+
+        <div class="pb-6">
+            <a class="k-button" id="submit-theme">
                 Submit
             </a>
         </div>
-    </div>
+    </section>
 @endsection
--- resources/views/website/themes/template.blade.php
+++ resources/views/website/themes/template.blade.php
@@ -1,5 +1,4 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ app()->getLocale() }}" style="background-image: url({{ asset('img/static/star_bg_lg.jpg') }}); background-position: center center; background-attachment: fixed; background-size: cover;">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,57 +7,16 @@
 
     <title>{{ isset($title) ? $title . ' | ' : null }}Kurozora Themes</title>
 
-    {{-- Font Awesome --}}
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    {{-- Stylesheets --}}
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
 </head>
-<body style="background-image: url({{ asset('img/static/star_bg_lg.jpg') }}); background-position: center center; background-attachment: fixed; background-size: cover;">
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="themesNavbar">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
+<body>
+    @include('website.resources.navigation.global')
 
-        <div id="themesNavbar" class="navbar-menu">
-            <div class="navbar-start">
-                <a class="navbar-item" href="{{ route('home') }}">
-                    <span class="icon">
-                        <i class="fas fa-chevron-left"></i>
-                    </span>
-                    &nbsp;Back to Kurozora
-                </a>
-            </div>
-
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
-                        <a class="button is-primary"
-                           href="{{ (\Illuminate\Support\Facades\Route::currentRouteName() !== 'themes.create') ? route('themes.create') : '#' }}"
-                            {{ (\Illuminate\Support\Facades\Route::currentRouteName() == 'themes.create') ? 'disabled' : '' }}
-                        >
-                            <strong>Create your own</strong>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
-
-    <div id="app">
+    <main id="app">
         @yield('content')
-    </div>
+    </main>
 
-    {{-- iro color picker --}}
-    <script src="https://cdn.jsdelivr.net/npm/@jaames/iro/dist/iro.min.js"></script>
-
-    {{-- jQuery --}}
-    <script type="text/javascript" src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
-
-    {{-- App Script --}}
-    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    @include('website.layouts.partials.footer')
 </body>
 </html>
