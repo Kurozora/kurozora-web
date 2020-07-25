3a1baf2f60f85921003e981078168de47f231452
kirito <casillaskhoren1@yahoo.com>
2020-07-25 03:04:27 +0200

    [Improvement] Tailwind config - Added grayBlue color scheme - Refactored orange color scheme to look nice - Refactored pagination theme to match the website's style

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,17 +14,35 @@ module.exports = {
         whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
     },
     theme: {
+        pagination: theme => ({
+            color: theme('colors.orange.500'),
+            linkFirst: 'mr-6 border rounded',
+            linkSecond: 'rounded-l border-l',
+            linkBeforeLast: 'rounded-r border-r',
+            linkLast: 'ml-6 border rounded',
+        }),
         extend: {
             colors: {
+                grayBlue: {
+                    '100': '#EBEBEE',
+                    '200': '#CDCED3',
+                    '300': '#AEB0B9',
+                    '400': '#727585',
+                    '500': '#353A50',
+                    '600': '#303448',
+                    '700': '#202330',
+                    '800': '#181A24',
+                    '900': '#101118',
+                },
                 orange: {
-                    '100': '#FFFAF0',
-                    '200': '#FEEBC8',
-                    '300': '#FBD38D',
-                    '400': '#F6AD55',
+                    '100': '#FFF4E6',
+                    '200': '#FFE4BF',
+                    '300': '#FFD499',
+                    '400': '#FFB34D',
                     '500': '#FF9300',
-                    '600': '#DD6B20',
-                    '700': '#C05621',
-                    '800': '#9C4221',
+                    '600': '#dd6b20',
+                    '700': '#c05621',
+                    '800': '#9c4221',
                     '900': '#7B341E',
                 }
             }
