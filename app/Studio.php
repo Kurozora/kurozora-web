c849f3fd70d566c838a97477c4eaa3f2694a569a
kirito <casillaskhoren1@yahoo.com>
2020-07-26 02:04:59 +0200

    [Improvement] Studio resource - Added name to Studio routes - Added anime_studio and getAnime methods to Studio model - Added anime route to Studio routes - Refactored Studio API to use the new cleaner method of limit and include query - Removed GetStudio.php - Renamed StudioResourceSmall to StudioResourceBasic - Standardised Studio resource - Standardised Studio response - Updated openapi.json with new Studio api changes

--- app/Studio.php
+++ app/Studio.php
@@ -2,8 +2,13 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\Cache;
+
 class Studio extends KModel
 {
+    // How long to cache certain responses
+    const CACHE_KEY_ANIME_SECONDS = 120 * 60;
+
     // Table name
     const TABLE_NAME = 'studios';
     protected $table = self::TABLE_NAME;
@@ -22,8 +27,35 @@ class Studio extends KModel
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    public function anime()
+    public function anime_studio()
     {
         return $this->hasMany(AnimeStudio::class);
+    }
+
+    /**
+     * Returns the anime that belongs to the studio
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
+     */
+    public function anime()
+    {
+        return $this->hasManyThrough(Anime::class, AnimeStudio::class, 'studio_id', 'id', 'id', 'anime_id');
+    }
+
+    /**
+     * Retrieves the anime for a Studio item in an array
+     *
+     * @return array
+     */
+    public function getAnime() {
+        // Find location of cached data
+        $cacheKey = self::cacheKey(['name' => 'studios', 'id' => $this->id]);
+
+        // Retrieve or save cached result
+        $animeInfo = Cache::remember($cacheKey, self::CACHE_KEY_ANIME_SECONDS, function () {
+            return $this->anime()->get();
+        });
+
+        return $animeInfo;
     }
 }
