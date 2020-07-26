9f842a9c2a46a730cccac5e5348891ab4a1b5990
kirito <casillaskhoren1@yahoo.com>
2020-07-27 00:39:13 +0200

    [Fix] Media Library migration 7.x -> 8.x - Added media-library config - Updated create_media_table with new changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'media' => [
+            'driver' => 'local',
+            'root'   => storage_path('media'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
