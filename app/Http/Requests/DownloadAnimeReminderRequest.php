145a6c79a5a937382d08f134f52ed8d21962d663
kirito <casillaskhoren1@yahoo.com>
2020-08-04 23:44:07 +0200

    [Improvement] ReminderAnime download - Added DownloadAnimeReminderRequest to check if user is authorized to make the download request - Added policy to prevent users from downloading other user's reminders - Added tests to make sure new changes work

---
+++ app/Http/Requests/DownloadAnimeReminderRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\User;
+use Illuminate\Foundation\Http\FormRequest;
+
+class DownloadAnimeReminderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        // Check if the user can download this user's reminders.
+        /** @var User $user */
+        $user = $this->route('user');
+
+        return $this->user()->can('download_anime_reminder', $user);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+        ];
+    }
+}
