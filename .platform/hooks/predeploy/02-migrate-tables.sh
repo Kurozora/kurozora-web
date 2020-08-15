2af5974de71e1a393e412635609f8fa5a29d0cd9
kirito <casillaskhoren1@yahoo.com>
2020-08-15 04:38:52 +0200

    [Fix] 02-migrate-tables.sh

--- .platform/hooks/predeploy/02-migrate-tables.sh
+++ .platform/hooks/predeploy/02-migrate-tables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /usr/local/bin/composer dump-autoload
-yes | php artisan migrate:rollback
+yes | php artisan migrate:reset --force
 yes | php artisan migrate
 php artisan love:reaction-type-add --default
