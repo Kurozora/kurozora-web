810eb16d5d6f6a784972b55766013fb189cba864
kirito <casillaskhoren1@yahoo.com>
2020-08-16 16:55:26 +0200

    [Improvement] Characters overview - Removed overview api as it's unnecessary and can be misused for scraping

--- routes/API/Characters.php
+++ routes/API/Characters.php
@@ -7,17 +7,14 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/characters')
     ->name('characters.')
     ->group(function() {
-        Route::get('/', [CharacterController::class, 'overview'])
-            ->name('overview');
-
         Route::get('/{character}', [CharacterController::class, 'details'])
             ->middleware('kurozora.userauth:optional')
             ->name('details');
 
+        Route::get('/{character}/actors', [CharacterController::class, 'actors'])
+            ->name('actors');
+
         Route::get('/{character}/anime', [CharacterController::class, 'anime'])
             ->middleware('kurozora.userauth:optional')
             ->name('anime');
-
-        Route::get('/{character}/actors', [CharacterController::class, 'actors'])
-            ->name('actors');
     });
