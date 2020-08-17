aac8e2fb63f8b141bc4d5e0cd06c1d83e0471e72
kirito <casillaskhoren1@yahoo.com>
2020-08-17 18:58:14 +0200

    [Improvement] Sessions - Moved Sessions to Me/Sessions endpoint - Moved users/register-siwa to users/signup/siwa - Moved users/login-siwa to users/signin/siwa - Moved sessions/create to users/signin - Updated openapi.json with new changes - Updated test cases with new changes

--- tests/API/SIWASignUpTest.php
+++ tests/API/SIWASignUpTest.php
@@ -7,19 +7,19 @@ use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
-class SIWAAccountRegistrationTest extends TestCase
+class SIWASignUpTest extends TestCase
 {
     use DatabaseMigrations;
 
     /**
-     * Test if an account can be registered.
+     * An account can be signed up.
      *
      * @return void
      * @test
      */
-    function an_account_can_be_registered_via_siwa()
+    function an_account_can_be_signed_up_via_siwa()
     {
-        $this->json('POST', '/api/v1/users/register-siwa', [
+        $this->json('POST', '/api/v1/users/signup/siwa', [
             'email'             => 'jdi8eji2@privaterelay.appleid.com',
             'siwa_id'           => '001100.6a54b3ccop5041bba0773fd1079b0e22.2447',
             'platform'          => 'iOS',
