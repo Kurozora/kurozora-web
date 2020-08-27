4facf539481764330d0759ec4181388f7f963f54
kirito <casillaskhoren1@yahoo.com>
2020-08-27 21:52:29 +0200

    [Improvement] Feed Message API - Refactored feed message resource

--- app/Http/Resources/FeedMessageResourceBasic.php
+++ app/Http/Resources/FeedMessageResourceBasic.php
@@ -23,29 +23,48 @@ class FeedMessageResourceBasic extends JsonResource
         $totalReactions = $feedMessage->viaLoveReactant()->getReactionTotal();
         $totalHearts = $feedMessage->viaLoveReactant()->getReactionCounterOfType(FeedVoteType::Heart()->description);
 
-        $isReply = $feedMessage->is_reply == 1;
-        $isReShare = $feedMessage->is_reshare == 1;
-
         $resource = [
-            'id'        => $feedMessage->id,
-            'type'      => 'feed-messages',
-            'href'      => route('api.feed.messages.details', $feedMessage, false),
-            'attribute' => [
+            'id'            => $feedMessage->id,
+            'type'          => 'feed-messages',
+            'href'          => route('api.feed.messages.details', $feedMessage, false),
+            'attributes'    => [
                 'body'          => $feedMessage->body,
                 'replyCount'    => $feedMessage->replies->count(),
+                'reShareCount'  => $feedMessage->reShares->count(),
                 'metrics'       => [
                     'count'     => $totalReactions->getCount(),
                     'weight'    => $totalReactions->getWeight(),
                     'hearts'    => $totalHearts->getCount()
                 ],
-                'is_reply'      => $isReply,
-                'is_reshare'    => $isReShare,
+                'isReply'       => $feedMessage->is_reply == 1,
+                'isReShare'     => $feedMessage->is_reshare == 1,
                 'isNSFW'        => $feedMessage->is_nsfw == 1,
                 'isSpoiler'     => $feedMessage->is_spoiler == 1,
                 'createdAt'     => $feedMessage->created_at->format('Y-m-d H:i:s'),
             ]
         ];
 
-        return $resource;
+        // Add relationships
+        $relationships = [];
+        $relationships = array_merge($relationships, $this->getUserDetails());
+
+        return array_merge($resource, ['relationships' => $relationships]);
+    }
+
+    /**
+     * Get the user details belonging to the feed message.
+     *
+     * @return array
+     */
+    private function getUserDetails(): array
+    {
+        /** @var FeedMessage $feedMessage */
+        $feedMessage = $this->resource;
+
+        return [
+            'users' => [
+                'data' => UserResourceBasic::collection([$feedMessage->user]),
+            ]
+        ];
     }
 }
