f608cf7af854b3e3da926c6d11864f2291bfecb2
kirito <casillaskhoren1@yahoo.com>
2021-04-04 23:39:09 +0200

    [Update] Swap Mix - TestCase now swaps out the Mix manifest implementation, so we don't need to run the npm commands to generate a manifest file for the assets in order to run tests that return views

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use App\Helpers\KuroAuthToken;
+use Illuminate\Foundation\Mix;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Testing\Assert as PHPUnit;
 use Illuminate\Testing\TestResponse;
@@ -22,6 +23,13 @@ abstract class TestCase extends BaseTestCase
     function setUp(): void
     {
         parent::setUp();
+
+        // Swap out the Mix manifest implementation, so we don't need
+        // to run the npm commands to generate a manifest file for
+        // the assets in order to run tests that return views.
+        $this->swap(Mix::class, function () {
+            return '';
+        });
 
         // API response macro's
         TestResponse::macro('assertSuccessfulAPIResponse', function() {
