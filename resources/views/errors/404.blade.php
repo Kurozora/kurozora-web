2cb1004676a78af5d79ff4c87f7be837b679c676
kirito <casillaskhoren1@yahoo.com>
2021-04-05 03:32:37 +0200

    [Update] Errors - Updated error views

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,9 +1,7 @@
-@extends('website.layouts.landing')
-
-@section('content')
+<x-base-layout>
     <div class="text-center my-12">
         <h1 class="text-white">The page you’re looking for can’t be found.</h1>
         <br>
         <a href="{{ url('/') }}" class="">Go back to home page?</a>
     </div>
-@endsection
+</x-base-layout>
