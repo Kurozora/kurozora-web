3f2ee26c77d7c706d3b37a40ff8eee04fbf0367d
kirito <casillaskhoren1@yahoo.com>
2021-04-05 03:27:04 +0200

    [Update] Home - Added Home Livewire component - Removed old home.blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('website.home');
-})->name('home');
+Route::get('/', Home::class)
+    ->name('home');
 
 // Landing pages
 require 'Web/Anime.php';
