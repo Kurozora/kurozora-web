fb84f52bac38fc9e573efdfc4136f1d59914057b
kirito <casillaskhoren1@yahoo.com>
2021-04-05 03:20:20 +0200

    [Add] Layouts - Added new base layout - Added new app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<x-base-layout>
+    <!-- Page Heading -->
+    <header class="bg-gray-100 shadow">
+        <div class="flex max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {{ $header }}
+        </div>
+    </header>
+
+    <!-- Page Content -->
+    <main class="mx-auto opacity-0">
+        {{ $slot }}
+    </main>
+
+    <script>
+        (function() {
+            let mainElement = document.querySelector("main");
+            mainElement.style.transition = "opacity 1s";
+            mainElement.style.opacity = "1";
+        })();
+    </script>
+</x-base-layout>
