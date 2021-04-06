b9d0bf6858064ddb4fa24484070280d1b7ca6f04
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-06 15:37:26 +0200

    [Update] User Landing Page - Added Profile/Details Livewire component - Updated profile web routes - Removed old profile files

---
+++ resources/views/livewire/profile/details.blade.php
@@ -0,0 +1,20 @@
+@php
+    $avatar = $user->getFirstMediaUrl('avatar');
+@endphp
+
+<div class="flex flex-col w-full h-full items-center justify-center">
+    <x-slot name="open-graph">
+        <meta property="og:title" content="{{ $page['title'] }}" />
+        <meta property="og:image" content="{{ $page['image'] }}" />
+        <meta property="og:type" content="{{ $page['type'] }}" />
+    </x-slot>
+
+    <div class="user-avatar" style="background-image: url('{{ !empty($avatar) ? $avatar : asset('img/static/placeholders/user_profile.png') }}')"></div>
+
+    <h1 class="text-white font-bold mt-6">{{ $user->username }}</h1>
+    <h2 class="text-white mb-2">{{ $user->getFollowerCount() }} {{ __('followers') }}</h2>
+
+    <x-link-button href="{{ ios_app_url('profile/' . $user->id) }}" class="rounded-full">
+        {{ __('Open in Kurozora App') }}
+    </x-link-button>
+</div>
--- routes/Web/Profile.php
+++ routes/Web/Profile.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Livewire\Profile\Details;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/profile')
     ->name('profile')
     ->group(function() {
-        Route::get('/{userID}', [PageController::class, 'userProfile'])
+        Route::get('/{user}', Details::class)
         ->name('.details');
     });
