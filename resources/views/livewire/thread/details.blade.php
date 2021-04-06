da14ec069783bf79fd5cebaaecd0a93c74a245e0
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-06 15:50:09 +0200

    [Update] Thread Landing Page - Added Thread/Details Livewire component - Updated thread web routes - Removed old thread files

---
+++ resources/views/livewire/thread/details.blade.php
@@ -0,0 +1,8 @@
+<div class="flex flex-col w-full h-full items-center justify-center">
+    <h1 class="text-white font-bold">{{ $thread->title }}</h1>
+    <h2 class="text-white mb-2">{{ __('Posted') }} {{ $thread->created_at->diffForHumans() }}</h2>
+
+    <x-link-button href="{{ ios_app_url('thread/' . $thread->id) }}" class="rounded-full">
+        {{ __('Open in Kurozora App') }}
+    </x-link-button>
+</div>
