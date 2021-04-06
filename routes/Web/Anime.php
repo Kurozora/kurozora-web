d044fce528a0d21c772fc60f2021c1d122b806ac
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-06 15:23:53 +0200

    [Update] Anime Landing Page - Added Anime/Details Livewire component - Updated anime web routes - Removed old anime files - Small bug fixes

--- routes/Web/Anime.php
+++ routes/Web/Anime.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Livewire\Anime\Details;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/anime')
     ->name('anime')
     ->group(function() {
-        Route::get('/{anime}', [PageController::class, 'anime'])
+        Route::get('/{anime}', Details::class)
         ->name('.details');
     });
