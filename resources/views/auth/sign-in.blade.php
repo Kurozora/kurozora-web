df7c76a9248568cba59c92ba5db88a02bade92e4
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-13 01:27:39 +0200

    [Update] Sign In View & Validation - Updated email validation - Updated view to match the app

--- resources/views/auth/sign-in.blade.php
+++ resources/views/auth/sign-in.blade.php
@@ -9,6 +9,11 @@
         </h2>
     </x-slot>
 
+    <div class="text-center mb-5">
+        <p class="text-2xl font-bold">{{ __('Welcome to Kurozora!') }}</p>
+        <p>{{ __('Sign in with your Kurozora ID to use the library and other Kurozora services.') }}</p>
+    </div>
+
     <x-validation-errors class="mb-4" />
 
     @if(session('status'))
@@ -22,12 +27,12 @@
 
         <div>
             <x-label for="email" value="{{ __('Kurozora ID') }}" />
-            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" placeholder="{{ __('The cool Kurozora ID you claimed') }} 🙌" required autofocus />
         </div>
 
         <div class="mt-4">
             <x-label for="password" value="{{ __('Password') }}" />
-            <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            <x-input id="password" class="block mt-1 w-full" type="password" name="password" placeholder="{{ __('Your super secret password') }} 👀" required autocomplete="current-password" />
         </div>
 
         <div class="block mt-4">
@@ -40,12 +45,18 @@
         <div class="flex items-center justify-end mt-4">
             @if(Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                    {{ __('Forgot your password? Let\'s reset it 📧') }}
                 </a>
+
+                <p>{{ __('or') }}</p>
             @endif
 
+            <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('sign-up') }}">
+                {{ __('New to Kurozora? Join us 🔥') }}
+            </a>
+
             <x-button class="ml-4">
-                {{ __('Sign In') }}
+                {{ __('Open sesame 👐') }}
             </x-button>
         </div>
     </form>
