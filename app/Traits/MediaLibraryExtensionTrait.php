3105ed2225f0807505b20c46df329ad57ddb2bae
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-13 21:41:27 +0200

    [Update] Media Library Extension - Refactored getFirstMediaFullUrl method to be more flexible and match the getFirstMediaUrl implementation

--- app/Traits/MediaLibraryExtensionTrait.php
+++ app/Traits/MediaLibraryExtensionTrait.php
@@ -3,10 +3,26 @@
 namespace App\Traits;
 
 trait MediaLibraryExtensionTrait {
-    function getFirstMediaFullUrl($mediaName) {
-        $media = $this->getFirstMedia($mediaName);
+    /**
+     * Return the full url of the first media.
+     *
+     * @param string $collectionName
+     * @param string $conversionName
+     *
+     * @return string
+     */
+    function getFirstMediaFullUrl(string $collectionName = 'default', string $conversionName = ''): string
+    {
+        $media = $this->getFirstMedia($collectionName);
 
-        if($media == null) return null;
-        else return $media->getFullUrl();
+        if (!$media) {
+            return $this->getFallbackMediaUrl($collectionName) ?: '';
+        }
+
+        if ($conversionName !== '' && !$media->hasGeneratedConversion($conversionName)) {
+            return $media->getFullUrl();
+        }
+
+        return $media->getFullUrl($conversionName);
     }
 }
