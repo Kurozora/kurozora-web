66cfac6da929c2b6bd56d555e7e52187f3328804
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-16 01:35:18 +0200

    [Add] Password Settings - Added UpdatePasswordForm Livewire component - Added UpdateUserPassword action - Added UpdatesUserPasswords contract - Updated settings page to show the update password form

---
+++ app/Contracts/UpdatesUserPasswords.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Contracts;
+
+use App\Models\User;
+
+interface UpdatesUserPasswords
+{
+    /**
+     * Validate and update the user's password.
+     *
+     * @param User $user
+     * @param array $input
+     * @return void
+     */
+    public function update(User $user, array $input);
+}
