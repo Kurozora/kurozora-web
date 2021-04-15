764dac9a4494269700cb28cc97b2e0f2851b9fc9
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-16 01:35:18 +0200

    [Add] Password Settings - Added UpdatePasswordForm Livewire component - Added UpdateUserPassword action - Added UpdatesUserPasswords contract - Updated settings page to show the update password form

---
+++ app/Actions/Web/Auth/UpdateUserPassword.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Actions\Web\Auth;
+
+use App\Contracts\UpdatesUserPasswords;
+use App\Rules\ValidatePassword;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class UpdateUserPassword implements UpdatesUserPasswords
+{
+    /**
+     * Validate and update the user's password.
+     *
+     * @param mixed $user
+     * @param array $input
+     *
+     * @return void
+     * @throws ValidationException
+     */
+    public function update($user, array $input)
+    {
+        Validator::make($input, [
+            'current_password' => ['required', 'string'],
+            'password' => ['required', new ValidatePassword, 'confirmed'],
+        ])->after(function ($validator) use ($user, $input) {
+            if (!Hash::check($input['current_password'], $user->password)) {
+                $validator->errors()->add('current_password', __('The provided password does not match your current password.'));
+            }
+        })->validateWithBag('updatePassword');
+
+        $user->forceFill([
+            'password' => Hash::make($input['password']),
+        ])->save();
+    }
+}
