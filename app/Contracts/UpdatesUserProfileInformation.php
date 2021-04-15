709de0056ac9c0f0fff29eede8626f68dacff5fb
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-15 23:55:19 +0200

    [Add] Profile Services - Added ProfileServiceProvider - Added UpdatesUserProfileInformation - Added UpdateUserProfileInformation

---
+++ app/Contracts/UpdatesUserProfileInformation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Contracts;
+
+use App\Models\User;
+
+interface UpdatesUserProfileInformation
+{
+    /**
+     * Validate and update the given user's profile information.
+     *
+     * @param User  $user
+     * @param array  $input
+     * @return void
+     */
+    public function update(User $user, array $input);
+}
