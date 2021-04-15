86f6e66415e7fa598095bf8bd2b09525c1b09a5a
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-16 00:04:51 +0200

    [Add] Profile Settings - Added new blade components - Added UserProfileController controller - Added profile settings blade component - Added UpdateProfileInformationForm Livewire component - Added update-profile-information-form Livewire component

---
+++ app/Http/Livewire/Profile/UpdateProfileInformationForm.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Livewire\Profile;
+
+use App\Contracts\UpdatesUserProfileInformation;
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class UpdateProfileInformationForm extends Component
+{
+    use WithFileUploads;
+
+    /**
+     * The component's state.
+     *
+     * @var array
+     */
+    public array $state = [];
+
+    /**
+     * The new profile image for the user.
+     *
+     * @var mixed
+     */
+    public $photo;
+
+    /**
+     * The component's listeners.
+     *
+     * @var array
+     */
+    protected $listeners = [
+        'refresh-component' => '$refresh'
+    ];
+
+    /**
+     * Prepare the component.
+     *
+     * @return void
+     */
+    public function mount()
+    {
+        $this->state = Auth::user()->withoutRelations()->toArray();
+    }
+
+    /**
+     * Update the user's profile information.
+     *
+     * @param UpdatesUserProfileInformation $updater
+     * @return RedirectResponse|void
+     */
+    public function updateProfileInformation(UpdatesUserProfileInformation $updater)
+    {
+        $this->resetErrorBag();
+
+        $updater->update(
+            Auth::user(),
+            $this->photo
+                ? array_merge($this->state, ['photo' => $this->photo])
+                : $this->state
+        );
+
+        if (isset($this->photo)) {
+            return redirect()->route('profile.settings');
+        }
+
+        $this->emit('saved');
+
+        $this->emit('refresh-navigation-dropdown');
+
+        return;
+    }
+
+    /**
+     * Delete user's profile photo.
+     *
+     * @return void
+     */
+    public function deleteProfilePhoto()
+    {
+        Auth::user()->getFirstMedia(User::MEDIA_PROFILE_IMAGE)->delete();
+
+        $this->emitSelf('refresh-component');
+
+        $this->emit('refresh-navigation-dropdown');
+    }
+
+    /**
+     * Render the component.
+     *
+     * @return Application|Factory|View
+     */
+    public function render(): Application|Factory|View
+    {
+        return view('livewire.profile.update-profile-information-form');
+    }
+}
---
+++ resources/views/profile/settings.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="title">
+        {{ __('Settings') }}
+    </x-slot>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Kurozora ID') }}
+        </h2>
+    </x-slot>
+
+    <div>
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            @livewire('profile.update-profile-information-form')
+        </div>
+    </div>
+</x-app-layout>
