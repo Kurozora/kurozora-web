2d9a1ae445c89132d3b273859026508794b5bdee
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-16 00:06:10 +0200

    [Add] Me Route - Added a /me route which redirects to the currently authenticated user's profile page

--- routes/Web/Profile.php
+++ routes/Web/Profile.php
@@ -13,3 +13,11 @@ Route::prefix('/profile')
         Route::get('/{user}', Details::class)
             ->name('.details');
     });
+
+Route::prefix('/me')
+    ->middleware(['auth'])
+    ->group(function () {
+        Route::get('/', function () {
+            return redirect(route('profile.details', ['user' => Auth::user()]));
+        });
+    });
