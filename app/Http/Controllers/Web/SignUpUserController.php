b719fb9a22af529b8e7dc40cb91c93242178c17a
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-17 12:04:48 +0200

    [Update] User Registration - Enabled Registered event listener - Removed NewUserRegisteredEvent in favor of the default Registered event - Removed the now unused SendEmailConfirmationMail - Updated RegistrationController logic to send a Registered event upon registration - Update SignUpUserController logic to send a Registered event upon registration

--- app/Http/Controllers/Web/SignUpUserController.php
+++ app/Http/Controllers/Web/SignUpUserController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Web;
 
-use App\Events\NewUserRegisteredEvent;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Web\SignUpRequest;
 use App\Models\User;
 use Auth;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -48,7 +48,13 @@ class SignUpUserController extends Controller
             'password'              => User::hashPass($data['password'])
         ]);
 
-        $newUser->sendEmailVerificationNotification();
+        if ($request->hasFile('profileImage') &&
+            $request->file('profileImage')->isValid()) {
+            // Save the uploaded profile image
+            $newUser->updateProfileImage($request->file('profileImage'));
+        }
+
+        event(new Registered($newUser));
 
         Auth::login($newUser);
 
