4b2b2121d8c9b477c6b8a4499c5b821cb813d81d
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-18 17:59:01 +0200

    [Update] App Version - Bumped app version

--- config/app.php
+++ config/app.php
@@ -37,7 +37,7 @@ return [
     | the framework needs to place the application's version in a notification
     | or any other location as required by the application or its packages.
     */
-    'version' => env('APP_VERSION', '1.0.0'),
+    'version' => '1.1.0',
 
     /*
     |--------------------------------------------------------------------------
