3884593874c605c7ed57d380db059cfb90083f1f
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-19 22:13:58 +0200

    [Update] User Interface - Added disabling options to the button components - Fixed website colors - Removed the now ugly background image

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,5 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-orange-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-orange-400 active:bg-orange-600 focus:outline-none focus:border-orange-600 focus:ring-orange disabled:opacity-25 transition ease-in-out duration-150']) }}>
+@props(['disabled' => false])
+
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-orange-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-orange-400 active:bg-orange-600 focus:outline-none focus:border-orange-600 focus:ring-orange disabled:bg-gray-200 disabled:border-gray-200 disabled:text-gray-300 disabled:cursor-default transition ease-in-out duration-150']) }} {{ $disabled ? 'disabled' : '' }}>
     {{ $slot }}
 </button>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="background-image: url({{ asset('images/static/star_bg_lg.jpg') }})">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -28,8 +28,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
     </head>
 
-    <body>
-        <livewire:navigation-dropdown />
+    <body class="bg-white">
+        @livewire('navigation-dropdown')
 
         {{ $slot }}
 
