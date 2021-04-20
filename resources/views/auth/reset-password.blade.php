55c1aa81889234e5b00b23a0f41b47cb88e7bfc2
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-21 01:51:51 +0200

    [Update] 2FA and Password Reset - Prettified the password reset page - Removed the email field as it's not really necessary - Updated password reset view to obfuscate the email address when shown

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -9,32 +9,38 @@
         </h2>
     </x-slot>
 
-    <x-validation-errors class="mb-4" />
-
-    <form method="POST" action="{{ route('password.update') }}">
-        @csrf
-
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-        <div class="block">
-            <x-label for="email" value="{{ __('Email Address') }}" />
-            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+    <div  class="max-w-prose w-screen my-auto mx-4 sm:mx-0 p-10 rounded-md shadow-lg">
+        <div class="text-center mb-5">
+            <p class="text-2xl font-bold">{{ __('New Kurozora ID Password') }}</p>
+            <p>
+                {{ __('Enter a new password for') }} <span class="font-bold">{{ $email }}</span>
+            </p>
         </div>
 
-        <div class="mt-4">
-            <x-label for="password" value="{{ __('Password') }}" />
-            <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-        </div>
+        <x-validation-errors class="mb-4" />
 
-        <div class="mt-4">
-            <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-            <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-        </div>
+        <form method="POST" action="{{ route('password.update') }}">
+            @csrf
 
-        <div class="flex items-center justify-end mt-4">
-            <x-button>
-                {{ __('Reset Password') }}
-            </x-button>
-        </div>
-    </form>
+            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+            <input id="email" class="block mt-1 w-full" type="hidden" name="email" value="{{ $request->email }}" />
+
+            <div class="block">
+                <x-label for="password" value="{{ __('Password') }}" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+            </div>
+
+            <div class="mt-4">
+                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <x-button>
+                    {{ __('Reset Password') }}
+                </x-button>
+            </div>
+        </form>
+    </div>
 </x-app-layout>
