6b253dd2299acc3533b71f1ad1671ecb61193b19
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-22 01:17:44 +0200

    [Add] Error Views - Added 403 error image - Added 404 error image - Added 500 error image - Added 500 error view - Added 503 error image - Added 503 error view - Updated 403 error view - Updated 404 error view

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,27 @@
-<x-base-layout>
-    <div class="text-center my-12">
-        <h1 class="text-white">The page you’re looking for can’t be found.</h1>
-        <br>
-        <a href="{{ url('/') }}" class="">Go back to home page?</a>
+<x-error-layout>
+    <x-slot name="title">
+        {{ __('Not Found') }}
+    </x-slot>
+
+    <div class="md:flex min-h-screen">
+        <div class="w-full md:w-1/2 bg-white flex items-center justify-center">
+            <div class="max-w-md m-8">
+                <div class="text-black text-5xl md:text-9xl font-black">404</div>
+
+                <div class="w-16 h-1 bg-orange-300 my-3 md:my-6"></div>
+
+                <p class="text-grey-500 text-2xl md:text-3xl font-light mb-8 leading-normal">{{ __('You seem to be lost on the path of life...') }}</p>
+
+                <x-outlined-button href="{{ url('/') }}" class="text-base font-bold py-3 px-6 border-2">
+                    {{ __('Go Home?') }}
+                </x-outlined-button>
+            </div>
+        </div>
+
+        <div class="relative pb-[100%] md:flex md:pb-0 md:min-h-screen w-full md:w-1/2">
+            <div style="background-image: url({{ asset('images/illustrations/404.svg') }});"
+                 class="absolute inset-0 bg-cover bg-no-repeat md:bg-left lg:bg-center">
+            </div>
+        </div>
     </div>
-</x-base-layout>
+</x-error-layout>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,27 @@
+<x-error-layout>
+    <x-slot name="title">
+        {{ __('Internal Server Error') }}
+    </x-slot>
+
+    <div class="md:flex min-h-screen">
+        <div class="w-full md:w-1/2 bg-white flex items-center justify-center">
+            <div class="max-w-md m-8">
+                <div class="text-black text-5xl md:text-9xl font-black">500</div>
+
+                <div class="w-16 h-1 bg-green-300 my-3 md:my-6"></div>
+
+                <p class="text-gray-500 text-2xl md:text-3xl font-light mb-8 leading-normal max-w-prose">{{ __('No worries, it’s just the server dying. The crack team has been dispatched to bring it back to health.') }}</p>
+
+                <x-outlined-button href="{{ url('/') }}" class="text-green-500 text-base font-bold py-3 px-6 border-2 border-green-500 hover:bg-green-400 hover:border-green-400 focus:border-green-600 focus:ring-green active:bg-green-600">
+                    {{ __('Go Home') }}
+                </x-outlined-button>
+            </div>
+        </div>
+
+        <div class="relative pb-[100%] md:flex md:pb-0 md:min-h-screen w-full md:w-1/2">
+            <div style="background-image: url({{ asset('images/illustrations/500.svg') }});"
+                 class="absolute inset-0 bg-cover bg-no-repeat md:bg-left lg:bg-center">
+            </div>
+        </div>
+    </div>
+</x-error-layout>
