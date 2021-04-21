53f691c868f20d595e814d36bbfcb5244a3a278e
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-22 01:14:49 +0200

    [Add] Error layout - Added a new error layout - Updated Tailwind Css config

---
+++ app/View/Components/ErrorLayout.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class ErrorLayout extends Component
+{
+    /**
+     * Get the view / contents that represents the component.
+     *
+     * @return Application|Factory|View
+     */
+    public function render()
+    {
+        return view('layouts.error');
+    }
+}
