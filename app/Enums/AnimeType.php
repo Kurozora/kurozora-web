89c264f80c11a2675c8168cc1a532f07d7fb1e2e
kiritokatklian <casillaskhoren1@gmail.com>
2021-04-25 00:32:24 +0200

    [Update] Anime Type Enum - Added better descriptions

--- app/Enums/AnimeType.php
+++ app/Enums/AnimeType.php
@@ -13,4 +13,24 @@ final class AnimeType extends Enum
     const ONA       = 4;
     const OVA       = 5;
     const Special   = 6;
+
+    /**
+     * Get the description for an enum value
+     *
+     * @param  mixed  $value
+     * @return string
+     */
+    public static function getDescription($value): string
+    {
+        switch ($value) {
+            case self::TV:
+                return 'TV';
+            case self::ONA:
+                return 'Original Net Animation';
+            case self::OVA:
+                return 'Original Video Animation';
+        }
+
+        return parent::getDescription($value);
+    }
 }
