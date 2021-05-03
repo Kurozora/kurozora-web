a062612d30ac4c574f68bb5c45ca5ed80a982796
kiritokatklian <casillaskhoren1@gmail.com>
2021-05-03 21:59:16 +0200

    [Update] Livewire Config - Updated Livewire temporary file upload config

--- config/livewire.php
+++ config/livewire.php
@@ -82,11 +82,13 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => null,        // Example: 'local', 's3'              Default: 'default'
-        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => null,   // Example: 'tmp'                      Default  'livewire-tmp'
-        'middleware' => null,  // Example: 'throttle:5,1'             Default: 'throttle:60,1'
-        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs.
+        'disk' => 'local',      // Example: 'local', 's3'              Default: 'default'
+        'rules' => [            // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
+            'required', 'file', 'max:12288'
+        ],
+        'directory' => null,    // Example: 'tmp'                      Default  'livewire-tmp'
+        'middleware' => null,   // Example: 'throttle:5,1'             Default: 'throttle:60,1'
+        'preview_mimes' => [    // Supported file types for temporary pre-signed file URLs.
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
             'mov', 'avi', 'wmv', 'mp3', 'm4a',
             'jpg', 'jpeg', 'mpga', 'webp', 'wma',
