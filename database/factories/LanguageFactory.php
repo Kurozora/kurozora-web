f1a25a70f73218702141359f9947a27e4ba4bf3f
kiritokatklian <casillaskhoren1@gmail.com>
2021-05-07 23:12:23 +0200

    [Add] Languages - Added language factory - Added language model - Added language Nova resource - Added language migration - Added language seeder

---
+++ database/factories/LanguageFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Language;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LanguageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Language::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'name'          => $this->faker->name,
+            'code'          => $this->faker->languageCode,
+            'created_at'    => now(),
+            'updated_at'    => now(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            LanguageSeeder::class,
             RoleSeeder::class,
             StaffRoleSeeder::class,
             CastRoleSeeder::class,
