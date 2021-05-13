876c91ee055752c824bd1721c06156a1958f5e9c
kiritokatklian <casillaskhoren1@gmail.com>
2021-05-13 21:29:13 +0200

    [Update] Anime Resources - Added AnimeStaffResource - Removed genres relationship from AnimeResource - Removed people relationship from AnimeResource - Updated AnimeResourceBasic to include genres

--- app/Http/Resources/PersonResource.php
+++ app/Http/Resources/PersonResource.php
@@ -11,7 +11,7 @@ class PersonResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  Request  $request
+     * @param Request $request
      * @return array
      */
     public function toArray($request): array
