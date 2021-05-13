94d34ece2a82d4113b1b8a73ddf9fe6028149dd0
kiritokatklian <casillaskhoren1@gmail.com>
2021-05-13 23:48:00 +0200

    [Update] Anime Cast - Made all columns unique - Updated Nova to validate data uniqueness

--- database/migrations/2020_06_27_204108_create_anime_casts_table.php
+++ database/migrations/2020_06_27_204108_create_anime_casts_table.php
@@ -24,20 +24,20 @@ class CreateAnimeCastsTable extends Migration
             $table->unsignedBigInteger('person_id');
             $table->unsignedBigInteger('character_id');
             $table->unsignedBigInteger('anime_id');
-            $table->unsignedBigInteger('cast_role_id')->nullable();
+            $table->unsignedBigInteger('cast_role_id');
             $table->unsignedBigInteger('language_id');
             $table->timestamps();
         });
 
         Schema::table(AnimeCast::TABLE_NAME, function(Blueprint $table) {
             // Set unique index constraints
-            $table->unique(['person_id', 'character_id', 'anime_id']);
+            $table->unique(['person_id', 'character_id', 'anime_id', 'cast_role_id', 'language_id'], 'anime_cast_person_character_language_unique');
 
             // Set foreign key constraints
             $table->foreign('person_id')->references('id')->on(Person::TABLE_NAME)->onDelete('cascade');
             $table->foreign('character_id')->references('id')->on(Character::TABLE_NAME)->onDelete('cascade');
             $table->foreign('anime_id')->references('id')->on(Anime::TABLE_NAME)->onDelete('cascade');
-            $table->foreign('cast_role_id')->references('id')->on(CastRole::TABLE_NAME)->onDelete('set null');
+            $table->foreign('cast_role_id')->references('id')->on(CastRole::TABLE_NAME)->onDelete('cascade');
             $table->foreign('language_id')->references('id')->on(Language::TABLE_NAME)->onDelete('cascade');
         });
     }
