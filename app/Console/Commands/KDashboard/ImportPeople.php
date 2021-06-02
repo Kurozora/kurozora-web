1feccb63032f17b83fd4ea8b23cdfaa7e9a78c7d
kiritokatklian <casillaskhoren1@gmail.com>
2021-06-03 00:16:59 +0200

    [Update] Import Person - Added ImportPersonProcessor - Added ProcessImportPerson - Updated ImportPeople command to make use of the person import processor

--- app/Console/Commands/KDashboard/ImportPeople.php
+++ app/Console/Commands/KDashboard/ImportPeople.php
@@ -2,10 +2,10 @@
 
 namespace App\Console\Commands\KDashboard;
 
+use App\Jobs\ProcessImportPerson;
 use App\Models\KDashboard\People as KPeople;
-use App\Models\Person;
-use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
 
 class ImportPeople extends Command
 {
@@ -40,48 +40,9 @@ class ImportPeople extends Command
      */
     public function handle(): int
     {
-        $kPeople = KPeople::all();
-        $oldCount = Person::count('id');
-
-        $this->info('Total old people: ' . $oldCount);
-
-        $this->withProgressBar($kPeople, function (KPeople $kPerson) {
-            $person = Person::where([
-                ['mal_id', $kPerson->id],
-            ])->first();
-
-            if ($person) {
-                return;
-            }
-
-            $name = explode(', ', $kPerson->name);
-            $firstName = $name[0];
-            $lastName = $name[1] ?? '';
-            $birthDate = null;
-
-            if ($kPerson->birthday_day != 0 && $kPerson->birthday_month != 0) {
-                $birthDate = $kPerson->birthday_year . '-' . $kPerson->birthday_month . '-' . $kPerson->birthday_day;
-            }
-
-            Person::create([
-                'mal_id'            => $kPerson->id,
-                'first_name'        => $firstName,
-                'last_name'         => $lastName,
-                'given_name'        => $kPerson->given_name,
-                'family_name'       => $kPerson->family_name,
-                'alternative_names' => explode(', ', $kPerson->alternative_name),
-                'about'             => $kPerson->more,
-                'birth_date'        => empty($birthDate) ? null : Carbon::parse($birthDate),
-                'image'             => $kPerson->image_url,
-                'website_urls'      => empty($kPerson->website) ? null : explode(', ', $kPerson->website),
-            ]);
+        KPeople::chunk(1000, function (Collection $kPeople) {
+            ProcessImportPerson::dispatch($kPeople);
         });
-
-        $newCount = Person::count('id');
-
-        $this->newLine();
-        $this->info('Total new people added: ' . $newCount - $oldCount);
-        $this->info('Total people: ' . $newCount);
 
         return 1;
     }
