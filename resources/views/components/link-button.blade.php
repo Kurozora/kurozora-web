514e7cf084c5a7605dcc85b8ee31c1a37184d3b9
kiritokatklian <casillaskhoren1@gmail.com>
2021-06-12 23:06:23 +0200

    [Add] Outlined Link Button - Added outlined link button component - Updated error pages to use the outlined link button to link the user back to the home page

--- resources/views/components/link-button.blade.php
+++ resources/views/components/link-button.blade.php
@@ -1,3 +1,5 @@
-<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-orange-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-orange-400 active:bg-orange-600 focus:outline-none focus:border-orange-600 focus:ring-orange disabled:opacity-25 transition ease-in-out duration-150']) }}>
+@props(['disabled' => false])
+
+<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-orange-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-orange-400 active:bg-orange-600 focus:outline-none focus:border-orange-600 focus:ring-orange disabled:opacity-25 transition ease-in-out duration-150']) }}  {{ $disabled ? 'disabled' : '' }}>
     {{ $slot }}
 </a>
