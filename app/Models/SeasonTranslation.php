9650e5f8aeb527c8baab49efbc52c8a66377915f
kiritokatklian <casillaskhoren1@gmail.com>
2021-06-13 13:44:42 +0200

    [Add] Season Translations - Added season translations migration - Added season translations model - Added season translations nova model - Updated season factory

---
+++ app/Models/SeasonTranslation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SeasonTranslation extends Model
+{
+    use HasFactory;
+
+    // Table name
+    const TABLE_NAME = 'season_translations';
+    protected $table = self::TABLE_NAME;
+
+    /**
+     * The season the translations belongs to.
+     *
+     * @return BelongsTo
+     */
+    public function season(): BelongsTo
+    {
+        return $this->belongsTo(Season::class);
+    }
+
+    /**
+     * The language the translations belongs to.
+     *
+     * @return BelongsTo
+     */
+    public function language(): BelongsTo
+    {
+        return $this->belongsTo(Language::class, 'locale', 'code');
+    }
+}
