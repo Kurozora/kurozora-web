26f4a94685d3e046b4c2a06572b9a6731a2f9901
kiritokatklian <casillaskhoren1@gmail.com>
2021-06-25 00:04:10 +0200

    [Update] Studio Pagination & API - Added GetStudioAnimeRequest - Added pagination to studio relations - Updated openapi.json with the latest changes

--- app/Http/Controllers/StudioController.php
+++ app/Http/Controllers/StudioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\JSONResult;
+use App\Http\Requests\GetStudioAnimeRequest;
 use App\Http\Resources\AnimeResourceBasic;
 use App\Http\Resources\StudioResource;
 use App\Models\Studio;
@@ -27,16 +28,23 @@ class StudioController extends Controller
     /**
      * Returns anime information of a Studio.
      *
+     * @param GetStudioAnimeRequest $request
      * @param Studio $studio
      * @return JsonResponse
      */
-    public function anime(Studio $studio): JsonResponse
+    public function anime(GetStudioAnimeRequest $request, Studio $studio): JsonResponse
     {
+        $data = $request->validated();
+
         // Get the anime
-        $anime = $studio->getAnime();
+        $anime = $studio->getAnime($data['limit'] ?? 25, $data['page'] ?? 1);
+
+        // Get next page url minus domain
+        $nextPageURL = str_replace($request->root(), '', $anime->nextPageUrl());
 
         return JSONResult::success([
-            'data' => AnimeResourceBasic::collection($anime)
+            'data' => AnimeResourceBasic::collection($anime),
+            'next' => empty($nextPageURL) ? null : $nextPageURL
         ]);
     }
 }
