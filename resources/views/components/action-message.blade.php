3e34d7791dc1e02ec31a8d2e4cb41a4ea762ebc5
kiritokatklian <casillaskhoren1@gmail.com>
2021-06-27 20:16:08 +0200

    [Add] Star Rating - Added StarRating Livewire component - Implemented star rating on anime details page - Updated assets - Updated cast relation to only return japanese cast for now - Updated notification resource to use the correct MALImportFinished class for the switch

--- resources/views/components/action-message.blade.php
+++ resources/views/components/action-message.blade.php
@@ -1,7 +1,7 @@
-@props(['on'])
+@props(['on', 'timeout'])
 
 <div x-data="{ shown: false, timeout: null }"
-     x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 2000);  })"
+     x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, ); {{ $timeout ?? 2000 }} })"
      x-show.transition.opacity.out.duration.1500ms="shown"
      style="display: none;"
     {{ $attributes->merge(['class' => 'text-sm text-gray-600']) }}>
