162c0631fe8a16b2a81a553c3f3d9adf1de88f1f
kiritokatklian <casillaskhoren1@gmail.com>
2021-09-19 23:41:29 +0200

    [Update] Explore & Lockups - Updated explore view code structure - Updated lockup layouts to look and act more uniform

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -16,89 +16,91 @@
     </x-slot>
 
     <div class="max-w-7xl mx-auto px-6 pb-6">
-    @foreach($explorePageCategories as $key => $explorePageCategory)
-        @switch($explorePageCategory->type)
-        @case('most-popular-shows')
-            <div class="flex pb-10 overflow-x-scroll no-scrollbar">
-                <div class="flex flex-nowrap space-x-4">
-                    @foreach(App\Models\Anime::mostPopular()->get() as $anime)
-                        <x-lockups.banner-lockup :anime="$anime" />
-                    @endforeach
-                </div>
-            </div>
-            @break
-        @case('shows')
-            <section class="pt-5 pb-8 border-t-2">
-                <x-section-nav class="flex flex-no-wrap justify-between mb-5">
-                    <x-slot name="title">
-                        {{ $explorePageCategory->title }}
-                    </x-slot>
-                    <x-slot name="action">
-                        <x-simple-link href="#">{{ __('See All') }}</x-simple-link>
-                    </x-slot>
-                </x-section-nav>
-
-                @switch($explorePageCategory->size)
-                    @case('large')
-                        <div class="flex mt-5 overflow-x-scroll no-scrollbar">
-                            <div class="flex flex-nowrap space-x-4">
-                                @foreach($explorePageCategory->animes as $anime)
-                                    <x-lockups.large-lockup :anime="$anime" />
-                                @endforeach
-                            </div>
-                        </div>
-                        @break
-                    @case('small')
-                        <div class="flex mt-5 overflow-x-scroll no-scrollbar">
-                            <div class="flex flex-nowrap space-x-4">
-                                @foreach($explorePageCategory->animes as $anime)
-                                    <x-lockups.small-lockup :anime="$anime" />
-                                @endforeach
-                            </div>
-                        </div>
-                        @break
-                    @case('video')
-                        <div class="flex mt-5 overflow-x-scroll no-scrollbar">
-                            <div class="flex flex-nowrap space-x-4">
-                                @foreach($explorePageCategory->animes as $anime)
-                                    <x-lockups.video-lockup :anime="$anime" />
-                                @endforeach
-                            </div>
-                        </div>
-                        @break
-                    @default
-                    {{ 'Unhandled size: ' . $explorePageCategory->size }}
-                @endswitch
-            </section>
-            @break
-        @case('genres')
-            <section class="pt-5 pb-8 border-t-2">
-                <x-section-nav class="flex flex-no-wrap justify-between mb-5">
-                    <x-slot name="title">
-                        {{ __('Top Genres') }}
-                    </x-slot>
-                    <x-slot name="action">
-                        <x-simple-link href="{{ url('/genres') }}">{{ __('See All') }}</x-simple-link>
-                    </x-slot>
-                </x-section-nav>
-
-                <div class="flex mt-5 overflow-x-scroll no-scrollbar">
+        @foreach($explorePageCategories as $key => $explorePageCategory)
+            @switch($explorePageCategory->type)
+            @case('most-popular-shows')
+                <section class="flex pb-10 overflow-x-scroll no-scrollbar">
                     <div class="flex flex-nowrap space-x-4">
-                        @foreach($explorePageCategory->genres as $genre)
-                            <x-lockups.medium-lockup
-                                :href="url('/?genre=' . $genre->id)"
-                                :title="$genre->name"
-                                :backgroundColor="$genre->color"
-                                :backgroundImage="$genre->symbol ?? asset('images/static/icon/logo.png')"
-                            />
+                        @foreach(App\Models\Anime::mostPopular()->get() as $anime)
+                            <x-lockups.banner-lockup :anime="$anime" />
                         @endforeach
                     </div>
-                </div>
-            </section>
-            @break
-        @default
-            {{ 'Unhandled type: ' . $explorePageCategory->type }}
-        @endswitch
-    @endforeach
+                </section>
+                @break
+            @case('shows')
+                <section class="pt-5 pb-8 border-t-2">
+                    <x-section-nav class="flex flex-no-wrap justify-between mb-5">
+                        <x-slot name="title">
+                            {{ $explorePageCategory->title }}
+                        </x-slot>
+
+                        <x-slot name="action">
+                            <x-simple-link href="#">{{ __('See All') }}</x-simple-link>
+                        </x-slot>
+                    </x-section-nav>
+
+                    @switch($explorePageCategory->size)
+                        @case('large')
+                            <div class="flex mt-5 overflow-x-scroll no-scrollbar">
+                                <div class="flex flex-nowrap space-x-4">
+                                    @foreach($explorePageCategory->animes as $anime)
+                                        <x-lockups.large-lockup :anime="$anime" />
+                                    @endforeach
+                                </div>
+                            </div>
+                            @break
+                        @case('small')
+                            <div class="flex mt-5 overflow-x-scroll no-scrollbar">
+                                <div class="flex flex-nowrap space-x-4">
+                                    @foreach($explorePageCategory->animes as $anime)
+                                        <x-lockups.small-lockup :anime="$anime" />
+                                    @endforeach
+                                </div>
+                            </div>
+                            @break
+                        @case('video')
+                            <div class="flex mt-5 overflow-x-scroll no-scrollbar">
+                                <div class="flex flex-nowrap space-x-4">
+                                    @foreach($explorePageCategory->animes as $anime)
+                                        <x-lockups.video-lockup :anime="$anime" />
+                                    @endforeach
+                                </div>
+                            </div>
+                            @break
+                        @default
+                        {{ 'Unhandled size: ' . $explorePageCategory->size }}
+                    @endswitch
+                </section>
+                @break
+            @case('genres')
+                <section class="pt-5 pb-8 border-t-2">
+                    <x-section-nav class="flex flex-no-wrap justify-between mb-5">
+                        <x-slot name="title">
+                            {{ __('Top Genres') }}
+                        </x-slot>
+
+                        <x-slot name="action">
+                            <x-simple-link href="{{ url('/genres') }}">{{ __('See All') }}</x-simple-link>
+                        </x-slot>
+                    </x-section-nav>
+
+                    <div class="flex mt-5 overflow-x-scroll no-scrollbar">
+                        <div class="flex flex-nowrap space-x-4">
+                            @foreach($explorePageCategory->genres as $genre)
+                                <x-lockups.medium-lockup
+                                    :href="url('/?genre=' . $genre->id)"
+                                    :title="$genre->name"
+                                    :backgroundColor="$genre->color"
+                                    :backgroundImage="$genre->symbol ?? asset('images/static/icon/logo.png')"
+                                />
+                            @endforeach
+                        </div>
+                    </div>
+                </section>
+                @break
+            @default
+                {{ 'Unhandled type: ' . $explorePageCategory->type }}
+            @endswitch
+        @endforeach
     </div>
 </main>
