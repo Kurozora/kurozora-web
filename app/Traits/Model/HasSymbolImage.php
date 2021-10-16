428396f71ec63dda2c2bf1b3d1a9252de64f55c5
kiritokatklian <casillaskhoren1@gmail.com>
2021-10-16 14:45:03 +0200

    [Update] Genre Details - Added genre details Livewire component - Added genre details route - Added HasSymbolImage trait - Added media support to Genre model - Added slugs to Genre model - Moved genre filtered explore logic to genre details - Updated genre migration - Updated genre seeder - Updated GenreResource to include the symbol ImageResource

---
+++ app/Traits/Model/HasSymbolImage.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Traits\Model;
+
+use App\Models\Media;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
+use Ramsey\Uuid\Uuid;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileCannotBeAdded;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
+
+trait HasSymbolImage
+{
+    /**
+     * The name of the symbol image media collection.
+     *
+     * @var string $symbolImageCollectionName
+     */
+    protected string $symbolImageCollectionName = 'symbol';
+
+    /**
+     * Get the model's symbol image object.
+     *
+     * @return Media|null
+     */
+    function getSymbolImageAttribute(): Media|null
+    {
+        return $this->getFirstMedia($this->symbolImageCollectionName);
+    }
+
+    /**
+     * Get the URL of the model's symbol image.
+     *
+     * @return string|null
+     */
+    function getSymbolImageUrlAttribute(): ?string
+    {
+        return $this->getFirstMediaFullUrl($this->symbolImageCollectionName) ?: null;
+    }
+
+    /**
+     * Updates the model's symbol image.
+     *
+     * @param string|UploadedFile $uploadFile
+     * @param string|null $name
+     * @param array $customProperties
+     * @throws FileDoesNotExist
+     * @throws FileIsTooBig
+     * @throws FileCannotBeAdded
+     */
+    function updateSymbolImage(string|UploadedFile $uploadFile, string $name = null, array $customProperties = [])
+    {
+        // Determine media adder
+        $addMedia = Str::startsWith($uploadFile, ['http://', 'https://']) ? $this->addMediaFromUrl($uploadFile) : $this->addMedia($uploadFile);
+
+        // Configure properties
+        if (!empty($name)) {
+            $addMedia->usingName($name);
+        }
+        if (!empty($customProperties)) {
+            $addMedia->withCustomProperties($customProperties);
+        }
+        if (is_string($uploadFile)) {
+            $extension = pathinfo($uploadFile, PATHINFO_EXTENSION);
+            $addMedia->usingFileName(Uuid::uuid4() . '.' . $extension);
+        } else {
+            $addMedia->usingFileName(Uuid::uuid4() . '.' . $uploadFile->extension());
+        }
+
+        // Add media
+        $addMedia->toMediaCollection($this->symbolImageCollectionName);
+    }
+
+    /**
+     * Delete the model's symbol image.
+     *
+     * @return void
+     */
+    public function deleteSymbolImage()
+    {
+        $this->clearMediaCollection($this->symbolImageCollectionName);
+    }
+}
--- resources/views/components/lockups/genre-lockup.blade.php
+++ resources/views/components/lockups/genre-lockup.blade.php
@@ -7,10 +7,10 @@
     };
 @endphp
 
-<a class="relative" href="{{ route('home', ['genre' => $genre->id]) }}">
+<a class="relative" href="{{ route('genres.details', ['genre' => $genre]) }}">
     <div class="flex">
         <picture class="relative rounded-lg overflow-hidden" style="{{ $backgroundColor }};">
-            <img class="p-3 lazyload" data-sizes="auto" data-src="{{ $genre->symbol ?? asset('images/static/icon/logo.png') }}" alt="{{ $genre->name }} Symbol" title="{{ $genre->name }}">
+            <img class="p-3 lazyload" data-sizes="auto" data-src="{{ $genre->symbol_image_url ?? asset('images/static/icon/logo.png') }}" alt="{{ $genre->name }} Symbol" title="{{ $genre->name }}">
 
             <div class="h-[95px] p-3 py-5 bg-black/30 backdrop-blur text-center">
                 <p class="text-white font-bold leading-tight line-clamp-1">{{ $genre->name }}</p>
