ebf401952c3d64c932d4dc1064c735faec4563b6
kiritokatklian <casillaskhoren1@gmail.com>
2021-11-17 00:42:26 +0100

    [Add] Media Conversion Job - Added ConvertImageToWebPJob.php - Added GenerateImageAttributesJob.php

---
+++ app/Jobs/GenerateImageAttributesJob.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Jobs;
+
+use ColorPalette;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use NikKanetiya\LaravelColorPalette\Color;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class GenerateImageAttributesJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The object containing the media data.
+     *
+     * @var Media
+     */
+    protected Media $media;
+
+    /**
+     * Create a new job instance.
+     *
+     * @var Media $media
+     * @return void
+     */
+    public function __construct(Media $media)
+    {
+        $this->media = $media;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // Get file contents for ColorThief, then generate colors.
+        $image = file_get_contents($this->media->getFullUrl());
+        $colors = $this->generateColorsFor($image);
+
+        // Generate custom properties for the image. Works from URL.
+        $dimensions = $this->generateDimensionsFor($this->media->getFullUrl());
+
+        // Add colors and dimensions data to custom properties
+        $customProperties = array_merge($colors, $dimensions, $this->media->custom_properties);
+
+        // Update the media with the custom properties.
+        $this->media->update([
+            'custom_properties' => $customProperties
+        ]);
+    }
+
+    /**
+     * Generate colors for the given image URL/path.
+     *
+     * @param string $path
+     * @return array
+     */
+    protected function generateColorsFor(string $path): array
+    {
+        /** @var Color[] $palette */
+        $palette = ColorPalette::getPalette($path, 5, 8);
+
+        return [
+            'background_color' => $palette[0]->toHexString(),
+            'text_color_1' => $palette[1]->toHexString(),
+            'text_color_2' => $palette[2]->toHexString(),
+            'text_color_3' => $palette[3]->toHexString(),
+            'text_color_4' => $palette[4]->toHexString(),
+        ];
+    }
+
+    /**
+     * Generate dimensions for the given image URL/path.
+     *
+     * @param string $path
+     * @return array
+     */
+    protected function generateDimensionsFor(string $path): array
+    {
+        list($width, $height) = getimagesize($path);
+
+        return [
+            'width' => $width,
+            'height' => $height,
+        ];
+    }
+}
