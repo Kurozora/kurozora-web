10edf3e5ccf1c5ee3a575882a6c0d95d12b8f889
kiritokatklian <casillaskhoren1@gmail.com>
2021-12-05 14:49:01 +0100

    [Add] Characters Index - Added Characters Index route - Added Characters Index livewire component - Enabled characters button in navigation - Added person_poster_square asset - Added search and filter to Characters Index

--- resources/views/components/lockups/character-lockup.blade.php
+++ resources/views/components/lockups/character-lockup.blade.php
@@ -3,7 +3,7 @@
 <div {{ $attributes->merge(['class' => 'relative w-full pb-2']) }}>
     <div class="flex flex-col">
         <picture class="relative w-28 h-40 rounded-lg shadow-md overflow-hidden md:w-32 md:h-48">
-            <img class="w-full h-full object-cover lazyload" data-sizes="auto" data-src="{{ $character->profile_image_url ?? asset('images/static/placeholders/person_poster.webp') }}" alt="{{ $character->name }} Profile Picture" title="{{ $character->name }}">
+            <img class="w-full h-full object-cover lazyload" data-sizes="auto" data-src="{{ $character->profile_image_url ?? asset('images/static/placeholders/person_poster.webp') }}" alt="{{ $character->name }} Profile Image" title="{{ $character->name }}">
 
             <div class="absolute top-0 left-0 h-full w-full border-[1px] border-solid border-black/20 rounded-lg"></div>
         </picture>
