dd84747a6541f7b7d5db7f427235f3419ed5206b
kiritokatklian <casillaskhoren1@gmail.com>
2021-12-05 15:56:17 +0100

    [Add] People Index - Added People Index route - Added People Index livewire component - Enabled people button in navigation - Added search and filter to People Index

--- resources/views/components/lockups/person-lockup.blade.php
+++ resources/views/components/lockups/person-lockup.blade.php
@@ -3,7 +3,7 @@
 <div {{ $attributes->merge(['class' => 'relative w-full pb-2']) }}>
     <div class="flex flex-col">
         <picture class="relative w-28 h-40 rounded-lg shadow-md overflow-hidden md:w-32 md:h-48">
-            <img class="w-full h-full object-cover lazyload" data-sizes="auto" data-src="{{ $person->profile_image_url ?? asset('images/static/placeholders/person_poster.webp') }}" alt="{{ $person->full_name }} Profile Picture" title="{{ $person->full_name }}">
+            <img class="w-full h-full object-cover lazyload" data-sizes="auto" data-src="{{ $person->profile_image_url ?? asset('images/static/placeholders/person_poster.webp') }}" alt="{{ $person->full_name }} Profile Image" title="{{ $person->full_name }}">
 
             <div class="absolute top-0 left-0 h-full w-full border-[1px] border-solid border-black/20 rounded-lg"></div>
         </picture>
