3df277b5aa9da3a0c11da1ed80158d7796c2f955
kiritokatklian <casillaskhoren1@gmail.com>
2021-12-05 14:45:12 +0100

    [Add] Search & Filters - Added filter symbol asset - Added filter for items per page - Added filter for ordering results - Added search field - Updated input component style - Updated dropdown component style

--- resources/views/livewire/navigation-dropdown.blade.php
+++ resources/views/livewire/navigation-dropdown.blade.php
@@ -51,7 +51,8 @@
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
                                 stroke-width="2"
-                                d="M6 18L18 6M6 6l12 12" />
+                                d="M6 18L18 6M6 6l12 12"
+                            />
                         </svg>
                     </button>
                 </div>
