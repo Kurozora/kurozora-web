1037c8890a4478fbadc08ae341a21307e981233f
kiritokatklian <casillaskhoren1@gmail.com>
2021-12-05 15:56:17 +0100

    [Add] People Index - Added People Index route - Added People Index livewire component - Enabled people button in navigation - Added search and filter to People Index

--- routes/Web/People.php
+++ routes/Web/People.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Livewire\Person\Anime as PersonAnime;
-use App\Http\Livewire\Person\Details as PersonDetails;
 use App\Http\Livewire\Person\Characters as PersonCharacters;
+use App\Http\Livewire\Person\Details as PersonDetails;
+use App\Http\Livewire\Person\Index as PersonIndex;
 
 Route::prefix('/people')
     ->name('people')
     ->group(function () {
+        Route::get('/', PersonIndex::class)
+            ->name('.index');
+
         Route::prefix('{person}')
             ->group(function () {
                 Route::get('/', PersonDetails::class)
