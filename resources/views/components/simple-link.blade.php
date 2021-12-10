603df5484fe63d2dc2a8c96417b710e71ab98982
kiritokatklian <casillaskhoren1@gmail.com>
2021-12-11 00:38:15 +0100

    [Add] Profile Info Badge - Added profile-information-badge Blade component

--- resources/views/components/simple-link.blade.php
+++ resources/views/components/simple-link.blade.php
@@ -1,5 +1,5 @@
 @props(['disabled' => false])
 
-<a {{ $attributes->merge(['class' => 'inline-flex items-center text-xs text-orange-500 tracking-widest focus:outline-none hover:text-orange-400 hover:underline active:text-orange-600 disabled:text-gray-300 disabled:cursor-default transition ease-in-out duration-150']) }}  {{ $disabled ? 'disabled' : '' }}>
+<a {{ $attributes->merge(['class' => 'inline-flex items-center text-xs text-orange-500 tracking-widest focus:outline-none hover:text-orange-400 hover:underline active:text-orange-600 disabled:text-gray-300 disabled:cursor-default transition ease-in-out duration-150']) }} {{ $disabled ? 'disabled' : '' }}>
     {{ $slot }}
 </a>
