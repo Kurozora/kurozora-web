a82009e7309948874dd57bd158251551900adb0c
kiritokatklian <casillaskhoren1@gmail.com>
2022-01-15 22:47:52 +0100

    [Add] UserLibrary to Nova - Added UserLibrary resource to Nova. - Updated UserLibrary with start_date and end_date attributes

--- app/Models/UserLibrary.php
+++ app/Models/UserLibrary.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class UserLibrary extends Pivot
@@ -9,4 +10,41 @@ class UserLibrary extends Pivot
     // Table name
     const TABLE_NAME = 'user_libraries';
     protected $table = self::TABLE_NAME;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+    ];
+
+    /**
+     * The anime the library belongs to.
+     *
+     * @return BelongsTo
+     */
+    public function anime(): BelongsTo
+    {
+        return $this->belongsTo(Anime::class);
+    }
+
+    /**
+     * The user the library belongs to.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
