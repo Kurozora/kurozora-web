356fe0db61036bd95379740695cee0b574badc9b
kiritokatklian <casillaskhoren1@gmail.com>
2022-01-15 23:46:00 +0100

    [Update] Nginx Config - Allow up to 10M http body size - Updated Livewire temp max file size

--- config/livewire.php
+++ config/livewire.php
@@ -98,7 +98,7 @@ return [
     'temporary_file_upload' => [
         'disk' => 'local',      // Example: 'local', 's3'              Default: 'default'
         'rules' => [            // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
-            'required', 'file', 'max:3072'
+            'required', 'file', 'max:10240' // 10MB
         ],
         'directory' => null,    // Example: 'tmp'                      Default  'livewire-tmp'
         'middleware' => null,   // Example: 'throttle:5,1'             Default: 'throttle:60,1'
