8b53be6091896419ab1519343685ee41bd8dcdd1
kiritokatklian <casillaskhoren1@gmail.com>
2022-01-15 18:04:18 +0100

    [Update] Move Settings Endpoint - Moved settings from /profile/settings to /settings. A user with the username 'settings' should now be able to view their profile page

--- routes/Web/Profile.php
+++ routes/Web/Profile.php
@@ -6,10 +6,10 @@ use App\Http\Livewire\Profile\Details;
 Route::prefix('/profile')
     ->name('profile')
     ->group(function () {
-        Route::get('/settings', [UserProfileController::class, 'settings'])
-            ->middleware(['auth', 'verified'])
-            ->name('.settings');
-
         Route::get('/{user}', Details::class)
             ->name('.details');
     });
+
+Route::get('/settings', [UserProfileController::class, 'settings'])
+    ->middleware(['auth', 'verified'])
+    ->name('profile.settings');
