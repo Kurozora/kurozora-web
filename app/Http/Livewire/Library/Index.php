f0c997d85798ecfd2958006c5551ef4848b52d29
kiritokatklian <casillaskhoren1@gmail.com>
2022-01-21 19:38:41 +0100

    [Add] Library - Added Library route - Added Library Livewire component - Added Library/Tab Livewire component - Added library assets - Updated packages

--- app/Http/Livewire/Library/Index.php
+++ app/Http/Livewire/Library/Index.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Livewire\Library;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Index extends Component
 {
-    public function render()
+    /**
+     * Render the component.
+     *
+     * @return Application|Factory|View
+     */
+    public function render(): Application|Factory|View
     {
         return view('livewire.library.index');
     }
