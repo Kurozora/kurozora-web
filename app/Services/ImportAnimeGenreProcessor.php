e3235d3b078b47f9183b26a97cb43ee6e116c948
kiritokatklian <casillaskhoren1@gmail.com>
2022-01-25 23:50:56 +0100

    [Update] Genre Import Process - Updated genre import process to also account for themes

--- app/Services/ImportAnimeGenreProcessor.php
+++ app/Services/ImportAnimeGenreProcessor.php
@@ -6,6 +6,8 @@ use App\Models\Anime;
 use App\Models\Genre;
 use App\Models\KDashboard\MediaGenre as KMediaGenre;
 use App\Models\MediaGenre;
+use App\Models\MediaTheme;
+use App\Models\Theme;
 use App\Scopes\TvRatingScope;
 use Illuminate\Database\Eloquent\Collection;
 
@@ -24,20 +26,39 @@ class ImportAnimeGenreProcessor
             $genre = Genre::withoutGlobalScope(new TvRatingScope)->firstWhere([
                 ['name', $kMediaGenre->genre->genre],
             ]);
+            $theme = Theme::withoutGlobalScope(new TvRatingScope)->firstWhere([
+                ['name', $kMediaGenre->genre->genre],
+            ]);
 
-            $mediaGenre = MediaGenre::where([
-                ['type', 'anime'],
-                ['media_id', $anime->id],
-                ['genre_id', $genre->id],
-            ])->first();
-
-            if (empty($mediaGenre)) {
-                MediaGenre::create([
-                    'media_id' => $anime->id,
-                    'genre_id' => $genre->id,
-                    'type' => 'anime',
+            if ($genre) {
+                $mediaGenre = MediaGenre::firstWhere([
+                    ['model_type', Anime::class],
+                    ['model_id', $anime->id],
+                    ['genre_id', $genre->id],
                 ]);
+
+                if (empty($mediaGenre)) {
+                    MediaGenre::create([
+                        'model_type' => Anime::class,
+                        'model_id' => $anime->id,
+                        'genre_id' => $genre->id,
+                    ]);
+                }
+            } else if ($theme) {
+                $mediaTheme = MediaTheme::firstWhere([
+                   ['nam', $kMediaGenre->genre->genre]
+                ]);
+
+                if (empty($mediaTheme)) {
+                    MediaTheme::create([
+                        'model_type' => Anime::class,
+                        'model_id' => $anime->id,
+                        'theme_id' => $theme->id,
+                    ]);
+                }
             }
+
+
         }
     }
 }
