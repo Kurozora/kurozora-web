8de190c884b0220e4eebdc28f9a3816c288eec1a
kiritokatklian <casillaskhoren1@gmail.com>
2022-01-25 23:06:10 +0100

    [Add] Theme Views - Added a new theme-lockup - Added themes details Livewire component - Added themes index Livewire component - Added themes route - Added new assets

--- resources/views/components/lockups/theme-lockup.blade.php
+++ resources/views/components/lockups/theme-lockup.blade.php
@@ -1,16 +1,23 @@
 @props(['theme'])
 
-<div class="max-w-lg sm:max-w-lg md:max-w-xs lg:max-w-sm rounded overflow-hidden shadow-lg">
-    <x-picture>
-        <img class="w-full aspect-ratio-1-1 object-cover object-bottom lazyload" data-sizes="auto" data-src="{{ $theme->screenshot_image_url }}" alt="{{ $theme->name }}" title="{{ $theme->name}}" style="background-color: {{ $theme->screenshot_image?->custom_properties['background_color'] ?? '#FFFFFF' }};">
-    </x-picture>
+@php
+    $backgroundColor = match ($theme->color) {
+        '#ffffff' => 'background: linear-gradient(-45deg, rgb(56, 62, 87) 22%, rgb(98, 112, 170) 88%)',
+        default => 'background-color: ' . $theme->color
+    };
+@endphp
 
-    <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{{ $theme->name }}</div>
-        <p class="text-gray-700 text-base">{{ trans_choice('{1} :x Download|:x Downloads', $theme->download_count, ['x' => $theme->download_count]) }}</p>
-    </div>
+<a class="relative" href="{{ route('themes.details', ['theme' => $theme]) }}">
+    <div class="flex">
+        <picture class="relative rounded-lg overflow-hidden" style="{{ $backgroundColor }};">
+            <img class="p-3 aspect-ratio-1-1 lazyload" data-sizes="auto" data-src="{{ $theme->symbol_image_url ?? asset('images/static/icon/logo.webp') }}" alt="{{ $theme->name }} Symbol" title="{{ $theme->name }}">
 
-    <div class="px-6 pt-4 pb-2">
-        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ __('Premium') }}</span>
+            <div class="h-[95px] p-3 py-5 bg-black/30 backdrop-blur text-center">
+                <p class="text-white font-bold leading-tight line-clamp-1">{{ $theme->name }}</p>
+                <p class="text-sm text-white/90 leading-tight line-clamp-2">{{ $theme->description }}</p>
+            </div>
+
+            <div class="absolute top-0 left-0 h-full w-full border border-solid border-black/20 rounded-lg"></div>
+        </picture>
     </div>
-</div>
+</a>
