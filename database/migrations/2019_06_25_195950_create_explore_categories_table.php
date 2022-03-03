3a84b8f6f95ef9b3d45b01e2536473bcd477d951
kiritokatklian <casillaskhoren1@gmail.com>
2022-03-03 13:10:26 +0100

    [Add] Explore Category Disabling - Added explore.only_enabled scope binding - Added ExploreCategoryAlwaysEnabled middleware - Added ExploreCategoryIsEnabledScope - Added is_enabled property - Added is_enabled property field to Nova

--- database/migrations/2019_06_25_195950_create_explore_categories_table.php
+++ database/migrations/2019_06_25_195950_create_explore_categories_table.php
@@ -23,6 +23,7 @@ class CreateExploreCategoriesTable extends Migration
             $table->string('size');
             $table->smallInteger('position');
             $table->boolean('is_global');
+            $table->boolean('is_enabled');
             $table->timestamps();
         });
 
