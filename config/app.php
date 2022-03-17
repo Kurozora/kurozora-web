b696484469b0ff107abb66ce1dc02550de272c17
kiritokatklian <casillaskhoren1@gmail.com>
2022-03-17 23:01:51 +0100

    [Fix] Broadcast Date - Fixed an error that caused anime/details to crash because of broadcast date

--- config/app.php
+++ config/app.php
@@ -40,7 +40,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '1.7.11-alpha.1',
+    'version' => '1.7.12-alpha.1',
 
     /*
     |--------------------------------------------------------------------------
