e152675953230d98e6f894daa7eaa859668f66b2
kiritokatklian <casillaskhoren1@gmail.com>
2022-03-31 23:16:06 +0200

    [Update] Studio Details - Added about section

--- config/app.php
+++ config/app.php
@@ -40,7 +40,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '1.8.5-alpha.0',
+    'version' => '1.8.5-alpha.1',
 
     /*
     |--------------------------------------------------------------------------
