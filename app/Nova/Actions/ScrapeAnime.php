e4d7656aaa0e829374c2cb444e154962a59c217b
kiritokatklian <casillaskhoren1@gmail.com>
2022-04-17 19:17:09 +0200

    [Add] Built-in Scraper - Added custom Nova Action resource - Added processors - Added roach config - Added scraper action to Nova - Added scraper config - Added scraper commands - Added spiders - Updated AppServiceProvider - Updated packages

---
+++ app/Nova/Actions/ScrapeAnime.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use App\Models\Anime;
+use Artisan;
+use Auth;
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+
+class ScrapeAnime extends Action
+{
+    use InteractsWithQueue, Queueable;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param ActionFields $fields
+     * @param Collection $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models): mixed
+    {
+        $isOwner = Auth::user()->id == 2;
+        $nextUpdateTime = $isOwner ? 0 : 15;
+
+        if (!$isOwner && $models->count() != 1) {
+            return Action::danger('You may only scrape 1 anime at a time.');
+        }
+
+        $scrapeCount = 0;
+        $nonScrapeCount = 0;
+
+        /** @var Anime $model */
+        foreach ($models as $model) {
+            $elapsedUpdatedAt = $model->updated_at->diffInMinutes(now());
+
+            if ($elapsedUpdatedAt >= $nextUpdateTime) {
+                try {
+                    Artisan::call('scrape:mal_anime', ['malID' => $model->mal_id]);
+
+                    $this->markAsFinished($model);
+                    $scrapeCount += 1;
+                } catch (Exception $e) {
+                    $this->markAsFailed($model, $e);
+                    $nonScrapeCount += 1;
+
+                    if (!$isOwner) {
+                        return Action::danger(__('There was an error scraping this anime. Please check the error message in the "Actions" section at the bottom of the anime’s detail page.'));
+                    }
+                }
+            } else {
+                $this->markAsFailed($model, __('Please wait :x minutes before scraping again.', ['x' => $nextUpdateTime - $elapsedUpdatedAt]));
+                $nonScrapeCount += 1;
+
+                if (!$isOwner) {
+                    return Action::danger(__('Please wait :x minutes before scraping again.', ['x' => $nextUpdateTime - $elapsedUpdatedAt]));
+                }
+            }
+        }
+
+        return Action::message('Scraped ' . $scrapeCount . '/' . $models->count() . ' anime, and had ' . $nonScrapeCount . ' issues.');
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array
+     */
+    public function fields(): array
+    {
+        return [];
+    }
+}
---
+++ config/scraper.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Scrapeable Domains
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the scrapeable domains such as MAL and TVDB,
+    | and others. This file provides a sane default location for this type
+    | of information, allowing packages to have a conventional place to
+    | find your various domains.
+    |
+    */
+
+    'domains' => [
+        'mal' => 'https://myanimelist.net',
+        'tvdb' => 'https://tvdb.com'
+    ]
+
+];
