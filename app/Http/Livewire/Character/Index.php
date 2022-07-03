8e348f4cb1a1b51b9e7f7e3574ee1054331ecff8
kiritokatklian <casillaskhoren1@gmail.com>
2022-07-03 18:50:45 +0200

    [Update] Character Search - Added search filtering to Character/Index - Added search ordering to Character/Index - Implemented MeiliSearch with Character/Index

--- app/Http/Livewire/Character/Index.php
+++ app/Http/Livewire/Character/Index.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire\Character;
 
+use App\Enums\AstrologicalSign;
+use App\Enums\CharacterStatus;
 use App\Models\Character;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
@@ -15,29 +17,49 @@ class Index extends Component
     use WithPagination;
 
     /**
-     * The component's filters.
+     * The search string.
+     *
+     * @var string $search
+     */
+    public string $search = '';
+
+    /**
+     * The number of results per page.
+     *
+     * @var int $perPage
+     */
+    public int $perPage = 25;
+
+    /**
+     * The component's filter attributes.
      *
      * @var array $filter
      */
-    public array $filter = [
-        'search' => '',
-        'order_type' => '',
-        'per_page' => 25,
-    ];
+    public array $filter = [];
+
+    /**
+     * The component's order attributes.
+     *
+     * @var array $order
+     */
+    public array $order = [];
 
     /**
      * Prepare the component.
      *
      * @return void
      */
-    public function mount() {}
+    public function mount(): void {
+        $this->setFilterableAttributes();
+        $this->setOrderableAttributes();
+    }
 
     /**
      * Redirect the user to a random character.
      *
      * @return void
      */
-    public function randomCharacter()
+    public function randomCharacter(): void
     {
         $this->redirectRoute('characters.details', Character::inRandomOrder()->first());
     }
@@ -49,20 +71,172 @@ class Index extends Component
      */
     public function getCharactersProperty(): LengthAwarePaginator
     {
-        $characters = Character::query();
-
         // Search
-        if (!empty($this->filter['search'])) {
-            $characters = $characters->whereTranslationLike('name', '%' . $this->filter['search'] . '%');
-        }
+        $characters = Character::search($this->search);
 
         // Order
-        if (!empty($this->filter['order_type'])) {
-            $characters = $characters->orderByTranslation('name', $this->filter['order_type']);
+        foreach ($this->order as $attribute => $order) {
+            $selected = $order['selected'];
+            if (!empty($selected)) {
+                $characters = $characters->orderBy($attribute, $selected);
+            }
+        }
+
+        // Filter
+        foreach ($this->filter as $attribute => $filter) {
+            $selected = $filter['selected'];
+            $type = $filter['type'];
+
+            if ((is_numeric($selected) && $selected >= 0) || !empty($selected)) {
+                switch ($type) {
+                    case 'double':
+                        $number = number_format($selected, 2, '.', '');
+                        $characters = $characters->where($attribute, $number);
+                        break;
+                    default:
+                        $characters = $characters->where($attribute, $selected);
+                }
+            }
         }
 
         // Paginate
-        return $characters->paginate($this->filter['per_page'] ?? 25);
+        return $characters->paginate($this->perPage);
+    }
+
+    /**
+     * Set the orderable attributes of the model.
+     *
+     * @return void
+     */
+    public function setOrderableAttributes(): void
+    {
+        $this->order = [
+            'name' => [
+                'title' => __('Name'),
+                'options' => [
+                    'Default' => null,
+                    'A-Z' => 'asc',
+                    'Z-A' => 'desc',
+                ],
+                'selected' => null,
+            ],
+            'age' => [
+                'title' => __('Age'),
+                'options' => [
+                    'Default' => null,
+                    'Youngest' => 'asc',
+                    'Oldest' => 'desc',
+                ],
+                'selected' => null,
+            ],
+            'height' => [
+                'title' => __('Height'),
+                'options' => [
+                    'Default' => null,
+                    'Shortest' => 'asc',
+                    'Tallest' => 'desc',
+                ],
+                'selected' => null,
+            ],
+            'weight' => [
+                'title' => __('Weight'),
+                'options' => [
+                    'Default' => null,
+                    'Lightest' => 'asc',
+                    'Heaviest' => 'desc',
+                ],
+                'selected' => null,
+            ],
+        ];
+    }
+
+    /**
+     * Set the filterable attributes of the model.
+     *
+     * @return void
+     */
+    public function setFilterableAttributes(): void
+    {
+        $this->filter = [
+            'status' => [
+                'title' => __('Status'),
+                'type' => 'select',
+                'options' => CharacterStatus::asSelectArray(),
+                'selected' => null,
+            ],
+            'age' => [
+                'title' => __('Age'),
+                'type' => 'double',
+                'selected' => null,
+            ],
+            'birth_day' => [
+                'title' => __('Birth Day'),
+                'type' => 'day',
+                'selected' => null,
+            ],
+            'birth_month' => [
+                'title' => __('Birth Month'),
+                'type' => 'month',
+                'selected' => null,
+            ],
+            'height' => [
+                'title' => __('Height (cm)'),
+                'type' => 'double',
+                'selected' => null,
+            ],
+            'weight' => [
+                'title' => __('Weight (grams)'),
+                'type' => 'double',
+                'selected' => null,
+            ],
+            'bust' => [
+                'title' => __('Bust'),
+                'type' => 'double',
+                'selected' => null,
+            ],
+            'waist' => [
+                'title' => __('Waist'),
+                'type' => 'double',
+                'selected' => null,
+            ],
+            'hip' => [
+                'title' => __('Hip'),
+                'type' => 'double',
+                'selected' => null,
+            ],
+            'astrological_sign' => [
+                'title' => __('Astrological Sign'),
+                'type' => 'select',
+                'options' => AstrologicalSign::asSelectArray(),
+                'selected' => null,
+            ],
+        ];
+    }
+
+    /**
+     * Reset order to default values.
+     *
+     * @return void
+     */
+    public function resetOrder(): void
+    {
+        $this->order = array_map(function ($order) {
+            $order['selected'] = null;
+            return $order;
+        }, $this->order);
+    }
+
+    /**
+     * Reset filter to default values.
+     *
+     * @return void
+     */
+    public function resetFilter(): void
+    {
+        $this->filter = array_map(function ($filter) {
+            $filter['selected'] = null;
+            return $filter;
+        }, $this->filter);
     }
 
     /**
