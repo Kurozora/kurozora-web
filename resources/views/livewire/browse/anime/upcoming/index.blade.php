8860cc2bf36f9418eac503fa8bcc2142316904ae
kiritokatklian <casillaskhoren1@gmail.com>
2022-07-03 18:42:38 +0200

    [Update] Anime Search - Added KEngineManager extension - Added KMeiliSearchEngine extension - Added ScoutServiceProvider - Added search filtering to Anime/Index - Added search filtering to Anime/Upcoming/Index - Added search ordering to Anime/Index - Added search ordering to Anime/Upcoming/Index - Implemented MeiliSearch with Anime/Index - Implemented MeiliSearch with Anime/Upcoming/Index

--- resources/views/livewire/browse/anime/upcoming/index.blade.php
+++ resources/views/livewire/browse/anime/upcoming/index.blade.php
@@ -28,26 +28,99 @@
                     <div class="flex flex-1 items-center justify-end space-x-1">
                         <x-spinner />
 
+                        <x-square-button wire:click="randomAnime">
+                            @svg('dice', 'fill-current', ['aria-labelledby' => 'random anime', 'width' => '28'])
+                        </x-square-button>
+
+                        {{-- Order --}}
                         <x-dropdown align="right" width="48">
                             <x-slot:trigger>
-                                <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                                    @svg('line_3_horizontal_decrease_circle', 'fill-current', ['aria-labelledby' => 'filter', 'width' => '28'])
-                                </button>
+                                <x-square-button>
+                                    @svg('arrow_up_arrow_down_circle', 'fill-current', ['aria-labelledby' => 'filter', 'width' => '28'])
+                                </x-square-button>
                             </x-slot>
 
                             <x-slot:content>
-                                {{-- Order --}}
-                                <div class="block px-4 py-2 bg-gray-100 text-xs text-gray-400 font-semibold">
-                                    {{ __('Order') }}
-                                </div>
+                                @foreach($this->order as $order)
+                                    @if($order['selected'])
+                                        {{-- Reset Order --}}
+                                        <button class="block w-full px-4 py-2 bg-gray-100 text-xs text-center text-gray-400 font-semibold hover:bg-gray-50 focus:bg-gray-200" wire:click="resetOrder">
+                                            {{ __('Reset Order') }}
+                                        </button>
+                                        @break
+                                    @endif
+                                @endforeach
 
-                                <div class="block px-4 py-2">
-                                    <x-select id="orderType" wire:model="filter.order_type">
-                                        <option value="">{{ __('Default') }}</option>
-                                        <option value="asc">{{ __('A-Z') }}</option>
-                                        <option value="desc">{{ __('Z-A') }}</option>
-                                    </x-select>
-                                </div>
+                                @foreach($this->order as $key => $order)
+                                    <div class="block px-4 py-2 bg-gray-100 text-xs text-gray-400 font-semibold">
+                                        {{ $order['title'] }}
+                                    </div>
+
+                                    <div class="block px-4 py-2">
+                                        <x-select id="{{ $key }}" wire:model="order.{{ $key }}.selected">
+                                            @foreach($order['options'] as $optionKey => $option)
+                                                <option value="{{ $option }}">{{ __($optionKey) }}</option>
+                                            @endforeach
+                                        </x-select>
+                                    </div>
+                                @endforeach
+                            </x-slot>
+                        </x-dropdown>
+
+                        {{-- Filter --}}
+                        <x-dropdown align="right" width="48">
+                            <x-slot:trigger>
+                                <x-square-button>
+                                    @svg('line_3_horizontal_decrease_circle', 'fill-current', ['aria-labelledby' => 'filter', 'width' => '28'])
+                                </x-square-button>
+                            </x-slot>
+
+                            <x-slot:content>
+                                @foreach($this->filter as $filter)
+                                    @if($filter['selected'] != null)
+                                        {{-- Reset Order --}}
+                                        <button class="block w-full px-4 py-2 bg-gray-100 text-xs text-center text-gray-400 font-semibold hover:bg-gray-50 focus:bg-gray-200" wire:click="resetFilter">
+                                            {{ __('Reset Filters') }}
+                                        </button>
+                                        @break
+                                    @endif
+                                @endforeach
+
+                                @foreach($this->filter as $key => $filter)
+                                    {{-- Per Page --}}
+                                    <div class="block px-4 py-2 bg-gray-100 text-xs text-gray-400 font-semibold">
+                                        {{ $filter['title'] }}
+                                    </div>
+
+                                    <div class="block px-4 py-2">
+                                        @switch($filter['type'])
+                                            @case('date')
+                                                <x-input id="{{ $key }}" type="date" wire:model="filter.{{ $key }}.selected" />
+                                                @break
+                                            @case('duration')
+                                                <x-input id="{{ $key }}" type="number" step="1" wire:model="filter.{{ $key }}.selected" />
+                                                @break
+                                            @case('time')
+                                                <x-input id="{{ $key }}" type="time" wire:model="filter.{{ $key }}.selected" />
+                                                @break
+                                            @case('select')
+                                                <x-select id="{{ $key }}" wire:model="filter.{{ $key }}.selected">
+                                                    <option value="">{{ __('Default') }}</option>
+                                                    @foreach($filter['options'] as $optionKey => $option)
+                                                        <option value="{{ $optionKey }}">{{ __($option) }}</option>
+                                                    @endforeach
+                                                </x-select>
+                                                @break
+                                            @case('bool')
+                                                <x-input id="{{ $key }}True" value="true" name="{{ $key }}" type="radio" wire:model="filter.{{ $key }}.selected" />
+                                                <label for="{{ $key }}True">{{ __('Enabled') }}</label>
+                                                <br>
+                                                <x-input id="{{ $key }}False" value="" name="{{ $key }}" type="radio" wire:model="filter.{{ $key }}.selected" />
+                                                <label for="{{ $key }}False">{{ __('Disabled') }}</label>
+                                                @break
+                                        @endswitch
+                                    </div>
+                                @endforeach
 
                                 {{-- Per Page --}}
                                 <div class="block px-4 py-2 bg-gray-100 text-xs text-gray-400 font-semibold">
@@ -55,7 +128,7 @@
                                 </div>
 
                                 <div class="block px-4 py-2">
-                                    <x-select id="perPage" wire:model="filter.per_page">
+                                    <x-select id="perPage" wire:model="perPage">
                                         <option value="25">25</option>
                                         <option value="50">50</option>
                                         <option value="100">100</option>
