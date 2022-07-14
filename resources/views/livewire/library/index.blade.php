9ad91649318a36afd0f9d9e034685bb59d5ec8b4
kiritokatklian <casillaskhoren1@gmail.com>
2022-07-14 16:56:25 +0200

    [Update] Library Components - Updated tab logic to interact with query string instead of hashtag - Updated UserLibraryStatus order

--- resources/views/livewire/library/index.blade.php
+++ resources/views/livewire/library/index.blade.php
@@ -16,60 +16,25 @@
 
     <div
         class="max-w-7xl mx-auto px-4 py-6 sm:px-6"
-        x-data="{ tab: window.location.hash ? window.location.hash : '#watching' }"
+        x-data="{
+            selectedStatus: @entangle('status')
+        }"
     >
         <div class="flex flex-nowrap gap-x-4 justify-between text-center whitespace-nowrap overflow-x-scroll no-scrollbar">
-            <a
-                class="px-4 pb-2 border-b-2 hover:border-green-500"
-                :class="{'border-green-500': tab === '#watching', 'border-gray-300': tab !== '#watching'}"
-                x-on:click="tab = '#watching'"
-                href="#watching"
-                data-toggle="tab"
-            >{{ __('Watching') }}</a>
-
-            <a
-                class="px-4 pb-2 border-b-2 hover:border-orange-500"
-                :class="{'border-orange-500': tab === '#planning', 'border-gray-300': tab !== '#planning'}"
-                href="#planning"
-                data-toggle="tab"
-                x-on:click="tab = '#planning'"
-            >{{ __('Planning') }}</a>
-
-            <a
-                class="px-4 pb-2 border-b-2 hover:border-blue-500"
-                :class="{'border-blue-500': tab === '#completed', 'border-gray-300': tab !== '#completed'}"
-                href="#completed"
-                data-toggle="tab"
-                x-on:click="tab = '#completed'"
-            >{{ __('Completed') }}</a>
-
-            <a
-                class="px-4 pb-2 border-b-2 hover:border-red-500"
-                :class="{'border-red-500': tab === '#onhold', 'border-gray-300': tab !== '#onhold'}"
-                href="#onhold"
-                data-toggle="tab"
-                x-on:click="tab = '#onhold'"
-            >{{ __('On-Hold') }}</a>
-
-            <a
-                class="px-4 pb-2 border-b-2 hover:border-gray-700"
-                :class="{'border-gray-500': tab === '#dropped', 'border-gray-300': tab !== '#dropped'}"
-                href="#dropped"
-                data-toggle="tab"
-                x-on:click="tab = '#dropped'"
-            >{{ __('Dropped') }}</a>
+            @foreach(\App\Enums\UserLibraryStatus::asSelectArray() as $key => $value)
+                <button
+                    class="px-4 pb-2 border-b-2 hover:border-orange-500"
+                    :class="{'border-orange-500': '{{ strtolower($status) }}' === '{{ strtolower($value) }}', 'border-gray-300': '{{ strtolower($status) }}' !== '{{ strtolower($value) }}'}"
+                    wire:click="$set('status', '{{ strtolower($value) }}')"
+                    data-toggle="tab"
+                >{{ __($value) }}</button>
+            @endforeach
         </div>
 
         <section class="mt-8">
-            <livewire:library.tab id="watching" :user-library-status="\App\Enums\UserLibraryStatus::Watching()" wire:key="{{ md5('watching') }}" />
-
-            <livewire:library.tab id="planning" :user-library-status="\App\Enums\UserLibraryStatus::Planning()" wire:key="{{ md5('planning') }}" />
-
-            <livewire:library.tab id="completed" :user-library-status="\App\Enums\UserLibraryStatus::Completed()" wire:key="{{ md5('completed') }}" />
-
-            <livewire:library.tab id="onhold" :user-library-status="\App\Enums\UserLibraryStatus::OnHold()" wire:key="{{ md5('onhold') }}" />
-
-            <livewire:library.tab id="dropped" :user-library-status="\App\Enums\UserLibraryStatus::Dropped()" wire:key="{{ md5('dropped') }}" />
+            @foreach(\App\Enums\UserLibraryStatus::asSelectArray() as $key => $value)
+                <livewire:library.tab id="{{ $value }}" :status="$value" wire:key="{{ md5($value) }}" />
+            @endforeach
         </section>
     </div>
 </main>
