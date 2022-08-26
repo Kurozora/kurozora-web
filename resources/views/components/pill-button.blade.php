04c8673db9fd8ecf0c8d471f07b37112c5a98830
kiritokatklian <casillaskhoren1@gmail.com>
2022-08-27 01:54:38 +0200

    [Update] Watch Button - Added pill-button component - Added tinted-pill-button component - Updated episode-lockup to use circle watch button - Updated watch-button Livewire component to support different styles

---
+++ resources/views/components/pill-button.blade.php
@@ -0,0 +1,12 @@
+@props(['color' => null, 'disabled' => false])
+
+@php
+    $colorCSS = match ($color) {
+        'orange' => 'text-orange-500 hover:text-orange-400 active:text-orange-500',
+        default => '',
+    }
+@endphp
+
+<button {{ $attributes->merge(['class' => 'inline-flex items-center justify-center gap-1 px-2 py-1 h-8 bg-white border border-transparent rounded-full text-xs font-semibold uppercase tracking-widest shadow-md transition ease-in-out duration-150 hover:bg-gray-100 active:bg-white focus:outline-none active:border-orange-600 active:ring-orange disabled:bg-gray-200 disabled:border-gray-300 disabled:text-gray-400 disabled:cursor-default sm:h-10 ' . $colorCSS ]) }} {{ $disabled ? 'disabled' : '' }}>
+    {{ $slot }}
+</button>
---
+++ resources/views/components/tinted-pill-button.blade.php
@@ -0,0 +1,12 @@
+@props(['color' => null, 'disabled' => false])
+
+@php
+    $colorCSS = match ($color) {
+        'orange' => 'bg-orange-500 text-white hover:bg-orange-400 active:bg-orange-500',
+        default => 'bg-white hover:bg-gray-100 active:bg-white',
+    }
+@endphp
+
+<button {{ $attributes->merge(['class' => 'inline-flex items-center justify-center gap-1 px-2 py-1 h-8 border border-transparent rounded-full text-xs font-semibold uppercase tracking-widest shadow-md transition ease-in-out duration-150  focus:outline-none active:border-orange-600 active:ring-orange disabled:bg-gray-200 disabled:border-gray-300 disabled:text-gray-400 disabled:cursor-default sm:h-10 ' . $colorCSS ]) }} {{ $disabled ? 'disabled' : '' }}>
+    {{ $slot }}
+</button>
