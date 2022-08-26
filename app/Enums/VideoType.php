d19ff4ab232cdfaa5672e14e009473673090d0bc
kiritokatklian <casillaskhoren1@gmail.com>
2022-08-27 01:57:47 +0200

    [Add] Embeds - Added app/Embed - Added DefaultEmbed class and component - Added DoodStreamEmbed class and component - Added FembedEmbed class and component - Added FileMoonEmbed class and component - Added HydraxEmbed class and component - Added MixDropEmbed class and component - Added Mp4UploadEmbed class and component - Added StreamTapeEmbed class and component - Added YouTubeEmbed class and component - Added VideoSource enum - Added VideoType enum

---
+++ app/Enums/VideoType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static VideoType Default()
+ * @method static VideoType CommercialMessage()
+ * @method static VideoType PromotionalVideo()
+ * @method static VideoType Teaser()
+ * @method static VideoType Trailer()
+ */
+final class VideoType extends Enum
+{
+    const Default           = 0;
+    const CommercialMessage = 1;
+    const PromotionalVideo  = 2;
+    const Teaser            = 3;
+    const Trailer           = 4;
+}
---
+++ resources/views/embeds/default.blade.php
@@ -0,0 +1,37 @@
+<div
+    style="all: inherit;"
+    x-data="{
+        player: null,
+        initPlayer() {
+            this.player = new PlyrManager('#videoPlayerContainer', {
+                mediaMetadata: {
+                    title: @js($title),
+                    artist: @js($artist),
+                    album: @js($album),
+                    artwork: @js($artworks)
+                },
+                poster: @js($poster),
+                urls: {
+                    download: @js($url)
+                },
+                youtube: {
+                    origin: '{{ config('app.url') }}'
+                }
+            })
+        }
+    }"
+    x-init="initPlayer()"
+>
+    <video
+        id="videoPlayerContainer"
+        class="m-auto aspect-video"
+        player-src="{{ $url }}"
+        width="1920"
+        height="1080"
+        autoplay="autoplay"
+        controls="controls"
+        crossorigin=""
+        poster="{{ $poster }}"
+    >
+    </video>
+</div>
