4a86cccec4019bcdef0b8a25247de75937000594
kiritokatklian <casillaskhoren1@gmail.com>
2022-09-01 23:17:57 +0200

    [Update] Modal Component - Replaced wire->('model') with get->('model') to fix a bug where any change in input tags was assigned to the wire:model attribute. - Updated focusable method to ignore hidden inputs. - Updated components that made use of modal to use `model` attribute instead of `wire:model`

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -14,10 +14,10 @@
 
 <div
     x-data="{
-        show: @entangle($attributes->wire('model')),
+        show: @entangle($attributes->get('model')),
         focusables() {
             // All focusable element types...
-            let selector = 'a, button, input, textarea, select, details, [tabindex]:not([tabindex=\'-1\'])'
+            let selector = 'a, button, input:not([type=\'hidden\']), textarea, select, details, [tabindex]:not([tabindex=\'-1\'])'
 
             return [...$el.querySelectorAll(selector)]
                 // All non-disabled elements...
@@ -30,6 +30,14 @@
         nextFocusableIndex() { return (this.focusables().indexOf(document.activeElement) + 1) % (this.focusables().length + 1) },
         prevFocusableIndex() { return Math.max(0, this.focusables().indexOf(document.activeElement)) -1 },
     }"
+    x-init="$watch('show', value => {
+        if (value) {
+            document.body.classList.add('overflow-y-hidden');
+            {{ $attributes->has('focusable') ? 'setTimeout(() => firstFocusable().focus(), 100)' : '' }}
+        } else {
+            document.body.classList.remove('overflow-y-hidden');
+        }
+    })"
     x-on:close.stop="show = false"
     x-on:keydown.escape.window="show = false"
     x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
@@ -53,13 +61,16 @@
         <div class="absolute inset-0 bg-black opacity-75"></div>
     </div>
 
-    <div x-show="show" class="max-h-[calc(100vh-3rem)] bg-white rounded-lg overflow-hidden shadow-xl transform transition-all overflow-y-scroll sm:w-full {{ $maxWidth }}"
-         x-transition:enter="ease-out duration-300"
-         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-         x-transition:leave="ease-in duration-200"
-         x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-         x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+    <div
+        class="max-h-[calc(100vh-3rem)] bg-white rounded-lg overflow-hidden shadow-xl transform transition-all overflow-y-scroll sm:w-full {{ $maxWidth }} sm:mx-auto"
+        x-show="show"
+        x-transition:enter="ease-out duration-300"
+        x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+        x-transition:leave="ease-in duration-200"
+        x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+        x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    >
         {{ $slot }}
     </div>
 </div>
