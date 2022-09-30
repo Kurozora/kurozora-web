8b8367eeb4b3371272a1f36f7d13e07936837ee3
kiritokatklian <casillaskhoren1@gmail.com>
2022-10-01 01:35:21 +0200

    [Add] Filler Episode Scraper - Added Filler console command - Added FillerSpider - Added FillerProcessor - Added ScrapeFiller Nova action - Updated Anime migration - Updated Anime Nova resource - Updated scraper config

---
+++ app/Processors/AnimeFillerList/FillerProcessor.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Processors\AnimeFillerList;
+
+use App\Models\Anime;
+use RoachPHP\ItemPipeline\ItemInterface;
+use RoachPHP\ItemPipeline\Processors\ItemProcessorInterface;
+use RoachPHP\Support\Configurable;
+
+class FillerProcessor implements ItemProcessorInterface
+{
+    use Configurable;
+
+    /**
+     * The current item.
+     *
+     * @var ItemInterface|null
+     */
+    private ?ItemInterface $item = null;
+
+    public function processItem(ItemInterface $item): ItemInterface
+    {
+        $this->item = $item;
+        $fillerID = $item->get('filler_id');
+        $episodeNumber = $item->get('episode_number');
+        $isFiller = $this->getIsFiller($item->get('filler_type'));
+
+        logger()->channel('stderr')->info(($isFiller ? 'true: ' : 'false: ') . $item->get('filler_type') . ' episode: ' . $episodeNumber);
+        logger()->channel('stderr')->info('🔄 [filler_id:' . $fillerID . '] Processing filler status');
+
+        $anime = Anime::withoutGlobalScopes()
+            ->firstWhere('filler_id', '=', $fillerID);
+
+//        dd([
+//            'filler_id' => $fillerID,
+//            'image_url' => $imageUrl,
+//        ]);
+
+        if (empty($anime)) {
+            logger()->channel('stderr')->info('⚠️ [filler_id:' . $fillerID . '] Anime not found');
+        } else {
+            $episode = $anime->episodes()
+                ->withoutGlobalScopes()
+                ->firstWhere('number_total', '=', $episodeNumber);
+
+            if (empty($episode)) {
+                logger()->channel('stderr')->info('⚠️ [filler_id:' . $fillerID . '] Episode `' . $episodeNumber . '` not found');
+            } else {
+                logger()->channel('stderr')->info('🛠️ [filler_id:' . $fillerID . '] Updating episode `' . $episodeNumber . '` filler status');
+                $episode->update([
+                    'is_filler' => $isFiller,
+                ]);
+                logger()->channel('stderr')->info('✅️ [filler_id:' . $fillerID . '] Done updating episode `' . $episodeNumber . '` filler status');
+            }
+        }
+
+        logger()->channel('stderr')->info('✅️ [filler_id:' . $fillerID . '] Done processing `' . $episodeNumber . '` filler status');
+        return $item;
+    }
+
+    /**
+     * Determines whether the episode is a filler.
+     *
+     * @param string $fillerType
+     * @return bool
+     */
+    private function getIsFiller(string $fillerType): bool
+    {
+        return str($fillerType)->lower()->contains('filler');
+    }
+}
---
+++ app/Spiders/AnimeFillerList/FillerSpider.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Spiders\AnimeFillerList;
+
+use App\Processors\AnimeFillerList\FillerProcessor;
+use Generator;
+use RoachPHP\Downloader\Middleware\RequestDeduplicationMiddleware;
+use RoachPHP\Downloader\Middleware\UserAgentMiddleware;
+use RoachPHP\Extensions\LoggerExtension;
+use RoachPHP\Extensions\StatsCollectorExtension;
+use RoachPHP\Http\Response;
+use RoachPHP\Spider\BasicSpider;
+use RoachPHP\Spider\ParseResult;
+use Symfony\Component\DomCrawler\Crawler;
+
+class FillerSpider extends BasicSpider
+{
+    /**
+     * @var array $startUrls
+     */
+    public array $startUrls = [
+        //
+        'https://www.animefillerlist.com/shows/one-piece'
+    ];
+
+    /**
+     * The downloader middleware that should be used for runs of this spider.
+     *
+     * @var array|string[] $downloaderMiddleware
+     */
+    public array $downloaderMiddleware = [
+        RequestDeduplicationMiddleware::class,
+        [
+            UserAgentMiddleware::class,
+            ['userAgent' => 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'],
+        ],
+    ];
+
+    /**
+     * The spider middleware that should be used for runs of this spider.
+     *
+     * @var array $spiderMiddleware
+     */
+    public array $spiderMiddleware = [
+        //
+    ];
+
+    /**
+     * The item processors that emitted items will be sent through.
+     *
+     * @var array $itemProcessors
+     */
+    public array $itemProcessors = [
+        FillerProcessor::class
+    ];
+
+    /**
+     * The extensions that should be used for runs of this spider.
+     *
+     * @var array|string[] $extensions
+     */
+    public array $extensions = [
+        LoggerExtension::class,
+        StatsCollectorExtension::class,
+    ];
+
+    /**
+     * How many requests are allowed to be sent concurrently.
+     *
+     * @var int $concurrency
+     */
+    public int $concurrency = 2;
+
+    /**
+     * The delay (in seconds) between requests. Note that there
+     * is no delay between concurrent requests. Instead, Roach
+     * will wait for the `$requestDelay` before sending the
+     * next "batch" of concurrent requests.
+     *
+     * @var int $requestDelay
+     */
+    public int $requestDelay = 1;
+
+    /**
+     * @param Response $response
+     *
+     * @return Generator<ParseResult>
+     */
+    public function parse(Response $response): Generator
+    {
+        $fillerID = trim(basename($response->getUri()));
+        logger()->channel('stderr')->info('🕷 [filler_id:' . $fillerID . '] Parsing response');
+
+        $episodeList = $response->filter('table.EpisodeList tbody')->filter('tr')
+            ->each(function(Crawler $item) {
+                $episode = [];
+                $episode['number'] = $item->filter('td.Number')->text();
+                $episode['type'] = $item->filter('td.Type')->text();
+                return $episode;
+            });
+
+        foreach ($episodeList as $episode) {
+            yield $this->item([
+                'filler_id' => $fillerID,
+                'episode_number' => $episode['number'],
+                'filler_type' => $episode['type'],
+            ]);
+        }
+
+        logger()->channel('stderr')->info('✅️ [filler_id:' . $fillerID . '] Done parsing');
+    }
+}
