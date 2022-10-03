62d38687f9f425e1d15f1c37a9348f137e2f8c3a
kiritokatklian <casillaskhoren1@gmail.com>
2022-10-03 11:27:26 +0200

    [Update] EB Extension & Platform Hooks - Added service.config - Added 04-restart-worker.sh - Renamed 04-laravel-up.sh to 05-laravel-up.sh - Updated 01-optimize-laravel.sh - Updated cronjob.config

--- .platform/hooks/postdeploy/01-optimize-laravel.sh
+++ .platform/hooks/postdeploy/01-optimize-laravel.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Clear caches
-php artisan cache:clear
-
 # Clear expired password reset tokens
 php artisan auth:clear-resets
-
-# Clear and cache routes
-php artisan route:cache
-
-# Clear and cache events
-php artisan event:cache
 
 # Clear and cache config
 php artisan config:cache
 
 # Clear and cache views
 php artisan view:cache
+
+# Clear and cache routes
+php artisan route:cache
+
+# Clear and cache events
+php artisan event:cache
