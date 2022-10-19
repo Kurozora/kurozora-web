0639910f9e16aab3c5d25d4149897aae1805ee37
kiritokatklian <casillaskhoren1@gmail.com>
2022-10-20 00:54:41 +0200

    [Add] Press-Kit - Added press-kit assets - Added press-kit link in footer - Added press-kit Livewire component - Added press-kit route

--- routes/Web/Misc.php
+++ routes/Web/Misc.php
@@ -2,6 +2,7 @@
 
 use App\Http\Livewire\Misc\Contact;
 use App\Http\Livewire\Misc\Creators;
+use App\Http\Livewire\Misc\PressKit;
 
 Route::name('misc')
     ->group(function() {
@@ -10,4 +11,7 @@ Route::name('misc')
 
         Route::get('/creators', Creators::class)
             ->name('.creators');
+
+        Route::get('/press-kit', PressKit::class)
+            ->name('.press-kit');
     });
