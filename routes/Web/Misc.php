36e30831675f855a653708448e0bf9a2d8eb7e07
kiritokatklian <casillaskhoren1@gmail.com>
2023-01-15 22:20:11 +0100

    [Update] Team Route - Updated team route

--- routes/Web/Misc.php
+++ routes/Web/Misc.php
@@ -1,17 +1,17 @@
 <?php
 
 use App\Http\Livewire\Misc\Contact;
-use App\Http\Livewire\Misc\Creators;
 use App\Http\Livewire\Misc\PressKit;
+use App\Http\Livewire\Misc\Team;
 
 Route::name('misc')
     ->group(function() {
         Route::get('/contact', Contact::class)
             ->name('.contact');
 
-        Route::get('/creators', Creators::class)
-            ->name('.creators');
-
         Route::get('/press-kit', PressKit::class)
             ->name('.press-kit');
+
+        Route::get('/team', Team::class)
+            ->name('.team');
     });
