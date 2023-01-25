6cad34a24ccd20479e593d68935a65ba2d95456f
kiritokatklian <casillaskhoren1@gmail.com>
2023-01-25 02:07:46 +0100

    [Update] Library - Added Trackable trait - Added Tracker trait

---
+++ app/Traits/Model/Trackable.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Traits\Model;
+
+use App\Models\User;
+use App\Models\UserLibrary;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+trait Trackable
+{
+    /**
+     * Bootstrap the model with Tracking.
+     *
+     * @return void
+     */
+    public static function bootTrackable(): void
+    {
+        static::deleting(function (Model $model) {
+            if (in_array(SoftDeletes::class, class_uses_recursive($model))) {
+                if (!$model->forceDeleting) {
+                    return;
+                }
+            }
+
+            $model->library()->delete();
+        });
+    }
+
+    /**
+     * Get the model's library entries.
+     *
+     * @return MorphMany
+     */
+    function library(): MorphMany
+    {
+        return $this->morphMany(UserLibrary::class, 'trackable');
+    }
+
+    /**
+     * The users who tracked the model.
+     *
+     * @return BelongsToMany
+     */
+    public function trackers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->withTimestamps();
+    }
+
+    /**
+     * Whether the model is tracked by the given user.
+     *
+     * @param User $user
+     * @return bool
+     */
+    public function isTrackedBy(User $user): bool
+    {
+        $trackersLoaded = $this->relationLoaded('trackers');
+
+        if ($trackersLoaded) {
+            return $this->trackers->contains($user);
+        }
+
+        return (
+        $this->relationLoaded('trackers')
+            ? $this->trackers
+            : $this->trackers()
+        )
+            ->where('user_id', '=', $user->id)
+            ->exists();
+    }
+
+    /**
+     * Whether the model is not tracked by the given user.
+     *
+     * @param User $user
+     * @return bool
+     */
+    public function isNotTrackedBy(User $user): bool
+    {
+        return !$this->isTrackedBy($user);
+    }
+
+    /**
+     * The number of users who tracked the model.
+     *
+     * @return int
+     */
+    public function trackersCount(): int
+    {
+        if ($this->trackers_count !== null) {
+            return (int) $this->trackers_count;
+        }
+
+        $this->loadCount('trackers');
+
+        return (int) $this->trackers_count;
+    }
+
+    /**
+     * The formatted number of users who tracked the model.
+     *
+     * @param int $precision
+     * @param bool $abbreviated
+     * @return string
+     */
+    public function trackersCountForHumans(int $precision = 1, bool $abbreviated = false): string
+    {
+        return number_shorten($this->trackersCount(), $precision, $abbreviated);
+    }
+
+    /**
+     * Eloquent builder scope that limits the query to the models tracked by the user.
+     *
+     * @param Builder $query
+     * @param Model $user
+     * @return Builder
+     */
+    public function scopeWhereTrackedBy(Builder $query, Model $user): Builder
+    {
+        return $query->whereHas('trackers', function (Builder $query) use ($user): Builder {
+            return $query->whereKey($user->getKey());
+        });
+    }
+
+    /**
+     * Eloquent builder scope that limits the query to the models not tracked by the user.
+     *
+     * @param Builder $query
+     * @param Model $user
+     * @return Builder
+     */
+    public function scopeWhereNotTrackedBy(Builder $query, Model $user): Builder
+    {
+        return $query->whereDoesntHave('trackers', function (Builder $query) use ($user): Builder {
+            return $query->whereKey($user->getKey());
+        });
+    }
+}
---
+++ app/Traits/Model/Tracker.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Traits\Model;
+
+use App\Enums\UserLibraryStatus;
+use App\Models\UserLibrary;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
+trait Tracker
+{
+    /**
+     * The user's library.
+     *
+     * @return HasMany
+     */
+    public function library(): HasMany
+    {
+        return $this->hasMany(UserLibrary::class);
+    }
+
+    /**
+     * The models tracked by the user.
+     *
+     * @param string $type
+     * @return MorphToMany
+     */
+    protected function trackedModels(string $type): MorphToMany
+    {
+        return $this->morphedByMany($type, 'trackable', UserLibrary::TABLE_NAME)
+            ->withTimestamps();
+    }
+
+    /**
+     * Whether the user has tracked the given model.
+     *
+     * @param Model $model
+     * @return bool
+     */
+    public function hasTracked(Model $model): bool
+    {
+        return ($this->relationLoaded('library') ? $this->library : $this->library())
+            ->where('trackable_id', $model->getKey())
+            ->where('trackable_type', $model->getMorphClass())
+            ->exists();
+    }
+
+    /**
+     * Whether the user has not tracked the given model.
+     *
+     * @param Model $model
+     * @return bool
+     */
+    public function hasNotTracked(Model $model): bool
+    {
+        return !$this->hasTracked($model);
+    }
+
+    /**
+     * Track the given model.
+     *
+     * @param Model $model
+     * @param UserLibraryStatus $status
+     * @return UserLibrary
+     */
+    public function track(Model $model, UserLibraryStatus $status): UserLibrary
+    {
+        $attributes = [
+            'trackable_id' => $model->getKey(),
+            'trackable_type' => $model->getMorphClass(),
+        ];
+
+        return $this->library()
+            ->where($attributes)
+            ->firstOr(function () use ($status, $attributes) {
+                $libraryLoaded = $this->relationLoaded('library');
+
+                if ($libraryLoaded) {
+                    $this->unsetRelation('library');
+                }
+
+                $attributes['status'] = $status->value;
+                return $this->library()
+                    ->create($attributes);
+            });
+    }
+
+    /**
+     * Un-track the given model
+     *
+     * @param Model $model
+     * @return bool
+     */
+    public function untrack(Model $model): bool
+    {
+        $hasNotTracked = $this->hasNotTracked($model);
+
+        if ($hasNotTracked) {
+            return true;
+        }
+
+        $libraryLoaded = $this->relationLoaded('library');
+        if ($libraryLoaded) {
+            $this->unsetRelation('library');
+        }
+
+        return (bool)$this->trackedModels(get_class($model))
+            ->detach($model->getKey());
+    }
+
+    /**
+     * Clears the library of the given type.
+     *
+     * @param string|null $type
+     * @return bool
+     */
+    public function clearLibrary(?string $type = null): bool
+    {
+        return $this->library()
+            ->where('trackable_type', '=', $type)
+            ->delete();
+    }
+
+    /**
+     * Toggle tracking status of the given model.
+     *
+     * @param Model $model
+     * @return UserLibrary|bool
+     */
+    public function toggleTracking(Model $model): bool|UserLibrary
+    {
+        return $this->hasTracked($model)
+            ? $this->untrack($model)
+            : $this->track($model, UserLibraryStatus::InProgress());
+    }
+
+    /**
+     * Eloquent builder scope that limits the query to the models of the specified type.
+     *
+     * @param string $type
+     * @return BelongsToMany
+     */
+    public function whereTracked(string $type): BelongsToMany
+    {
+        return $this->belongsToMany($type, UserLibrary::class, 'user_id', 'trackable_id')
+            ->where('trackable_type', '=', $type)
+            ->withPivot('status') // Needed for get library API
+            ->withTimestamps();
+    }
+}
