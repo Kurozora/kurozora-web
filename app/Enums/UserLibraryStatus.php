347167e4934c33a40222e635783f3bcca9bc1d62
kiritokatklian <casillaskhoren1@gmail.com>
2023-01-25 02:14:10 +0100

    [Update] UserLibraryStatus - Added support to convert Watching and Reading keys to InProgress - Renamed Watching to InProgress

--- app/Enums/UserLibraryStatus.php
+++ app/Enums/UserLibraryStatus.php
@@ -3,9 +3,11 @@
 namespace App\Enums;
 
 use BenSampo\Enum\Enum;
+use BenSampo\Enum\Exceptions\InvalidEnumKeyException;
+use BenSampo\Enum\Exceptions\InvalidEnumMemberException;
 
 /**
- * @method static UserLibraryStatus Watching()
+ * @method static UserLibraryStatus InProgress()
  * @method static UserLibraryStatus Planning()
  * @method static UserLibraryStatus Completed()
  * @method static UserLibraryStatus OnHold()
@@ -13,11 +15,44 @@ use BenSampo\Enum\Enum;
  */
 final class UserLibraryStatus extends Enum
 {
-    const Watching  = 0;
-    const Planning  = 2;
-    const Completed = 3;
-    const OnHold    = 4;
-    const Dropped   = 1;
+    const InProgress    = 0;
+    const Planning      = 2;
+    const Completed     = 3;
+    const OnHold        = 4;
+    const Dropped       = 1;
+
+    /**
+     * Make an enum instance from a given key.
+     *
+     * @throws InvalidEnumKeyException
+     * @throws InvalidEnumMemberException
+     */
+    public static function fromKey(string $key): static
+    {
+        if (strtolower($key) == 'watching' || strtolower($key) == 'reading') {
+            return UserLibraryStatus::InProgress();
+        }
+
+        if (UserLibraryStatus::hasKey($key)) {
+            $enumValue = UserLibraryStatus::getValue($key);
+
+            return new UserLibraryStatus($enumValue);
+        }
+
+        throw new InvalidEnumKeyException($key, UserLibraryStatus::class);
+    }
+
+    /**
+     * Check that the enum contains a specific key.
+     */
+    public static function hasKey(string $key): bool
+    {
+        if (strtolower($key) == 'watching' || strtolower($key) == 'reading') {
+            return true;
+        }
+
+        return in_array($key, UserLibraryStatus::getKeys(), true);
+    }
 
     /**
      * Returns the description of the status
@@ -25,23 +60,61 @@ final class UserLibraryStatus extends Enum
      * @param int|string $value
      * @return string
      */
-    public static function getDescription(mixed $value): string
+    public static function getAnimeDescription(mixed $value): string
     {
         return match ((int) $value) {
+            self::InProgress => 'Watching',
             self::OnHold => 'On-Hold',
             default => parent::getDescription((int) $value),
         };
     }
 
     /**
-     * Returns an error displaying the valid library statuses
+     * Returns the description of the status
      *
+     * @param int|string $value
      * @return string
      */
-    static function error(): string
+    public static function getMangaDescription(mixed $value): string
     {
-        return 'Pick a valid library status: ' .
-            implode(', ', self::getKeys())
-        ;
+        return match ((int) $value) {
+            self::InProgress => 'Reading',
+            self::OnHold => 'On-Hold',
+            default => parent::getDescription((int) $value),
+        };
+    }
+
+    /**
+     * Get the enum as an array formatted for a select.
+     *
+     * @return array<array-key, string>
+     */
+    public static function asAnimeSelectArray(): array
+    {
+        $array = UserLibraryStatus::asArray();
+        $selectArray = [];
+
+        foreach ($array as $value) {
+            $selectArray[$value] = UserLibraryStatus::getAnimeDescription($value);
+        }
+
+        return $selectArray;
+    }
+
+    /**
+     * Get the enum as an array formatted for a select.
+     *
+     * @return array<array-key, string>
+     */
+    public static function asMangaSelectArray(): array
+    {
+        $array = UserLibraryStatus::asArray();
+        $selectArray = [];
+
+        foreach ($array as $value) {
+            $selectArray[$value] = UserLibraryStatus::getMangaDescription($value);
+        }
+
+        return $selectArray;
     }
 }
