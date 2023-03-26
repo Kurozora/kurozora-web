a7341d9db5dff4de85f5b611543772d68ffadd15
kiritokatklian <casillaskhoren1@gmail.com>
2023-03-26 03:28:46 +0200

    [Update] Search - Added OpenSearch case to SearchSource enum - Updated opensearch.xml

--- app/Enums/SearchSource.php
+++ app/Enums/SearchSource.php
@@ -7,9 +7,11 @@ use BenSampo\Enum\Enum;
 /**
  * @method static SearchSource Kurozora()
  * @method static SearchSource Google()
+ * @method static SearchSource OpenSearch()
  */
 final class SearchSource extends Enum
 {
     const Kurozora = 'gs_kurozora';
     const Google = 'mc_google';
+    const OpenSearch = 'opensearch';
 }
