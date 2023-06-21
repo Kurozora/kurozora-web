40b6613107b076c65e58703c88d857e9076d3c69
kiritokatklian <casillaskhoren1@gmail.com>
2023-06-21 02:25:49 +0200

    [Update] Composer - Moved Nova packages to suggest - Updated packages

--- app/Http/Controllers/Web/Nova/SignInController.php
+++ app/Http/Controllers/Web/Nova/SignInController.php
@@ -13,47 +13,49 @@ use Illuminate\Validation\ValidationException;
 use Laravel\Nova\Http\Controllers\LoginController;
 use Symfony\Component\HttpFoundation\Response;
 
-class SignInController extends LoginController
-{
-    /**
-     * Handle a sign in request to the application.
-     *
-     * @param Request $request
-     * @return JsonResponse|Response|RedirectResponse
-     *
-     * @throws ValidationException
-     */
-    public function login(Request $request): JsonResponse|Response|RedirectResponse
+if (class_exists('Laravel\Nova\Http\Controllers\LoginController')) {
+    class SignInController extends LoginController
     {
-        $this->validateLogin($request);
+        /**
+         * Handle a sign in request to the application.
+         *
+         * @param Request $request
+         * @return JsonResponse|Response|RedirectResponse
+         *
+         * @throws ValidationException
+         */
+        public function login(Request $request): JsonResponse|Response|RedirectResponse
+        {
+            $this->validateLogin($request);
 
-        // If the class is using the ThrottlesLogins trait, we can automatically throttle
-        // the sign in attempts for this application. We'll key this by the username and
-        // the IP address of the client making these requests into this application.
-        if (method_exists($this, 'hasTooManyLoginAttempts') &&
-            $this->hasTooManyLoginAttempts($request)) {
-            $this->fireLockoutEvent($request);
+            // If the class is using the ThrottlesLogins trait, we can automatically throttle
+            // the sign in attempts for this application. We'll key this by the username and
+            // the IP address of the client making these requests into this application.
+            if (method_exists($this, 'hasTooManyLoginAttempts') &&
+                $this->hasTooManyLoginAttempts($request)) {
+                $this->fireLockoutEvent($request);
 
-            return $this->sendLockoutResponse($request);
+                return $this->sendLockoutResponse($request);
+            }
+
+            return $this->signInPipeline($request)->then(function () {
+                return redirect()->intended($this->redirectPath());
+            });
         }
 
-        return $this->signInPipeline($request)->then(function () {
-            return redirect()->intended($this->redirectPath());
-        });
-    }
-
-    /**
-     * Get the authentication pipeline instance.
-     *
-     * @param Request $request
-     * @return Pipeline
-     */
-    protected function signInPipeline(Request $request): Pipeline
-    {
-        return (new Pipeline(app()))->send($request)->through(array_filter([
-            RedirectIfTwoFactorAuthenticatable::class,
-            AttemptToAuthenticate::class,
-            PrepareAuthenticatedSession::class,
-        ]));
+        /**
+         * Get the authentication pipeline instance.
+         *
+         * @param Request $request
+         * @return Pipeline
+         */
+        protected function signInPipeline(Request $request): Pipeline
+        {
+            return (new Pipeline(app()))->send($request)->through(array_filter([
+                RedirectIfTwoFactorAuthenticatable::class,
+                AttemptToAuthenticate::class,
+                PrepareAuthenticatedSession::class,
+            ]));
+        }
     }
 }
