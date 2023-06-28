abe3b4ed29c12f0479480a88e50834700d8fbcd2
kiritokatklian <casillaskhoren1@gmail.com>
2023-06-28 23:56:20 +0200

    [Add] Tooltip - Added tooltip component

---
+++ resources/views/components/tooltip.blade.php
@@ -0,0 +1,18 @@
+@props(['id'])
+
+@php
+    $id = $id ?? md5($attributes->wire('model'));
+@endphp
+
+<div
+    {{ $attributes->merge(['class' => 'absolute']) }}
+    id="{{ $id }}"
+    x-cloak=""
+    x-transition:enter="ease-out duration-300"
+    x-transition:enter-start="opacity-0"
+    x-transition:enter-end="opacity-100"
+>
+    <div class="flex gap-1 pl-2 pt-2 pb-2 pr-2 w-64 bg-white border border-black/5 shadow-sm rounded-md">
+        {{ $slot }}
+    </div>
+</div>
