71be839b626e24b41be6d3113901c053bb45cdc5
kiritokatklian <casillaskhoren1@gmail.com>
2023-07-09 17:55:37 +0200

    [Update] Rating Calculation - Fixed issue with performance - Fixed issue with wrong calculation of half point ratings

--- app/Console/Commands/Calculators/CalculateRatings.php
+++ app/Console/Commands/Calculators/CalculateRatings.php
@@ -69,15 +69,17 @@ class CalculateRatings extends Command
                 $newMediaStats = [];
 
                 foreach ($chunk as $modelID) {
+                    /** @var ?MediaStat $mediaStat */
                     $mediaStat = $existingMediaStats->get($modelID);
+                    $isNew = false;
 
                     // If MediaStat doesn't exist for this Model, create a new one.
                     if (!$mediaStat) {
-                        $mediaStat = new MediaStat([
+                        $mediaStat = (new MediaStat([
                             'model_type' => $class,
                             'model_id' => $modelID,
-                        ]);
-                        $newMediaStats[$modelID] = $mediaStat;
+                        ]));
+                        $isNew = true;
                     }
 
                     // Get all current Model records from MediaRating
@@ -94,37 +96,48 @@ class CalculateRatings extends Command
                         $weightedRating = ($totalRatingCount / ($totalRatingCount + $class::MINIMUM_RATINGS_REQUIRED)) * $basicAverageRating + ($class::MINIMUM_RATINGS_REQUIRED / ($totalRatingCount + $class::MINIMUM_RATINGS_REQUIRED)) * $meanRating;
 
                         // Get count of ratings from 0.5 to 5.0
-                        $ratingCounts = $mediaRatingForModel->whereIn('rating', [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0])
-                            ->pluck('total', 'rating')
+                        $ratingCounts = $mediaRatingForModel->whereIn('rating', ['0.5', '1.0', '1.5', '2.0', '2.5', '3.0', '3.5', '4.0', '4.5', '5.0'])
+                            ->mapWithKeys(function ($rating) {
+                                // Convert 0.5...5 to 1...10
+                                return [($rating->rating * 2) => $rating->total];
+                            })
                             ->toArray();
 
                         // Update media stat
-                        $updates[$modelID] = [
-                            'rating_1' => $ratingCounts[0.5] ?? 0,
-                            'rating_2' => $ratingCounts[1.0] ?? 0,
-                            'rating_3' => $ratingCounts[1.5] ?? 0,
-                            'rating_4' => $ratingCounts[2.0] ?? 0,
-                            'rating_5' => $ratingCounts[2.5] ?? 0,
-                            'rating_6' => $ratingCounts[3.0] ?? 0,
-                            'rating_7' => $ratingCounts[3.5] ?? 0,
-                            'rating_8' => $ratingCounts[4.0] ?? 0,
-                            'rating_9' => $ratingCounts[4.5] ?? 0,
-                            'rating_10' => $ratingCounts[5.0] ?? 0,
+                        $attributes = [
+                            'model_id' => $modelID,
+                            'model_type' => $class,
+                            'rating_1' => $ratingCounts[1] ?? 0,
+                            'rating_2' => $ratingCounts[2] ?? 0,
+                            'rating_3' => $ratingCounts[3] ?? 0,
+                            'rating_4' => $ratingCounts[4] ?? 0,
+                            'rating_5' => $ratingCounts[5] ?? 0,
+                            'rating_6' => $ratingCounts[6] ?? 0,
+                            'rating_7' => $ratingCounts[7] ?? 0,
+                            'rating_8' => $ratingCounts[8] ?? 0,
+                            'rating_9' => $ratingCounts[9] ?? 0,
+                            'rating_10' => $ratingCounts[10] ?? 0,
                             'rating_average' => $weightedRating,
                             'rating_count' => $totalRatingCount,
                         ];
+
+                        if ($isNew) {
+                            $mediaStat->fill($attributes);
+                            $newMediaStats[$modelID] = $mediaStat->toArray();
+                        } else {
+                            $updates[] = $attributes;
+                        }
                     }
                 }
 
                 // Create MediaStat for the Model
                 if (!empty($newMediaStats)) {
-                    MediaStat::insert($newMediaStats);
+                    MediaStat::upsert($newMediaStats, ['model_type', 'model_id']);
                 }
 
                 // Update the MediaStat of the Model
                 if (!empty($updates)) {
-                    MediaStat::whereIn('model_id', array_keys($updates))
-                        ->update($updates);
+                    MediaStat::upsert($updates, ['model_type', 'model_id']);
                 }
             });
 
