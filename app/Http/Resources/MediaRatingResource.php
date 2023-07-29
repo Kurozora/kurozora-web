487123fda2de0d4ff419ca96038b87b2e5c97bf5
kiritokatklian <casillaskhoren1@gmail.com>
2023-07-29 20:04:37 +0200

    [Add] Media Rating Resource - Added MediaRatingResource API resource

---
+++ app/Http/Resources/MediaRatingResource.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\MediaRating;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MediaRatingResource extends JsonResource
+{
+    /**
+     * The resource instance.
+     *
+     * @var MediaRating $resource
+     */
+    public $resource;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray(Request $request): array
+    {
+        $resource = [
+            'rating' => $this->resource->rating,
+            'description' => $this->resource->description,
+            'createdAt' => $this->resource->created_at->timestamp
+        ];
+
+        // Add relationships
+        $relationships = [];
+        $relationships = array_merge($relationships, $this->getUserDetails());
+
+        return array_merge($resource, ['relationships' => $relationships]);
+    }
+
+    /**
+     * Get the user details belonging to the feed message.
+     *
+     * @return array
+     */
+    private function getUserDetails(): array
+    {
+        $mediaRating = $this->resource;
+
+        return [
+            'users' => [
+                'data' => UserResource::collection([$mediaRating->user]),
+            ]
+        ];
+    }
+}
