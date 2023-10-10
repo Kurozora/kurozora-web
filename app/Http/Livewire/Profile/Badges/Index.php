825a897b90bac7414aedf71b5e72457b451367c2
kiritokatklian <casillaskhoren1@gmail.com>
2023-10-10 19:27:44 +0200

    [Add] Eager Load User Badges - Added eager loading to user badges

--- app/Http/Livewire/Profile/Badges/Index.php
+++ app/Http/Livewire/Profile/Badges/Index.php
@@ -3,14 +3,18 @@
 namespace App\Http\Livewire\Profile\Badges;
 
 use App\Models\User;
+use App\Models\UserBadge;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\CursorPaginator;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
     /**
      * The object containing the user data.
      *
@@ -19,25 +23,58 @@ class Index extends Component
     public User $user;
 
     /**
+     * The current page query parameter's alias.
+     *
+     * @var string $bgc
+     */
+    public string $bgc = '';
+
+    /**
+     * The current page query parameter.
+     *
+     * @var string $cursor
+     */
+    public string $cursor = '';
+
+    /**
+     * The query strings of the component.
+     *
+     * @var string[] $queryString
+     */
+    protected $queryString = [
+        'cursor' => ['except' => '', 'as' => 'bgc']
+    ];
+
+    /**
      * Prepare the component.
      *
      * @param User $user
      *
      * @return void
      */
-    public function mount(User $user)
+    public function mount(User $user): void
     {
         $this->user = $user;
     }
 
     /**
-     * The user's badges.
+     * The user's badge list.
      *
-     * @return LengthAwarePaginator
+     * @return CursorPaginator
      */
-    public function getBadgesProperty(): LengthAwarePaginator
+    public function getBadgesProperty(): CursorPaginator
     {
-        return $this->user->badges()->paginate(25);
+        // We're aliasing `cursorName` as `bgc`, and setting
+        // query rule to never show `cursor` param when it's
+        // empty. Since `cursor` is also aliased as `bgc` in
+        // query rules, and we always keep it empty, as far
+        // as Livewire is concerned, `bgc` is also empty. So,
+        // `bgc` doesn't show up in the query params in the
+        // browser.
+        return $this->user->badges()
+            ->with(['media'])
+            ->orderBy(UserBadge::TABLE_NAME . '.created_at', 'desc')
+            ->cursorPaginate(25, ['*'], 'bgc');
     }
 
     /**
