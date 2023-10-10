5d9f01ec4a0c57b06608cb396f54c80374aa47e2
kiritokatklian <casillaskhoren1@gmail.com>
2023-10-10 19:32:59 +0200

    [Add] Eager Load Feed Messages - Added eager loading to the feed messages

--- resources/views/components/lockups/feed-message-lockup.blade.php
+++ resources/views/components/lockups/feed-message-lockup.blade.php
@@ -1,16 +1,15 @@
 @props(['feedMessage'])
 
 @php
-    $totalHearts = $feedMessage->viaLoveReactant()->getReactionCounterOfType(\App\Enums\FeedVoteType::Heart()->description);
     $authUser = auth()->user();
-    $isReShared = $authUser && $feedMessage->reShares()->where('user_id', $authUser->id)->exists();
+    $totalHearts = $feedMessage->viaLoveReactant()->getReactionCounterOfType(\App\Enums\FeedVoteType::Heart()->description);
     $isHearted = $authUser && $authUser->getCurrentHeartValueFor($feedMessage) == \App\Enums\FeedVoteType::Heart
 @endphp
 
 <div
     class="relative flex flex-row gap-2 pr-2 pl-2 pt-2 bg-gray-100 rounded-xl"
     x-data="{}"
-    wire:key="{{ uniqid($feedMessage->created_at, true) }}"
+    wire:key="{{ uniqid(more_entropy: true) }}"
 >
     <x-profile-image-view class="w-16 h-16" :user="$feedMessage->user" />
 
@@ -19,7 +18,7 @@
             <div class="flex gap-1 overflow-hidden">
                 <a class="inline-flex items-center text-sm font-semibold whitespace-nowrap overflow-hidden" href="{{ route('profile.details', $feedMessage->user) }}">{{ $feedMessage->user->username }}</a>
 
-                <livewire:components.user.badge-shelf :user="$feedMessage->user" />
+                <livewire:components.user.badge-shelf :user="$feedMessage->user" wire:key="{{ uniqid('badges-', true) }}" />
             </div>
 
             <p class="text-sm text-gray-500 whitespace-nowrap" title="{{ $feedMessage->created_at }}">{{ $feedMessage->created_at->shortAbsoluteDiffForHumans() }}</p>
@@ -31,17 +30,17 @@
             <div class="flex justify-between">
                 <x-square-button>
                     @svg('bubble_left_and_bubble_right_fill', 'fill-current', ['width' => '18'])
-                    <p class="ml-2 mr-2">{{ $feedMessage->replies()->count() }}</p>
+                    <p class="ml-2 mr-2">{{ $feedMessage->replies_count ?: '' }}</p>
                 </x-square-button>
 
-                <x-square-button :class="$isReShared ? 'text-green-500' : ''">
+                <x-square-button :class="$feedMessage->isReShared ? 'text-green-500' : ''">
                     @svg('square_and_arrow_up_on_square_fill', 'fill-current', ['width' => '18'])
-                    <p class="ml-2 mr-2">{{ $feedMessage->reshares()->count() }}</p>
+                    <p class="ml-2 mr-2">{{ $feedMessage->re_shares_count ?: '' }}</p>
                 </x-square-button>
 
                 <x-square-button :class="$isHearted ? 'text-red-500' : ''">
                     @svg('heart_fill', 'fill-current', ['width' => '18'])
-                    <p class="ml-2 mr-2">{{ $totalHearts->getCount() }}</p>
+                    <p class="ml-2 mr-2">{{ $totalHearts->getCount() ?: '' }}</p>
                 </x-square-button>
             </div>
 
