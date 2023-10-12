90dbc7b044fa4ac6fc2417832b50dc9f3b7e2e19
kiritokatklian <casillaskhoren1@gmail.com>
2023-10-12 22:18:22 +0200

    [Add] Eager Load Related Shows - Added eager loading to related shows

--- app/Http/Livewire/Game/RelatedShows.php
+++ app/Http/Livewire/Game/RelatedShows.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -22,6 +23,13 @@ class RelatedShows extends Component
     public Game $game;
 
     /**
+     * Whether the component is ready to load.
+     *
+     * @var bool $readyToLoad
+     */
+    public bool $readyToLoad = false;
+
+    /**
      * Prepare the component.
      *
      * @param Game $game
@@ -30,17 +38,38 @@ class RelatedShows extends Component
      */
     public function mount(Game $game): void
     {
-        $this->game = $game;
+        $this->game = $game->load(['media']);
+    }
+
+    /**
+     * Sets the property to load the page.
+     *
+     * @return void
+     */
+    public function loadPage(): void
+    {
+        $this->readyToLoad = true;
     }
 
     /**
      * The object containing the related anime.
      *
-     * @return LengthAwarePaginator
+     * @return Collection|LengthAwarePaginator
      */
-    public function getAnimeRelationsProperty(): LengthAwarePaginator
+    public function getAnimeRelationsProperty(): Collection|LengthAwarePaginator
     {
-        return $this->game->animeRelations()->paginate(25);
+        if (!$this->readyToLoad) {
+            return collect();
+        }
+
+        return $this->game->animeRelations()
+            ->with([
+                'related' => function ($query) {
+                    $query->with(['genres', 'media', 'mediaStat', 'themes', 'translations', 'tv_rating']);
+                },
+                'relation'
+            ])
+            ->paginate(25);
     }
 
     /**
--- resources/views/livewire/game/related-shows.blade.php
+++ resources/views/livewire/game/related-shows.blade.php
@@ -4,12 +4,12 @@
     </x-slot:title>
 
     <x-slot:description>
-        {{ __('An extensive list of sequel, prequel, side story, spin off, and adaptation to :x only on Kurozora, the largest, free online anime, manga, game & music database in the world.', ['x' => $game->title]) }}
+        {{ __('An extensive list of sequel, prequel, side story, spin off, and adaptations of :x only on Kurozora, the largest, free online anime, manga, game & music database in the world.', ['x' => $game->title]) }}
     </x-slot:description>
 
     <x-slot:meta>
         <meta property="og:title" content="{{ __('Relations') }} | {{ $game->title }} — {{ config('app.name') }}" />
-        <meta property="og:description" content="{{ __('An extensive list of sequel, prequel, side story, spin off, and adaptation to :x only on Kurozora, the largest, free online anime, manga, game & music database in the world.', ['x' => $game->title]) }}" />
+        <meta property="og:description" content="{{ __('An extensive list of sequel, prequel, side story, spin off, and adaptations of :x only on Kurozora, the largest, free online anime, manga, game & music database in the world.', ['x' => $game->title]) }}" />
         <meta property="og:image" content="{{ $game->getFirstMediaFullUrl(\App\Enums\MediaCollection::Poster()) ?? asset('images/static/placeholders/anime_poster.webp') }}" />
         <meta property="og:type" content="video.tv_show" />
         <meta property="video:duration" content="{{ $game->duration }}" />
@@ -21,12 +21,12 @@
         anime/{{ $game->id }}/related-shows
     </x-slot:appArgument>
 
-    <div class="max-w-7xl mx-auto pl-4 pr-4 py-6 sm:px-6">
+    <div class="max-w-7xl mx-auto pl-4 pr-4 py-6 sm:px-6" wire:init="loadPage">
         <section class="mb-4">
             <div>
                 <div class="flex gap-1">
                     <div class="flex flex-wrap items-center w-full">
-                        <h1 class="text-2xl font-bold">{{ __(':x’s Show Adaptations', ['x' => $game->title]) }}</h1>
+                        <h1 class="text-2xl font-bold">{{ __(':x’s Related Anime', ['x' => $game->title]) }}</h1>
                     </div>
 
                     <div class="flex flex-wrap justify-end items-center w-full">
@@ -35,10 +35,28 @@
             </div>
         </section>
 
-        <x-rows.small-lockup :related-animes="$this->animeRelations" :is-row="false" />
+        @if ($readyToLoad)
+            <x-rows.small-lockup :related-animes="$this->animeRelations" :is-row="false" />
 
-        <section class="mt-4">
-            {{ $this->animeRelations->links() }}
-        </section>
+            <section class="mt-4">
+                {{ $this->animeRelations->links() }}
+            </section>
+        @else
+            <section  class="mt-4 pt-5 pb-8 border-t-2">
+                <div class="flex gap-4 justify-between flex-wrap">
+                    <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                    <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                    <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                    <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                    <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                    <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                    <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                    <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                    <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                    <div class="w-64 md:w-80 flex-grow"></div>
+                    <div class="w-64 md:w-80 flex-grow"></div>
+                </div>
+            </section>
+        @endif
     </div>
 </main>
