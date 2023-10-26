142d4284e04a3164054111569f52d3347d7649d1
kiritokatklian <casillaskhoren1@gmail.com>
2023-10-27 01:53:02 +0200

    [Update] Eager Load Me API - Added eager loading to the `me` endpoint - Updated MeController to use logic from other controllers where possible

--- app/Http/Controllers/API/v1/MeController.php
+++ app/Http/Controllers/API/v1/MeController.php
@@ -3,27 +3,14 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\Enums\MediaCollection;
-use App\Enums\UserLibraryKind;
 use App\Helpers\JSONResult;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\GetAccessTokensRequest;
 use App\Http\Requests\GetFeedMessagesRequest;
 use App\Http\Requests\GetFollowersRequest;
 use App\Http\Requests\GetFollowingRequest;
 use App\Http\Requests\GetUserFavoritesRequest;
 use App\Http\Requests\UpdateProfileRequest;
-use App\Http\Resources\AccessTokenResource;
-use App\Http\Resources\AnimeResourceBasic;
-use App\Http\Resources\FeedMessageResource;
-use App\Http\Resources\GameResourceBasic;
-use App\Http\Resources\LiteratureResourceBasic;
-use App\Http\Resources\SessionResource;
 use App\Http\Resources\UserResource;
-use App\Http\Resources\UserResourceBasic;
-use App\Models\Anime;
-use App\Models\Game;
-use App\Models\Manga;
-use App\Models\PersonalAccessToken;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -42,12 +29,30 @@ class MeController extends Controller
     public function me(Request $request): JsonResponse
     {
         // Get authenticated session
-        $bearerToken = $request->bearerToken();
-        $personalAccessToken = PersonalAccessToken::findToken($bearerToken);
+        $personalAccessToken = auth()->user()->currentAccessToken()
+            ->load(['session_attribute']);
+
+        $user = $request->user()
+            ->load([
+                'badges' => function ($query) {
+                    $query->with(['media']);
+                },
+                'media',
+                'sessions' => function ($query) {
+                    $query
+                        ->orderBy('last_activity', 'desc')
+                        ->limit(1);
+                },
+            ])
+            // Since we already have the latest access token, we
+            // simply set the relation here instead of loading
+            // the same relation on the user again.
+            ->setRelation('tokens', collect([$personalAccessToken]));
 
         return JSONResult::success([
             'data' => [
-                UserResource::make($personalAccessToken->user)->includingAccessToken($personalAccessToken)
+                UserResource::make($user)
+                    ->includingAccessToken($personalAccessToken)
             ]
         ]);
     }
@@ -144,31 +149,6 @@ class MeController extends Controller
     }
 
     /**
-     * Returns the current active access tokens for a user
-     *
-     * @param GetAccessTokensRequest $request
-     * @return JsonResponse
-     */
-    public function getAccessTokens(GetAccessTokensRequest $request): JsonResponse
-    {
-        $data = $request->validated();
-
-        // Get the authenticated user
-        $user = auth()->user();
-
-        // Get paginated sessions except current session
-        $tokens = $user->tokens()->paginate($data['limit'] ?? 25);
-
-        // Get next page url minus domain
-        $nextPageURL = str_replace($request->root(), '', $tokens->nextPageUrl());
-
-        return JSONResult::success([
-            'data' => AccessTokenResource::collection($tokens),
-            'next' => empty($nextPageURL) ? null : $nextPageURL
-        ]);
-    }
-
-    /**
      * Returns a list of the authenticated user's favorite anime.
      *
      * @param GetUserFavoritesRequest $request
@@ -176,37 +156,11 @@ class MeController extends Controller
      */
     function getFavorites(GetUserFavoritesRequest $request): JsonResponse
     {
-        $data = $request->validated();
-
         // Get the authenticated user
         $user = auth()->user();
 
-        // Get morph class
-        $morphClass = match ((int) ($data['library'] ?? UserLibraryKind::Anime)) {
-            UserLibraryKind::Manga => Manga::class,
-            UserLibraryKind::Game => Game::class,
-            default => Anime::class,
-        };
-
-        // Paginate the user favorites
-        $userFavorites = $user->whereFavorited($morphClass)
-            ->paginate($data['limit'] ?? 25);
-
-        // Get next page url minus domain
-        $nextPageURL = str_replace($request->root(), '', $userFavorites->nextPageUrl());
-
-        // Get data collection
-        $data = match ((int) ($data['library'] ?? UserLibraryKind::Anime)) {
-            UserLibraryKind::Manga => LiteratureResourceBasic::collection($userFavorites),
-            UserLibraryKind::Game => GameResourceBasic::collection($userFavorites),
-            default => AnimeResourceBasic::collection($userFavorites),
-        };
-
-        // Show successful response
-        return JSONResult::success([
-            'data' => $data,
-            'next' => empty($nextPageURL) ? null : $nextPageURL
-        ]);
+        return (new UserFavoriteController())
+            ->index($request, $user);
     }
 
     /**
@@ -217,23 +171,8 @@ class MeController extends Controller
      */
     function getFeedMessages(GetFeedMessagesRequest $request): JsonResponse
     {
-        $data = $request->validated();
-
-        // Get the authenticated user
-        $user = auth()->user();
-
-        // Get the feed messages
-        $feedMessages = $user->feed_messages()
-            ->orderByDesc('created_at')
-            ->paginate($data['limit'] ?? 25);
-
-        // Get next page url minus domain
-        $nextPageURL = str_replace($request->root(), '', $feedMessages->nextPageUrl());
-
-        return JSONResult::success([
-            'data' => FeedMessageResource::collection($feedMessages),
-            'next' => empty($nextPageURL) ? null : $nextPageURL
-        ]);
+        return (new UserController())
+            ->getFeedMessages($request, auth()->user());
     }
 
     /**
@@ -244,21 +183,8 @@ class MeController extends Controller
      */
     function getFollowers(GetFollowersRequest $request): JsonResponse
     {
-        $data = $request->validated();
-
-        // Get the authenticated user
-        $user = auth()->user();
-
-        // Get the followers
-        $followers = $user->followers()->paginate($data['limit'] ?? 25);
-
-        // Get next page url minus domain
-        $nextPageURL = str_replace($request->root(), '', $followers->nextPageUrl());
-
-        return JSONResult::success([
-            'data' => UserResourceBasic::collection($followers),
-            'next' => empty($nextPageURL) ? null : $nextPageURL
-        ]);
+        return (new FollowingController())
+            ->getFollowers($request, auth()->user());
     }
 
     /**
@@ -269,45 +195,7 @@ class MeController extends Controller
      */
     function getFollowing(GetFollowingRequest $request): JsonResponse
     {
-        $data = $request->validated();
-
-        // Get the authenticated user
-        $user = auth()->user();
-
-        // Get the following
-        $following = $user->following()->paginate($data['limit'] ?? 25);
-
-        // Get next page url minus domain
-        $nextPageURL = str_replace($request->root(), '', $following->nextPageUrl());
-
-        return JSONResult::success([
-            'data' => UserResourceBasic::collection($following),
-            'next' => empty($nextPageURL) ? null : $nextPageURL
-        ]);
-    }
-
-    /**
-     * Returns the current active sessions for a user
-     *
-     * @param GetAccessTokensRequest $request
-     * @return JsonResponse
-     */
-    public function getSessions(GetAccessTokensRequest $request): JsonResponse
-    {
-        $data = $request->validated();
-
-        // Get the authenticated user
-        $user = auth()->user();
-
-        // Get paginated sessions except current session
-        $sessions = $user->sessions()->paginate($data['limit'] ?? 25);
-
-        // Get next page url minus domain
-        $nextPageURL = str_replace($request->root(), '', $sessions->nextPageUrl());
-
-        return JSONResult::success([
-            'data' => SessionResource::collection($sessions),
-            'next' => empty($nextPageURL) ? null : $nextPageURL
-        ]);
+        return (new FollowingController())
+            ->getFollowing($request, auth()->user());
     }
 }
