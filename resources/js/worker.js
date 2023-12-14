a096007ef13cc5ac6b4548682acc228724b2cf60
kiritokatklian <casillaskhoren1@gmail.com>
2023-12-15 00:11:59 +0100

    [Update] Worker JS - Updated service-worker.js path

--- resources/js/worker.js
+++ resources/js/worker.js
@@ -1,7 +1,9 @@
 if ('serviceWorker' in navigator) {
+    const mixManifest = require('/mix-manifest.json')
+
     window.addEventListener('load', () => {
         navigator.serviceWorker
-            .register('/service-worker.js')
+            .register(mixManifest['/service-worker.js'])
             .then(registration => {
                 console.log('Service worker registered:', registration)
             })
