915d2ab2dd2da79a04046cf1bce2dc5e3f89fd8f
kiritokatklian <casillaskhoren1@gmail.com>
2024-01-04 00:28:15 +0100

    [Update] Game Reviews Page - Updated star rating component usage to work with the new changes

--- app/Http/Livewire/Game/Reviews.php
+++ app/Http/Livewire/Game/Reviews.php
@@ -5,18 +5,19 @@ namespace App\Http\Livewire\Game;
 use App\Models\Game;
 use App\Models\MediaRating;
 use App\Models\MediaStat;
+use App\Traits\Livewire\WithReviewBox;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Pagination\CursorPaginator;
-use Illuminate\Routing\Redirector;
 use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class Reviews extends Component
 {
+    use WithReviewBox;
+
     /**
      * The object containing the game data.
      *
@@ -30,37 +31,6 @@ class Reviews extends Component
      * @var bool $readyToLoad
      */
     public bool $readyToLoad = false;
-
-    /**
-     * Whether to show the review box to the user.
-     *
-     * @var bool $showReviewBox
-     */
-    public bool $showReviewBox = false;
-
-    /**
-     * Whether to show the popup to the user.
-     *
-     * @var bool $showPopup
-     */
-    public bool $showPopup = false;
-
-    /**
-     * The written review text.
-     *
-     * @var string|null $reviewText
-     */
-    public ?string $reviewText;
-
-    /**
-     * The data used to populate the popup.
-     *
-     * @var array|string[]
-     */
-    public array $popupData = [
-        'title' => '',
-        'message' => '',
-    ];
 
     /**
      * Prepare the component.
@@ -82,23 +52,6 @@ class Reviews extends Component
     public function loadPage(): void
     {
         $this->readyToLoad = true;
-    }
-
-    /**
-     * Shows the review text box to the user.
-     *
-     * @return Application|RedirectResponse|Redirector|void
-     */
-    public function showReviewBox()
-    {
-        // Require user to authenticate if necessary.
-        if (!auth()->check()) {
-            return redirect(route('sign-in'));
-        }
-
-        $this->reviewText = $this->userRating->description;
-        $this->showReviewBox = true;
-        $this->showPopup = true;
     }
 
     /**
@@ -138,20 +91,6 @@ class Reviews extends Component
     {
         return $this->game->mediaRatings()
             ->firstWhere('user_id', auth()->user()?->id);
-    }
-
-    /**
-     * Submits the written review.
-     *
-     * @return void
-     */
-    public function submitReview(): void
-    {
-        $this->userRating->update([
-            'description' => strip_tags($this->reviewText)
-        ]);
-        $this->showReviewBox = false;
-        $this->showPopup = false;
     }
 
     /**
