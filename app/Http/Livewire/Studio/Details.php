6f7dbdef420bad1476f11d8fd9dcf81ee82a9869
kiritokatklian <casillaskhoren1@gmail.com>
2024-01-03 18:21:45 +0100

    [Update] Studio Star Rating - Updated star rating component usage to work with the new changes

--- app/Http/Livewire/Studio/Details.php
+++ app/Http/Livewire/Studio/Details.php
@@ -5,15 +5,17 @@ namespace App\Http\Livewire\Studio;
 use App\Events\StudioViewed;
 use App\Models\MediaRating;
 use App\Models\Studio;
+use App\Traits\Livewire\WithReviewBox;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class Details extends Component
 {
+    use WithReviewBox;
+
     /**
      * The object containing the studio data.
      *
@@ -27,38 +29,6 @@ class Details extends Component
      * @var Collection|MediaRating[] $userRating
      */
     public Collection|array $userRating;
-
-    /**
-     * Whether to show the review box to the user.
-     *
-     * @var bool $showReviewBox
-     */
-    public bool $showReviewBox = false;
-
-    /**
-     * Whether to show the popup to the user.
-     *
-     * @var bool $showPopup
-     */
-    public bool $showPopup = false;
-
-    /**
-     * The written review text.
-     *
-     * @var string|null $reviewText
-     */
-    public ?string $reviewText;
-
-    /**
-     * The data used to populate the popup.
-     *
-     * @var array|string[]
-     */
-    public array $popupData = [
-        'title' => '',
-        'message' => '',
-        'type' => 'default'
-    ];
 
     /**
      * Whether the component is ready to load.
@@ -101,44 +71,6 @@ class Details extends Component
     public function loadPage(): void
     {
         $this->readyToLoad = true;
-    }
-
-    /**
-     * Shows the review text box to the user.
-     *
-     * @return RedirectResponse|void
-     */
-    public function showReviewBox()
-    {
-        // Require user to authenticate if necessary.
-        if (!auth()->check()) {
-            return to_route('sign-in');
-        }
-
-        $this->reviewText = $this->studio->mediaRatings->first()?->description;
-        $this->showReviewBox = true;
-        $this->showPopup = true;
-    }
-
-    /**
-     * Submits the written review.
-     *
-     * @return void
-     */
-    public function submitReview(): void
-    {
-        $reviewText = strip_tags($this->reviewText);
-
-        auth()->user()->mediaRatings()
-            ->updateOrCreate([
-                'model_type' => $this->studio->getMorphClass(),
-                'model_id' => $this->studio->id,
-            ], [
-                'description' => $reviewText,
-            ]);
-
-        $this->showReviewBox = false;
-        $this->showPopup = false;
     }
 
     /**
