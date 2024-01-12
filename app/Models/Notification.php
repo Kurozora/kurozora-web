805517bdaee19cdbb7208a04992a4058287d26a5
kiritokatklian <casillaskhoren1@gmail.com>
2024-01-12 23:26:16 +0100

    [Update] Notification Model - Changed subclass to DatabaseNotification - Added getDescriptionAttribute method

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use App\Notifications\LibraryImportFinished;
+use App\Notifications\NewFeedMessageReply;
+use App\Notifications\NewFeedMessageReShare;
+use App\Notifications\NewFollower;
+use App\Notifications\NewSession;
+use App\Notifications\SubscriptionStatus;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\MassPrunable;
+use Illuminate\Notifications\DatabaseNotification;
 
-class Notification extends KModel
+class Notification extends DatabaseNotification
 {
     use MassPrunable;
 
@@ -22,5 +29,81 @@ class Notification extends KModel
     {
         return static::whereNotNull('read_at')
             ->where('read_at', '<=', now()->subWeeks(2));
+    }
+
+    /**
+     * Returns the body string that describes the notification.
+     *
+     * @return string
+     */
+    public function getDescriptionAttribute(): string
+    {
+        switch ($this->type) {
+            // Session notifications
+            case NewSession::class:
+                $body = 'A new client has logged in to your account.';
+
+                if (self::hasData('ip_address')) {
+                    $body .= ' (IP: ' . self::getData('ip_address') . ')';
+                }
+
+                return $body;
+            // Follower notifications
+            case NewFollower::class:
+                $body = self::hasData('username') ? self::getData('username') : 'Someone';
+                $body .= ' has started following you.';
+                return $body;
+            // Feed notifications
+            case NewFeedMessageReply::class:
+                $body = self::hasData('username') ? self::getData('username') : 'Someone';
+                $body .= ' Replied to Your Message.';
+                return $body;
+            case NewFeedMessageReShare::class:
+                $body = self::hasData('username') ? self::getData('username') : 'Someone';
+                $body .= ' ReShared Your Message';
+                return $body;
+            // Anime import notifications
+            case LibraryImportFinished::class:
+                $serviceName = self::getData('service');
+                $body = 'Your "' . $serviceName . '" anime import request has been processed.';
+
+                if (self::hasData('successful_count')) {
+                    $body .= ' ' . self::getData('successful_count') . ' Anime successfully imported.';
+                }
+
+                if (self::hasData('failure_count')) {
+                    $body .= ' ' . self::getData('failure_count') . ' failed imports.';
+                }
+
+                return $body;
+            // Subscription notifications
+            case SubscriptionStatus::class:
+                return SubscriptionStatus::getDescription(self::getData('subscriptionStatus')) ?? '';
+        }
+
+        return 'Something went wrong... please contact an administrator.';
+    }
+
+    /**
+     * Checks whether the notification has data under the key value.
+     *
+     * @param string $key
+     * @return bool
+     */
+    private function hasData(string $key): bool
+    {
+        return isset($this->data[$key]);
+    }
+
+    /**
+     * Gets a data variable from the notification or return null when
+     * it doesn't exist.
+     *
+     * @param string $key
+     * @return mixed
+     */
+    private function getData(string $key): mixed
+    {
+        return self::hasData($key) ? $this->data[$key] : null;
     }
 }
