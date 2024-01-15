f7174f6790686e7ddfea712879e546caecb76d70
kiritokatklian <casillaskhoren1@gmail.com>
2024-01-15 22:54:12 +0100

    [Update] Notification Dropdown - Updated style for NewFollower notification type

--- app/Http/Livewire/NavNotification.php
+++ app/Http/Livewire/NavNotification.php
@@ -2,34 +2,74 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class NavNotification extends Component
 {
+    /**
+     * Whether the notification dropdown is open.
+     *
+     * @var bool $isNotificationOpen
+     */
     public bool $isNotificationOpen = false;
 
+    /**
+     * The component's listeners.
+     *
+     * @var array
+     */
     protected $listeners = [
         'is-notifications-open' => 'handleIsNotificationOpen',
     ];
 
-    public function mount(){}
+    /**
+     * Prepare the component.
+     *
+     * @return void
+     */
+    public function mount(): void
+    {}
 
+    /**
+     * Handle the `is-notifications-open` event.
+     *
+     * @param bool $isOpen
+     *
+     * @return void
+     */
     public function handleIsNotificationOpen(bool $isOpen): void
     {
         $this->isNotificationOpen = $isOpen;
     }
 
-    public function getNotificationsProperty() {
+    /**
+     * Returns the list of user's notifications.
+     *
+     * @return Collection
+     */
+    public function getNotificationsProperty(): Collection
+    {
         if (!auth()->check()) {
             return collect();
         }
 
         return auth()->user()
             ->notifications()
+            ->whereJsonContainsKey('data->userID')
+            ->with(['newFollower'])
             ->get();
     }
 
-    public function render()
+    /**
+     * Render the component.
+     *
+     * @return Application|Factory|View
+     */
+    public function render(): Application|Factory|View
     {
         return view('livewire.nav-notification');
     }
--- resources/views/livewire/nav-notification.blade.php
+++ resources/views/livewire/nav-notification.blade.php
@@ -1,3 +1,4 @@
+@php use App\Models\User; @endphp
 <div class="pt-2 pb-2">
     <p class="pr-2 mb-2 pl-2 text-lg font-semibold">{{ __('Notifications') }}</p>
 
@@ -6,24 +7,47 @@
             @foreach ($this->notifications as $key => $notification)
                 <li>
                     @switch($notification->type)
-{{--                        @case(\App\Notifications\NewSession::class)--}}
+                        @case(\App\Notifications\NewSession::class)
 {{--                            {{ $notification->data['ipAddress'] }}--}}
 {{--                        @break--}}
-{{--                        @case(\App\Notifications\SubscriptionStatus::class)--}}
+                        @case(\App\Notifications\SubscriptionStatus::class)
 {{--                            {{ $notification->data['subscriptionStatus'] }}--}}
 {{--                        @break--}}
-{{--                        @case(\App\Notifications\LocalLibraryImportFinished::class)--}}
+                        @case(\App\Notifications\LocalLibraryImportFinished::class)
 {{--                            {{ $notification->data['behavior'] }}--}}
 {{--                        @break--}}
-{{--                        @case(\App\Notifications\LibraryImportFinished::class)--}}
+                        @case(\App\Notifications\LibraryImportFinished::class)
 {{--                            {{ $notification->data['library'] }}--}}
-{{--                        @break--}}
-{{--                        @case(\App\Notifications\NewFeedMessageReply::class)--}}
+                        @case(\App\Notifications\NewFeedMessageReply::class)
 {{--                            {{ $notification->data['username'] }}--}}
-{{--                        @break--}}
-{{--                        @case(\App\Notifications\NewFollower::class)--}}
-{{--                            {{ $notification->data['username'] }}--}}
-{{--                        @break--}}
+                            <p class="pr-2 pl-2 text-sm">{{ $notification->description }}</p>
+                        @break
+                        @case(\App\Notifications\NewFollower::class)
+                            <div class="flex flex-nowrap justify-between">
+                                <div class="flex items-center">
+                                    <picture class="relative shrink-0 w-10 aspect-square rounded-full overflow-hidden">
+                                        <img
+                                            class="w-full h-full object-cover lazyload"
+                                            data-sizes="auto"
+                                            data-src="{{ $notification->getData('profileImageURL') ?? 'https://ui-avatars.com/api/?name=' . $notification->getData('username') . '&color=FFFFFF&background=AAAAAA&length=1&bold=true&size=256' }}"
+                                            alt="{{ $notification->getData('username') }} Profile Image"
+                                            title="{{ $notification->getData('username') }}"
+                                            width="24"
+                                            height="24"
+                                        >
+
+                                        <div class="absolute top-0 left-0 h-full w-full border border-solid border-black/20 rounded-full"></div>
+                                    </picture>
+
+                                    <div class="pl-2 pr-2">
+                                        <p class="leading-tight line-clamp-2">{{ $notification->getData('username') }}</p>
+                                        <p class="text-xs leading-tight opacity-75">{{ $notification->description }}</p>
+                                    </div>
+                                </div>
+
+                                <a class="absolute w-full h-full" href="{{ route('profile.details', $notification->newFollower) }}"></a>
+                            </div>
+                        @break
                         @default
                             <p class="pr-2 pl-2 text-sm">{{ $notification->description }}</p>
                     @endswitch
