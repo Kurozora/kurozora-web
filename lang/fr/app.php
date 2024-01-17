67dd51bbf7a3ff22081fc5585a212dac14d7a1ea
kiritokatklian <casillaskhoren1@gmail.com>
2024-01-18 00:47:28 +0100

    [Add] French Localization - Added initial French localization

---
+++ lang/fr/app.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'description' => 'Une communauté pour les fans d\'anime avec une vaste bibliothèque d\'anime, de manga, de musique, de jeux, de films, de spéciaux, d\'OVA et d\'ONA. Uniquement sur Kurozora, la plus grande base de données d\'anime, de manga, de jeux et de musique en ligne et gratuite au monde. Suivez, partagez et découvrez des anime avec vos amis.',
+];
---
+++ lang/fr/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Votre mot de passe a été réinitialisé!',
+    'sent' => 'Nous avons envoyé le lien de réinitialisation de votre mot de passe par e-mail!',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => 'Ce jeton de réinitialisation de mot de passe n\'est pas valide.',
+    'user' => 'Nous ne pouvons pas trouver d\'utilisateur avec cette adresse e-mail.',
+];
