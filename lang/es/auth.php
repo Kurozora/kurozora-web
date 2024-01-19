0bf381f77d8272a5663b09ceecf36bcf74263fee
moonpic <99568458+Imaguy0@users.noreply.github.com>
2024-01-19 02:51:57 +0000

    [Add] Spanish Localization

---
+++ lang/es/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña proporcionada es incorrecta.',
+    'throttle' => 'Demasiados intentos de inicio de sesión. Inténtelo de nuevo en :segundos segundos.',
+];
