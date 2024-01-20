0c1cb5774e5050d0664f2c287c674db229211470
kiritokatklian <casillaskhoren1@gmail.com>
2024-01-20 16:23:37 +0100

    [Update] Action Event Migration - Added check to only run migration if Nova/Util class exists

--- database/migrations/2020_01_27_190518_change_action_event_ids_to_strings.php
+++ database/migrations/2020_01_27_190518_change_action_event_ids_to_strings.php
@@ -4,35 +4,36 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up(): void
-    {
-        Schema::table('action_events', function (Blueprint $table) {
-            $table->string('actionable_id', 36)->change();
-            $table->string('model_id', 36)->change();
-            $table->string('target_id', 36)->change();
-            $table->string('user_id', 36)->change();
-        });
-    }
+if (class_exists('Laravel\Nova\Util')) {
+    return new class extends Migration {
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up(): void
+        {
+            Schema::table('action_events', function (Blueprint $table) {
+                $table->string('actionable_id', 36)->change();
+                $table->string('model_id', 36)->change();
+                $table->string('target_id', 36)->change();
+                $table->string('user_id', 36)->change();
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down(): void
-    {
-        Schema::table('action_events', function (Blueprint $table) {
-            $table->integer('actionable_id')->unsigned()->change();
-            $table->integer('model_id')->unsigned()->change();
-            $table->integer('target_id')->unsigned()->change();
-            $table->integer('user_id')->unsigned()->change();
-        });
-    }
-};
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down(): void
+        {
+            Schema::table('action_events', function (Blueprint $table) {
+                $table->integer('actionable_id')->unsigned()->change();
+                $table->integer('model_id')->unsigned()->change();
+                $table->integer('target_id')->unsigned()->change();
+                $table->integer('user_id')->unsigned()->change();
+            });
+        }
+    };
+}
