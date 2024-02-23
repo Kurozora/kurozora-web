869a3b946468c40def8a649caefc3d39a5872a48
kiritokatklian <casillaskhoren1@gmail.com>
2024-02-23 18:02:42 +0100

    [Update] TV Rating Settings - Updated option from "Allow All Shows" to "Allow All Titles"

--- resources/views/livewire/profile/select-preferred-tv-rating-form.blade.php
+++ resources/views/livewire/profile/select-preferred-tv-rating-form.blade.php
@@ -17,7 +17,7 @@
 
             <div class="mt-5">
                 <x-select id="tv_rating" wire:model.defer="state.tv_rating">
-                    <option value="-1">{{ __('Allow All Shows') }}</option>
+                    <option value="-1">{{ __('Allow All Titles') }}</option>
                     @foreach ($this->tvRatings as $tvRating)
                         <option value="{{ $tvRating->weight }}">{{ $tvRating->full_name }}</option>
                     @endforeach
