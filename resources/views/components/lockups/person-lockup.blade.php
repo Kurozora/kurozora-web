285e340db3c6ffda2696919e5799ca0699ea2f50
kiritokatklian <casillaskhoren1@gmail.com>
2024-03-18 01:37:25 +0100

    [Fix] Person Lockup - Fixed border roundness

--- resources/views/components/lockups/person-lockup.blade.php
+++ resources/views/components/lockups/person-lockup.blade.php
@@ -11,7 +11,7 @@
         <picture class="relative aspect-square rounded-full shadow-md overflow-hidden">
             <img class="w-full h-full object-cover lazyload" data-sizes="auto" data-src="{{ $person->getFirstMediaFullUrl(\App\Enums\MediaCollection::Profile()) ?? asset('images/static/placeholders/person_poster.webp') }}" alt="{{ $person->full_name }} Profile Image" title="{{ $person->full_name }}">
 
-            <div class="absolute top-0 left-0 h-full w-full border border-solid border-black/20 rounded-lg"></div>
+            <div class="absolute top-0 left-0 h-full w-full border border-solid border-black/20 rounded-full"></div>
         </picture>
 
         <a class="absolute w-full h-full" href="{{ route('people.details', $person) }}"></a>
