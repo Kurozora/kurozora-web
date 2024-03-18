eb2d8a548eeea0f47c88bf3d9846ebaf92473b96
kiritokatklian <casillaskhoren1@gmail.com>
2024-03-18 01:36:11 +0100

    [Update] Reviews And Ratings - Added support for character reviews - Added support for person reviews - Added support for song reviews - Added support for studio reviews - Updated layout

--- resources/views/components/lockups/media-rating-lockup.blade.php
+++ resources/views/components/lockups/media-rating-lockup.blade.php
@@ -16,18 +16,86 @@
 
                 <a class="absolute w-full h-full" href="{{ route('anime.details', $mediaRating->model) }}"></a>
 
-                <div class="flex flex-col w-full gap-2">
-                    <div class="flex flex-row justify-between">
-                        <p class="leading-tight line-clamp-2">{{ $mediaRating->model->title }}</p>
+                <div class="flex flex-col items-baseline w-full">
+                    <div class="flex justify-between gap-2 w-full">
+                        <p class="inline-flex items-center text-sm font-semibold break-all overflow-hidden">{{ $mediaRating->model->title }}</p>
 
+                        <p class="text-sm text-gray-500 whitespace-nowrap" title="{{ $mediaRating->created_at->toFormattedDateString() }}">{{ $mediaRating->created_at->toFormattedDateString() }}</p>
+                    </div>
+
+                    <div>
                         <livewire:components.star-rating :rating="$mediaRating->rating" :star-size="'sm'" :disabled="true" wire:key="{{ uniqid('rating-', true) }}" />
                     </div>
 
-                    <x-truncated-text>
-                        <x-slot:text>
-                            {!! nl2br(e($mediaRating->description)) !!}
-                        </x-slot:text>
-                    </x-truncated-text>
+                    <div class="mt-1 w-full">
+                        <x-truncated-text>
+                            <x-slot:text>
+                                {!! nl2br(e($mediaRating->description)) !!}
+                            </x-slot:text>
+                        </x-truncated-text>
+                    </div>
+                </div>
+            </div>
+            @break
+        @case(\App\Models\Character::class)
+            <div class="flex flex-nowrap gap-2">
+                <picture class="relative shrink-0 w-28 aspect-square rounded-full overflow-hidden">
+                    <img class="w-full h-full object-cover lazyload" data-sizes="auto" data-src="{{ $mediaRating->model->getFirstMediaFullUrl(\App\Enums\MediaCollection::Profile()) ?? asset('images/static/placeholders/person_poster.webp') }}" alt="{{ $mediaRating->model->name }} Profile Image" title="{{ $mediaRating->model->name }}">
+
+                    <div class="absolute top-0 left-0 h-full w-full border border-solid border-black/20 rounded-full"></div>
+                </picture>
+
+                <a class="absolute w-full h-full" href="{{ route('characters.details', $mediaRating->model) }}"></a>
+
+                <div class="flex flex-col items-baseline w-full">
+                    <div class="flex justify-between gap-2 w-full">
+                        <p class="inline-flex items-center text-sm font-semibold break-all overflow-hidden">{{ $mediaRating->model->name }}</p>
+
+                        <p class="text-sm text-gray-500 whitespace-nowrap" title="{{ $mediaRating->created_at->toFormattedDateString() }}">{{ $mediaRating->created_at->toFormattedDateString() }}</p>
+                    </div>
+
+                    <div>
+                        <livewire:components.star-rating :rating="$mediaRating->rating" :star-size="'sm'" :disabled="true" wire:key="{{ uniqid('rating-', true) }}" />
+                    </div>
+
+                    <div class="mt-1 w-full">
+                        <x-truncated-text>
+                            <x-slot:text>
+                                {!! nl2br(e($mediaRating->description)) !!}
+                            </x-slot:text>
+                        </x-truncated-text>
+                    </div>
+                </div>
+            </div>
+            @break
+        @case(\App\Models\Episode::class)
+            <div class="flex flex-nowrap gap-2">
+                <picture class="relative shrink-0 w-28 aspect-video rounded-lg overflow-hidden">
+                    <img class="w-full h-full object-cover lazyload" data-sizes="auto" data-src="{{ $mediaRating->model->getFirstMediaFullUrl(\App\Enums\MediaCollection::Banner()) ?? asset('images/static/placeholders/episode_banner.webp') }}" alt="{{ $mediaRating->model->title }} Banner" title="{{ $mediaRating->model->title }}">
+
+                    <div class="absolute top-0 left-0 h-full w-full border border-solid border-black/20 rounded-lg"></div>
+                </picture>
+
+                <a class="absolute w-full h-full" href="{{ route('episodes.details', $mediaRating->model) }}"></a>
+
+                <div class="flex flex-col items-baseline w-full">
+                    <div class="flex justify-between gap-2 w-full">
+                        <p class="inline-flex items-center text-sm font-semibold break-all overflow-hidden">{{ $mediaRating->model->title }}</p>
+
+                        <p class="text-sm text-gray-500 whitespace-nowrap" title="{{ $mediaRating->created_at->toFormattedDateString() }}">{{ $mediaRating->created_at->toFormattedDateString() }}</p>
+                    </div>
+
+                    <div>
+                        <livewire:components.star-rating :rating="$mediaRating->rating" :star-size="'sm'" :disabled="true" wire:key="{{ uniqid('rating-', true) }}" />
+                    </div>
+
+                    <div class="mt-1 w-full">
+                        <x-truncated-text>
+                            <x-slot:text>
+                                {!! nl2br(e($mediaRating->description)) !!}
+                            </x-slot:text>
+                        </x-truncated-text>
+                    </div>
                 </div>
             </div>
             @break
@@ -41,18 +109,24 @@
 
                 <a class="absolute w-full h-full" href="{{ route('games.details', $mediaRating->model) }}"></a>
 
-                <div class="flex flex-col w-full gap-2">
-                    <div class="flex flex-row justify-between">
-                        <p class="leading-tight line-clamp-2">{{ $mediaRating->model->title }}</p>
+                <div class="flex flex-col items-baseline w-full">
+                    <div class="flex justify-between gap-2 w-full">
+                        <p class="inline-flex items-center text-sm font-semibold break-all overflow-hidden">{{ $mediaRating->model->title }}</p>
 
+                        <p class="text-sm text-gray-500 whitespace-nowrap" title="{{ $mediaRating->created_at->toFormattedDateString() }}">{{ $mediaRating->created_at->toFormattedDateString() }}</p>
+                    </div>
+
+                    <div>
                         <livewire:components.star-rating :rating="$mediaRating->rating" :star-size="'sm'" :disabled="true" wire:key="{{ uniqid('rating-', true) }}" />
                     </div>
 
-                    <x-truncated-text>
-                        <x-slot:text>
-                            {!! nl2br(e($mediaRating->description)) !!}
-                        </x-slot:text>
-                    </x-truncated-text>
+                    <div class="mt-1 w-full">
+                        <x-truncated-text>
+                            <x-slot:text>
+                                {!! nl2br(e($mediaRating->description)) !!}
+                            </x-slot:text>
+                        </x-truncated-text>
+                    </div>
                 </div>
             </div>
             @break
@@ -72,22 +146,171 @@
 
                 <a class="absolute w-full h-full" href="{{ route('manga.details', $mediaRating->model) }}"></a>
 
-                <div class="flex flex-col w-full gap-2">
-                    <div class="flex flex-row justify-between">
-                            <p class="leading-tight line-clamp-2">{{ $mediaRating->model->title }}</p>
+                <div class="flex flex-col items-baseline w-full">
+                    <div class="flex justify-between gap-2 w-full">
+                        <p class="inline-flex items-center text-sm font-semibold break-all overflow-hidden">{{ $mediaRating->model->title }}</p>
 
-                            <livewire:components.star-rating :rating="$mediaRating->rating" :star-size="'sm'" :disabled="true" wire:key="{{ uniqid('rating-', true) }}" />
+                        <p class="text-sm text-gray-500 whitespace-nowrap" title="{{ $mediaRating->created_at->toFormattedDateString() }}">{{ $mediaRating->created_at->toFormattedDateString() }}</p>
                     </div>
 
-                    <x-truncated-text>
-                        <x-slot:text>
-                            {!! nl2br(e($mediaRating->description)) !!}
-                        </x-slot:text>
-                    </x-truncated-text>
+                    <div>
+                        <livewire:components.star-rating :rating="$mediaRating->rating" :star-size="'sm'" :disabled="true" wire:key="{{ uniqid('rating-', true) }}" />
+                    </div>
+
+                    <div class="mt-1 w-full">
+                        <x-truncated-text>
+                            <x-slot:text>
+                                {!! nl2br(e($mediaRating->description)) !!}
+                            </x-slot:text>
+                        </x-truncated-text>
+                    </div>
+                </div>
+            </div>
+            @break
+        @case(\App\Models\Person::class)
+            <div class="flex flex-nowrap gap-2">
+                <picture class="relative shrink-0 w-28 aspect-square rounded-full overflow-hidden">
+                    <img class="w-full h-full object-cover lazyload" data-sizes="auto" data-src="{{ $mediaRating->model->getFirstMediaFullUrl(\App\Enums\MediaCollection::Profile()) ?? asset('images/static/placeholders/person_poster.webp') }}" alt="{{ $mediaRating->model->full_name }} Profile Image" title="{{ $mediaRating->model->full_name }}">
+
+                    <div class="absolute top-0 left-0 h-full w-full border border-solid border-black/20 rounded-full"></div>
+                </picture>
+
+                <a class="absolute w-full h-full" href="{{ route('people.details', $mediaRating->model) }}"></a>
+
+                <div class="flex flex-col items-baseline w-full">
+                    <div class="flex justify-between gap-2 w-full">
+                        <p class="inline-flex items-center text-sm font-semibold break-all overflow-hidden">{{ $mediaRating->model->full_name }}</p>
+
+                        <p class="text-sm text-gray-500 whitespace-nowrap" title="{{ $mediaRating->created_at->toFormattedDateString() }}">{{ $mediaRating->created_at->toFormattedDateString() }}</p>
+                    </div>
+
+                    <div>
+                        <livewire:components.star-rating :rating="$mediaRating->rating" :star-size="'sm'" :disabled="true" wire:key="{{ uniqid('rating-', true) }}" />
+                    </div>
+
+                    <div class="mt-1 w-full">
+                        <x-truncated-text>
+                            <x-slot:text>
+                                {!! nl2br(e($mediaRating->description)) !!}
+                            </x-slot:text>
+                        </x-truncated-text>
+                    </div>
+                </div>
+            </div>
+            @break
+        @case(\App\Models\Song::class)
+            <div
+                class="flex flex-nowrap gap-2"
+                x-data="{
+                    song: null,
+                    musicManager: null,
+                    bgColor: '#A660B2',
+                    songTitle: '{{ str($mediaRating->model->title)->replace('\'', '’') }}',
+                    artistName: '{{ str($mediaRating->model->artist ?? 'Unknown')->replace('\'', '’') }}',
+                    artworkURL: '{{ $mediaRating->model->getFirstMediaFullUrl(\App\Enums\MediaCollection::Artwork()) ?? asset('images/static/placeholders/music_album.webp') }}',
+                    async fetchSongData(songID) {
+                        if (!!songID) {
+                            this.song = await musicManager.fetchSong(songID)
+                            this.musicManager = window.musicManager
+                            this.bgColor = '#' + this.song.attributes.artwork.bgColor
+                            this.songTitle = this.song.attributes.name
+                            this.artistName = this.song.attributes.artistName
+                            this.artworkURL = musicManager.getArtworkURL(this.song, 320, 320)
+                        }
+                    }
+                }"
+                x-on:musicmanagerloaded.window="await fetchSongData('{{ $mediaRating->model->am_id }}')"
+                x-init="window.musicManager !== undefined ? await fetchSongData('{{ $mediaRating->model->am_id }}') : null"
+            >
+                <x-picture class="relative shrink-0 w-28 aspect-square rounded-lg overflow-hidden">
+                    <img class="w-full h-full object-cover"
+                         width="320" height="320"
+                         src="{{ $mediaRating->model->getFirstMediaFullUrl(\App\Enums\MediaCollection::Artwork()) ?? asset('images/static/placeholders/music_album.webp') }}"
+                         x-bind:title="songTitle"
+                         x-bind:alt="songTitle + ' Artwork'"
+                         x-bind:src="artworkURL"
+                         x-bind:style="{'background-color': bgColor}"
+                    >
+
+                    <div class="absolute top-0 left-0 h-full w-full border border-solid border-black/20 rounded-lg"></div>
+
+                    <div class="absolute top-0 bottom-0 left-0 right-0">
+                        <div class="flex flex-col justify-center items-center h-full">
+                            @if(!empty($mediaRating->model->am_id))
+                                <button
+                                    class="inline-flex items-center pt-3 pr-3 pb-3 pl-3 bg-white/60 backdrop-blur border border-transparent rounded-full font-semibold text-xs text-gray-500 uppercase tracking-widest shadow-md hover:opacity-75 active:opacity-50 focus:outline-none disabled:bg-gray-100 disabled:text-gray-300 disabled:cursor-default disabled:opacity-100 transition ease-in-out duration-150"
+                                    x-on:click="await musicManager?.playSong(song)"
+                                >
+                                    <template x-if="musicManager?.isPlaying && musicManager?.currentMusicID === '{{ $mediaRating->model->am_id }}'">
+                                        @svg('pause_fill', 'fill-current', ['width' => '18'])
+                                    </template>
+
+                                    <template x-if="!(musicManager?.isPlaying && musicManager?.currentMusicID === '{{ $mediaRating->model->am_id }}')">
+                                        @svg('play_fill', 'fill-current', ['width' => '18'])
+                                    </template>
+                                </button>
+                            @endif
+                        </div>
+                    </div>
+                </x-picture>
+
+                <div class="flex flex-col items-baseline w-full">
+                    <a class="absolute w-full h-full" href="{{ route('songs.details', $mediaRating->model) }}"></a>
+
+                    <div class="flex justify-between gap-2 w-full">
+                        <p class="inline-flex items-center text-sm font-semibold break-all overflow-hidden" x-text="songTitle">{{ $mediaRating->model->title }}</p>
+
+                        <p class="text-sm text-gray-500 whitespace-nowrap" title="{{ $mediaRating->created_at->toFormattedDateString() }}">{{ $mediaRating->created_at->toFormattedDateString() }}</p>
+                    </div>
+
+                    <div>
+                        <livewire:components.star-rating :rating="$mediaRating->rating" :star-size="'sm'" :disabled="true" wire:key="{{ uniqid('rating-', true) }}" />
+                    </div>
+
+                    <div class="mt-1 w-full">
+                        <x-truncated-text>
+                            <x-slot:text>
+                                {!! nl2br(e($mediaRating->description)) !!}
+                            </x-slot:text>
+                        </x-truncated-text>
+                    </div>
+                </div>
+            </div>
+            @break
+        @case(\App\Models\Studio::class)
+            <div class="flex flex-nowrap gap-2">
+                <picture class="relative shrink-0 w-28 rounded-full overflow-hidden">
+                    <img class="w-full h-full object-cover lazyload" data-sizes="auto" data-src="{{ $mediaRating->model->getFirstMediaFullUrl(\App\Enums\MediaCollection::Profile()) ?? asset('images/static/placeholders/studio_profile.webp') }}" alt="{{ $mediaRating->model->name }} Logo" title="{{ $mediaRating->model->name }}">
+
+                    <div class="absolute top-0 left-0 h-full w-full border-2 border-solid border-black/20 rounded-full"></div>
+                </picture>
+
+                <a class="absolute w-full h-full" href="{{ route('episodes.details', $mediaRating->model) }}"></a>
+
+                <div class="flex flex-col items-baseline w-full">
+                    <div class="flex justify-between gap-2 w-full">
+                        <p class="inline-flex items-center text-sm font-semibold break-all overflow-hidden">{{ $mediaRating->model->name }}</p>
+
+                        <p class="text-sm text-gray-500 whitespace-nowrap" title="{{ $mediaRating->created_at->toFormattedDateString() }}">{{ $mediaRating->created_at->toFormattedDateString() }}</p>
+                    </div>
+
+                    <div>
+                        <livewire:components.star-rating :rating="$mediaRating->rating" :star-size="'sm'" :disabled="true" wire:key="{{ uniqid('rating-', true) }}" />
+                    </div>
+
+                    <div class="mt-1 w-full">
+                        <x-truncated-text>
+                            <x-slot:text>
+                                {!! nl2br(e($mediaRating->description)) !!}
+                            </x-slot:text>
+                        </x-truncated-text>
+                    </div>
                 </div>
             </div>
         @break
         @default
-            not supported yet
+            @if (app()->isLocal())
+                {{ 'Unhandled type: ' . $mediaRating->model_type }}
+            @endif
     @endswitch
 </div>
--- resources/views/components/rows/media-rating-lockup.blade.php
+++ resources/views/components/rows/media-rating-lockup.blade.php
@@ -6,7 +6,7 @@
 
 <div {{ $attributes->merge(['class' => 'flex gap-4 justify-between ' . $class]) }}>
     @foreach($mediaRatings as $mediaRating)
-        <x-lockups.media-rating-lockup :media-rating="$mediaRating" :is-row="$isRow" />
+        <x-lockups.media-rating-lockup :media-rating="$mediaRating" :is-row="$isRow" wire:key="{{ uniqid($mediaRating->id, true) }}" />
     @endforeach
 
     <div class="w-64 sm:w-96 flex-grow"></div>
