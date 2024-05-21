c02190e62b72de25a0f057973fb0ee9d83658c66
kiritokatklian <casillaskhoren1@gmail.com>
2024-05-22 00:01:13 +0200

    [Update] Migrations - Updated migrations for Laravel 11

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create(PasswordReset::TABLE_NAME, function (Blueprint $table) {
-            $table->string('email')->index();
+            $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
