9c4852335249ad0399c6a217def6c8568ec2100b
kiritokatklian <casillaskhoren1@gmail.com>
2024-05-22 00:06:52 +0200

    [Update] Middlewares - Removed Middleware/Kernel - Removed unnecessary middlewares - Renamed VerifyCsrfToken to ValidateCsrfToken

--- app/Http/Middleware/ValidateCsrfToken.php
+++ app/Http/Middleware/ValidateCsrfToken.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken as Middleware;
 
-class VerifyCsrfToken extends Middleware
+class ValidateCsrfToken extends Middleware
 {
     /**
      * The URIs that should be excluded from CSRF verification.
