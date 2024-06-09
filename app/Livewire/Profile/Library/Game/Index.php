7d5d22f03c54aaa6fefed46ff197731f6d2f7332
kiritokatklian <casillaskhoren1@gmail.com>
2024-06-09 13:26:42 +0200

    [Update] Game Library - Updated Profile/Library/Game/Index to include search and tab logic - Removed Library/Games/Tab component which caused unnecessary requests

--- app/Livewire/Profile/Library/Game/Index.php
+++ app/Livewire/Profile/Library/Game/Index.php
@@ -3,14 +3,24 @@
 namespace App\Livewire\Profile\Library\Game;
 
 use App\Enums\UserLibraryStatus;
+use App\Models\Game;
 use App\Models\User;
+use App\Models\UserLibrary;
+use App\Traits\Livewire\WithGameSearch;
+use BenSampo\Enum\Exceptions\InvalidEnumKeyException;
+use BenSampo\Enum\Exceptions\InvalidEnumMemberException;
+use Carbon\Carbon;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Livewire\Component;
 
 class Index extends Component
 {
+    use WithGameSearch;
+
     /**
      * The object containing the user data.
      *
@@ -35,6 +45,13 @@ class Index extends Component
     ];
 
     /**
+     * Whether the component is ready to load.
+     *
+     * @var bool $readyToLoad
+     */
+    public bool $readyToLoad = false;
+
+    /**
      * Prepare the component.
      *
      * @param User $user
@@ -50,6 +67,136 @@ class Index extends Component
         if (!UserLibraryStatus::hasKey($status)) {
             $this->status = 'playing';
         }
+    }
+
+    /**
+     * Sets the property to load the page.
+     *
+     * @return void
+     */
+    public function loadPage(): void
+    {
+        $this->readyToLoad = true;
+    }
+
+    /**
+     * Redirect the user to a random model.
+     *
+     * @return void
+     * @throws InvalidEnumKeyException
+     * @throws InvalidEnumMemberException
+     */
+    public function randomGame(): void
+    {
+        // Get library status
+        $status = str_replace('-', '', $this->status);
+        $userLibraryStatus = UserLibraryStatus::fromKey($status);
+
+        $game = $this->user
+            ->whereTracked(Game::class)
+            ->wherePivot('status', $userLibraryStatus->value)
+            ->inRandomOrder()
+            ->first();
+        $this->redirectRoute('game.details', $game);
+    }
+
+    /**
+     * The computed search results property.
+     *
+     * @return ?LengthAwarePaginator
+     * @throws InvalidEnumKeyException
+     * @throws InvalidEnumMemberException
+     */
+    public function getSearchResultsProperty(): ?LengthAwarePaginator
+    {
+        if (!$this->readyToLoad) {
+            return null;
+        }
+
+        // Order
+        $orders = [];
+        foreach ($this->order as $attribute => $order) {
+            $attribute = str_replace(':', '.', $attribute);
+            $selected = $order['selected'];
+
+            if (!empty($selected)) {
+                $orders[] = [
+                    'column' => $attribute,
+                    'direction' => $selected,
+                ];
+            }
+        }
+
+        // Filter
+        $wheres = [];
+        $whereIns = [];
+        foreach ($this->filter as $attribute => $filter) {
+            $attribute = str_replace(':', '.', $attribute);
+            $selected = $filter['selected'];
+            $type = $filter['type'];
+
+            if ((is_numeric($selected) && $selected >= 0) || !empty($selected)) {
+                if ($type === 'multiselect') {
+                    $whereIns[$attribute] = $selected;
+                } else {
+                    $wheres[$attribute] = match ($type) {
+                        'date' => Carbon::createFromFormat('Y-m-d', $selected)
+                            ?->setTime(0, 0)
+                            ->timestamp,
+                        'time' => $selected . ':00',
+                        'double' => number_format($selected, 2, '.', ''),
+                        default => $selected,
+                    };
+                }
+            }
+        }
+
+        // Get library status
+        $upperCaseStatus = implode('-', array_map('ucfirst', explode('-', $this->status)));
+        $status = str_replace('-', '', $upperCaseStatus);
+        $userLibraryStatus = UserLibraryStatus::fromKey($status);
+
+        // If no search was performed, return all game
+        if (empty($this->search) && empty($wheres) && empty($whereIns)) {
+            $games = $this->user
+                ->whereTracked(Game::class)
+                ->with(['genres', 'media', 'mediaStat', 'themes', 'translations', 'tv_rating'])
+                ->when(auth()->user(), function ($query, $user) {
+                    $query->with(['library' => function ($query) use ($user) {
+                        $query->where('user_id', '=', $user->id);
+                    }]);
+                })
+                ->wherePivot('status', $userLibraryStatus->value);
+            return $games->paginate($this->perPage);
+        }
+
+        // Search
+        $gameIDs = collect(UserLibrary::search($this->search)
+            ->where('user_id', $this->user->id)
+            ->where('trackable_type', addslashes(Game::class))
+            ->where('status', $userLibraryStatus->value)
+            ->simplePaginateRaw(perPage: 2000, page: 1)
+            ->items()['hits'] ?? [])
+            ->pluck('trackable_id')
+            ->toArray();
+        $whereIns['id'] = $gameIDs;
+
+        $games = Game::search($this->search);
+        $games->orders = $orders;
+        $games->wheres = $wheres;
+        $games->whereIns = $whereIns;
+
+        $games->query(function (Builder $query) {
+            $query->with(['genres', 'media', 'mediaStat', 'themes', 'translations', 'tv_rating'])
+                ->when(auth()->user(), function ($query, $user) {
+                    $query->with(['library' => function ($query) use ($user) {
+                        $query->where('user_id', '=', $user->id);
+                    }]);
+                });
+        });
+
+        // Paginate
+        return $games->paginate($this->perPage);
     }
 
     /**
--- resources/views/livewire/profile/library/game/index.blade.php
+++ resources/views/livewire/profile/library/game/index.blade.php
@@ -14,12 +14,7 @@
         <meta property="og:type" content="website" />
     </x-slot:meta>
 
-    <div
-        class="max-w-7xl mx-auto pl-4 pr-4 py-6 sm:px-6"
-        x-data="{
-            selectedStatus: @entangle('status').live
-        }"
-    >
+    <div class="max-w-7xl mx-auto pl-4 pr-4 py-6 sm:px-6">
         <div class="flex flex-nowrap gap-4 justify-between text-center whitespace-nowrap overflow-x-scroll no-scrollbar">
             @foreach(\App\Enums\UserLibraryStatus::asGameSelectArray() as $key => $value)
                 <button
@@ -31,10 +26,54 @@
             @endforeach
         </div>
 
-        <section class="mt-8">
-            @foreach(\App\Enums\UserLibraryStatus::asGameSelectArray() as $key => $value)
-                <livewire:library.games.tab id="{{ $value }}" :user="$user" :status="$value" wire:key="{{ uniqid($value, true) }}" />
-            @endforeach
+        <section class="mt-8" wire:init="loadPage">
+            <section>
+                <x-search-bar>
+                    <x-slot:rightBarButtonItems>
+                        <x-square-button wire:click="randomGame">
+                            @svg('dice', 'fill-current', ['aria-labelledby' => 'random game from ' . strtolower($status) . ' library', 'width' => '28'])
+                        </x-square-button>
+                    </x-slot:rightBarButtonItems>
+                </x-search-bar>
+            </section>
+
+            @if(!empty($this->searchResults))
+                @if(!empty($this->searchResults->total()))
+                    <section class="mt-4" wire:key="not-empty-{{ strtolower($status) }}">
+                        <x-rows.small-lockup :games="$this->searchResults" :is-row="false" />
+
+                        <div class="mt-4">
+                            {{ $this->searchResults->links() }}
+                        </div>
+                    </section>
+                @else
+                    <section class="flex flex-col items-center mt-4 text-center" wire:key="empty-{{ strtolower($status) }}">
+                        <x-picture>
+                            <img class="w-full max-w-sm" src="{{ asset('images/static/placeholders/empty_game_library.webp') }}" alt="Empty Library" title="Empty Library">
+                        </x-picture>
+
+                        <p class="font-bold">{{ __('No Shows') }}</p>
+
+                        <p class="text-sm text-gray-500">{{ __('Add a show to your :x list and it will show up here.', ['x' => strtolower($status)]) }}</p>
+                    </section>
+                @endif
+            @elseif (!$readyToLoad)
+                <section class="mt-4 pt-5 pb-8 border-t-2">
+                    <div class="flex gap-4 justify-between flex-wrap">
+                        <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                        <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                        <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                        <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                        <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                        <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                        <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                        <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                        <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                        <div class="w-64 md:w-80 flex-grow"></div>
+                        <div class="w-64 md:w-80 flex-grow"></div>
+                    </div>
+                </section>
+            @endif
         </section>
     </div>
 </main>
