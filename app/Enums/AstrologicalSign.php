f545b950e4f281804d4d4334c3300b34c8c0e2ae
kiritokatklian <casillaskhoren1@gmail.com>
2024-06-27 22:54:20 +0200

    [Update] Astrological Sign - Added method for determining astrological sign from date

--- app/Enums/AstrologicalSign.php
+++ app/Enums/AstrologicalSign.php
@@ -3,6 +3,7 @@
 namespace App\Enums;
 
 use BenSampo\Enum\Enum;
+use Carbon\Carbon;
 
 /**
  * @method static AstrologicalSign Aries()
@@ -66,5 +67,48 @@ final class AstrologicalSign extends Enum
         }
 
         return parent::getDescription((int) $value) . ' ' . self::$signsEmoji[(int) $value];
+    }
+
+    public static function getFromDate(Carbon $date): AstrologicalSign
+    {
+        $day = $date->day;
+        $month = $date->month;
+
+        if (($month == 1 && $day >= 20) || ($month == 2 && $day <= 18)) {
+            return self::Aquarius();
+        }
+        if (($month == 2 && $day >= 19) || ($month == 3 && $day <= 20)) {
+            return self::Pisces();
+        }
+        if (($month == 3 && $day >= 21) || ($month == 4 && $day <= 19)) {
+            return self::Aries();
+        }
+        if (($month == 4 && $day >= 20) || ($month == 5 && $day <= 20)) {
+            return self::Taurus();
+        }
+        if (($month == 5 && $day >= 21) || ($month == 6 && $day <= 20)) {
+            return self::Gemini();
+        }
+        if (($month == 6 && $day >= 21) || ($month == 7 && $day <= 22)) {
+            return self::Cancer();
+        }
+        if (($month == 7 && $day >= 23) || ($month == 8 && $day <= 22)) {
+            return self::Leo();
+        }
+        if (($month == 8 && $day >= 23) || ($month == 9 && $day <= 22)) {
+            return self::Virgo();
+        }
+        if (($month == 9 && $day >= 23) || ($month == 10 && $day <= 22)) {
+            return self::Libra();
+        }
+        if (($month == 10 && $day >= 23) || ($month == 11 && $day <= 21)) {
+            return self::Scorpio();
+        }
+        if (($month == 11 && $day >= 22) || ($month == 12 && $day <= 21)) {
+            return self::Sagittarius();
+        }
+
+//        if (($month == 12 && $day >= 22) || ($month == 1 && $day <= 19))
+            return self::Capricorn();
     }
 }
