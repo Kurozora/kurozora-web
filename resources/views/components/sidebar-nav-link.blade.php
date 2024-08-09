1b28e5aac5183fa402edebe2607ac553c123239f
kiritokatklian <casillaskhoren1@gmail.com>
2024-08-09 16:31:55 +0200

    [Update] Sidebar Nav Link - Updated sidebar-nav-link design

--- resources/views/components/sidebar-nav-link.blade.php
+++ resources/views/components/sidebar-nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
     $classes = ($active ?? false)
-                ? 'block pl-3 pr-4 pt-2 pb-2 text-base font-medium text-white bg-orange-500 focus:outline-none focus:text-orange-800 focus:bg-orange-100 rounded-lg transition duration-150 ease-in-out'
-                : 'block pl-3 pr-4 pt-2 pb-2 text-base font-medium text-black hover:text-orange-800 hover:bg-orange-50 focus:outline-none focus:text-orange-800 focus:bg-orange-50 rounded-lg transition duration-150 ease-in-out';
+                ? 'block pl-3 pr-4 pt-2 pb-2 text-base text-white bg-orange-500 focus:outline-none focus:text-orange-800 focus:bg-orange-100 rounded-lg transition duration-150 ease-in-out'
+                : 'block pl-3 pr-4 pt-2 pb-2 text-base text-black hover:text-orange-800 hover:bg-orange-50 focus:outline-none focus:text-orange-800 focus:bg-orange-50 rounded-lg transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }} {{ $disabled ? 'disabled' : '' }}>
