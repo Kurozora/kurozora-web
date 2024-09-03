8c0491ca544a7318160fd095d777e769106d7b21
kiritokatklian <casillaskhoren1@gmail.com>
2024-09-04 01:46:01 +0200

    [Update] Follow Button - Removed commented out code

--- resources/views/livewire/components/follow-button.blade.php
+++ resources/views/livewire/components/follow-button.blade.php
@@ -4,14 +4,10 @@
         wire:click="toggleFollow"
         :title="$this->isFollowed ? __('Unfollow :x', ['x' => $user->username]) : __('Follow :x', ['x' => $user->username])"
     >
-{{--        @auth--}}
-            @if ($this->isFollowed)
-                {{ __('Following') }}
-            @else
-                {{ __('Follow') }}
-            @endif
-{{--        @else--}}
-{{--            {{ __('Follow') }}--}}
-{{--        @endauth--}}
+        @if ($this->isFollowed)
+            {{ __('Following') }}
+        @else
+            {{ __('Follow') }}
+        @endif
     </x-tinted-pill-button>
 </div>
