51cc1534e29ef9207e1535193aaccef8f3fa5c4c
kiritokatklian <casillaskhoren1@gmail.com>
2024-09-19 00:13:58 +0200

    [Update] Anime Index - Added search scopes to quickly filter on anime types

--- app/Livewire/Anime/Index.php
+++ app/Livewire/Anime/Index.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Anime;
 
+use App\Models\MediaType;
 use App\Traits\Livewire\WithAnimeSearch;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -16,7 +17,22 @@ class Index extends Component
 {
     use WithAnimeSearch {
         getSearchResultsProperty as protected getParentSearchResultsProperty;
+        queryString as protected parentQueryString;
     }
+
+    /**
+     * The type query parameter.
+     *
+     * @var string $typeQuery
+     */
+    public string $typeQuery = 'all';
+
+    /**
+     * The type of the search.
+     *
+     * @var int $type
+     */
+    public int $type = 0;
 
     /**
      * Whether the component is ready to load.
@@ -26,12 +42,42 @@ class Index extends Component
     public bool $readyToLoad = false;
 
     /**
+     * The query strings of the component.
+     *
+     * @return string[]
+     */
+    protected function queryString(): array
+    {
+        $queryString = $this->parentQueryString();
+        $queryString['typeQuery'] = ['except' => 'all', 'as' => 'type'];
+        return $queryString;
+    }
+
+    /**
      * Prepare the component.
      *
      * @return void
      */
     public function mount(): void
     {
+    }
+
+    /**
+     * @param string $newValue
+     *
+     * @return void
+     */
+    public function updatedTypeQuery(string $newValue): void
+    {
+        if ($newValue === 'all') {
+            $this->type = 0;
+            return;
+        }
+
+        $this->type = $this->searchScopes
+            ->search(function ($value) use ($newValue) {
+                return str($value)->slug()->value() === $newValue;
+            });
     }
 
     /**
@@ -53,6 +99,9 @@ class Index extends Component
     public function searchIndexQuery(EloquentBuilder $query): EloquentBuilder
     {
         return $query->with(['genres', 'media', 'mediaStat', 'themes', 'translations', 'tv_rating'])
+            ->when(!empty($this->type), function (EloquentBuilder $query) {
+                $query->whereRelation('media_type', 'id', '=', $this->type);
+            })
             ->when(auth()->user(), function ($query, $user) {
                 $query->with(['library' => function ($query) use ($user) {
                     $query->where('user_id', '=', $user->id);
@@ -70,6 +119,13 @@ class Index extends Component
     {
         return $query->query(function (EloquentBuilder $query) {
             $query->with(['genres', 'media', 'mediaStat', 'themes', 'translations', 'tv_rating'])
+                ->when(!empty($this->letter), function (EloquentBuilder $query) {
+                    if ($this->letter == '.') {
+                        $query->whereRaw('original_title REGEXP \'^[^a-zA-Z]*$\'');
+                    } else {
+                        $query->whereLike('original_title', $this->letter . '%');
+                    }
+                })
                 ->when(auth()->user(), function ($query, $user) {
                     $query->with(['library' => function ($query) use ($user) {
                         $query->where('user_id', '=', $user->id);
@@ -90,6 +146,19 @@ class Index extends Component
         }
 
         return $this->getParentSearchResultsProperty();
+    }
+
+    /**
+     * The computed collection of manga types.
+     *
+     * @return Collection
+     */
+    public function getSearchScopesProperty(): Collection
+    {
+        return MediaType::where('type', '=', 'anime')
+            ->orderBy('name')
+            ->pluck('name', 'id')
+            ->prepend(__('All'), '');
     }
 
     /**
