0ebaa987c410d5df6665d56961eb235f8d3ff139
kiritokatklian <casillaskhoren1@gmail.com>
2024-09-19 00:13:36 +0200

    [Update] Manga Index - Added search scopes to quickly filter on manga types

--- app/Livewire/Manga/Index.php
+++ app/Livewire/Manga/Index.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Manga;
 
+use App\Models\MediaType;
 use App\Traits\Livewire\WithMangaSearch;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -16,7 +17,22 @@ class Index extends Component
 {
     use WithMangaSearch {
         getSearchResultsProperty as protected getParentSearchResultsProperty;
+        queryString as protected parentQueryString;
     }
+
+    /**
+     * The type query parameter.
+     *
+     * @var string $typeQuery
+     */
+    public string $typeQuery = 'all';
+
+    /**
+     * The type of the search.
+     *
+     * @var int $type
+     */
+    public int $type = 0;
 
     /**
      * Whether the component is ready to load.
@@ -24,6 +40,18 @@ class Index extends Component
      * @var bool $readyToLoad
      */
     public bool $readyToLoad = false;
+
+    /**
+     * The query strings of the component.
+     *
+     * @return string[]
+     */
+    protected function queryString(): array
+    {
+        $queryString = $this->parentQueryString();
+        $queryString['typeQuery'] = ['except' => 'all', 'as' => 'type'];
+        return $queryString;
+    }
 
     /**
      * Prepare the component.
@@ -35,14 +63,36 @@ class Index extends Component
     }
 
     /**
+     * @param string $newValue
+     *
+     * @return void
+     */
+    public function updatedTypeQuery(string $newValue): void
+    {
+        if ($newValue === 'all') {
+            $this->type = 0;
+            return;
+        }
+
+        $this->type = $this->searchScopes
+            ->search(function ($value) use ($newValue) {
+                return str($value)->slug()->value() === $newValue;
+            });
+    }
+
+    /**
      * Build a 'search index' query for the given resource.
      *
      * @param EloquentBuilder $query
+     *
      * @return EloquentBuilder
      */
     public function searchIndexQuery(EloquentBuilder $query): EloquentBuilder
     {
         return $query->with(['genres', 'media', 'mediaStat', 'themes', 'translations', 'tv_rating'])
+            ->when(!empty($this->type), function (EloquentBuilder $query) {
+                $query->whereRelation('media_type', 'id', '=', $this->type);
+            })
             ->when(auth()->user(), function ($query, $user) {
                 $query->with(['library' => function ($query) use ($user) {
                     $query->where('user_id', '=', $user->id);
@@ -54,18 +104,29 @@ class Index extends Component
      * Build a 'search' query for the given resource.
      *
      * @param ScoutBuilder $query
+     *
      * @return ScoutBuilder
      */
     public function searchQuery(ScoutBuilder $query): ScoutBuilder
     {
         return $query->query(function (EloquentBuilder $query) {
             $query->with(['genres', 'media', 'mediaStat', 'themes', 'translations', 'tv_rating'])
+                ->when(!empty($this->letter), function (EloquentBuilder $query) {
+                    if ($this->letter == '.') {
+                        $query->whereRaw('original_title REGEXP \'^[^a-zA-Z]*$\'');
+                    } else {
+                        $query->whereLike('original_title', $this->letter . '%');
+                    }
+                })
                 ->when(auth()->user(), function ($query, $user) {
                     $query->with(['library' => function ($query) use ($user) {
                         $query->where('user_id', '=', $user->id);
                     }]);
                 });
-        });
+        })
+            ->when(!empty($this->type), function (ScoutBuilder $query) {
+                $query->where('media_type_id', $this->type);
+            });
     }
 
     /**
@@ -90,6 +151,19 @@ class Index extends Component
         }
 
         return $this->getParentSearchResultsProperty();
+    }
+
+    /**
+     * The computed collection of manga types.
+     *
+     * @return Collection
+     */
+    public function getSearchScopesProperty(): Collection
+    {
+        return MediaType::where('type', '=', 'manga')
+            ->orderBy('name')
+            ->pluck('name', 'id')
+            ->prepend(__('All'), '');
     }
 
     /**
