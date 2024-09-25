bb6bdc3b5b635367a125a9aef6f9ba8f0cde2350
kiritokatklian <casillaskhoren1@gmail.com>
2024-09-25 02:31:58 +0200

    [Update] Library Favorites - Updated search to work with the new changes in the `WithSearch` trait

--- app/Livewire/Profile/Library/Game/Favorites.php
+++ app/Livewire/Profile/Library/Game/Favorites.php
@@ -2,16 +2,19 @@
 
 namespace App\Livewire\Profile\Library\Game;
 
+use App\Models\Episode;
 use App\Models\Game;
 use App\Models\User;
+use App\Models\UserLibrary;
 use App\Traits\Livewire\WithGameSearch;
 use Carbon\Carbon;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
+use Laravel\Scout\Builder as ScoutBuilder;
 use Livewire\Component;
 
 class Favorites extends Component
@@ -94,55 +97,148 @@ class Favorites extends Component
 
         // Filter
         $wheres = [];
+        $whereIns = [];
         foreach ($this->filter as $attribute => $filter) {
+            if ($attribute == 'library_status') {
+                continue;
+            }
+
             $attribute = str_replace(':', '.', $attribute);
             $selected = $filter['selected'];
             $type = $filter['type'];
 
             if ((is_numeric($selected) && $selected >= 0) || !empty($selected)) {
-                $wheres[$attribute] = match ($type) {
-                    'date' => Carbon::createFromFormat('Y-m-d', $selected)
-                        ?->setTime(0, 0)
-                        ->timestamp,
-                    'time' => $selected . ':00',
-                    default => $selected,
-                };
+                if ($type === 'multiselect') {
+                    $whereIns[$attribute] = $selected;
+                } else {
+                    $wheres[$attribute] = match ($type) {
+                        'date' => Carbon::createFromFormat('Y-m-d', $selected)
+                            ?->setTime(0, 0)
+                            ->timestamp,
+                        'time' => $selected . ':00',
+                        'double' => number_format($selected, 2, '.', ''),
+                        default => $selected,
+                    };
+                }
             }
         }
 
+        // Get library status
+        $userLibraryStatuses = $this->filter['library_status']['selected'] ?? null;
+
         // If no search was performed, return all games
-        if (empty($this->search) && empty($wheres) && empty($orders)) {
-            $games = $this->user
-                ->whereFavorited(Game::class)
+        if (empty($this->search) && (empty($wheres) && empty($whereIns)) && empty($orders)) {
+            $models = $this->user
+                ->whereFavorited(static::$searchModel)
+                ->withoutIgnoreList()
+                ->when($userLibraryStatuses, function ($query) use ($userLibraryStatuses) {
+                    $query->join(UserLibrary::TABLE_NAME, function ($query) use ($userLibraryStatuses) {
+                        $query->on(UserLibrary::TABLE_NAME.'.trackable_id', '=', static::$searchModel::TABLE_NAME . '.id')
+                            ->where(UserLibrary::TABLE_NAME.'.user_id', '=', $this->user->id)
+                            ->where(UserLibrary::TABLE_NAME.'.trackable_type', '=', static::$searchModel)
+                            ->whereIn(UserLibrary::TABLE_NAME.'.status', $userLibraryStatuses);
+                    });
+                })
                 ->with(['genres', 'media', 'mediaStat', 'themes', 'translations', 'tv_rating'])
                 ->when(auth()->user(), function ($query, $user) {
                     $query->with(['library' => function ($query) use ($user) {
                         $query->where('user_id', '=', $user->id);
                     }]);
                 });
-            return $games->paginate($this->perPage);
+
+            $models = $models
+                ->when(!empty($this->typeValue), function (EloquentBuilder $query) {
+                    $query->where($this->typeColumn(), '=', $this->typeValue);
+                })
+                ->when(!empty($this->letter), function (EloquentBuilder $query) {
+                    if (static::$searchModel === Episode::class) {
+                        $query->whereRelation('translations', function ($query) {
+                            $query->where('locale', '=', 'en');
+
+                            if ($this->letter == '.') {
+                                $query->whereRaw($this->letterIndexColumn() . ' REGEXP \'^[^a-zA-Z]*$\'');
+                            } else {
+                                $query->whereLike($this->letterIndexColumn(), $this->letter . '%');
+                            }
+                        });
+                    } else {
+                        if ($this->letter == '.') {
+                            $query->whereRaw($this->letterIndexColumn() . ' REGEXP \'^[^a-zA-Z]*$\'');
+                        } else {
+                            $query->whereLike($this->letterIndexColumn(), $this->letter . '%');
+                        }
+                    }
+                });
+
+            return $models->paginate($this->perPage);
         }
 
-        $gameIDs = $this->user
-            ->whereFavorited(Game::class)
+        $modelIDs = $this->user
+            ->whereFavorited(static::$searchModel)
+            ->withoutIgnoreList()
+            ->when($userLibraryStatuses, function ($query) use ($userLibraryStatuses) {
+                $query->join(UserLibrary::TABLE_NAME, function ($query) use ($userLibraryStatuses) {
+                    $query->on(UserLibrary::TABLE_NAME.'.trackable_id', '=', static::$searchModel::TABLE_NAME . '.id')
+                        ->where(UserLibrary::TABLE_NAME.'.user_id', '=', $this->user->id)
+                        ->where(UserLibrary::TABLE_NAME.'.trackable_type', '=', static::$searchModel)
+                        ->whereIn(UserLibrary::TABLE_NAME.'.status', $userLibraryStatuses);
+                });
+            })
             ->limit(2000)
             ->pluck('favorable_id')
             ->toArray();
-        $games = Game::search($this->search);
-        $games->whereIn('id', $gameIDs);
-        $games->wheres = $wheres;
-        $games->orders = $orders;
-        $games->query(function (Builder $query) {
-            $query->with(['genres', 'media', 'mediaStat', 'themes', 'translations', 'tv_rating'])
-                ->when(auth()->user(), function ($query, $user) {
-                    $query->with(['library' => function ($query) use ($user) {
-                        $query->where('user_id', '=', $user->id);
-                    }]);
-                });
-        });
+        $whereIns['id'] = $modelIDs;
+
+        if (!empty($this->letter)) {
+            $wheres['letter'] = $this->letter;
+        }
+
+        if (!empty($this->typeValue)) {
+            $wheres[$this->typeColumn()] = $this->typeValue;
+        }
+
+        $models = static::$searchModel::search($this->search);
+        $models->wheres = $wheres;
+        $models->whereIns = $whereIns;
+        $models->orders = $orders;
+        $models = $this->searchQuery($models);
 
         // Paginate
-        return $games->paginate($this->perPage);
+        return $models->paginate($this->perPage);
+    }
+
+    /**
+     * Build a 'search index' query for the given resource.
+     *
+     * @param EloquentBuilder $query
+     *
+     * @return EloquentBuilder
+     */
+    public function searchIndexQuery(EloquentBuilder $query): EloquentBuilder
+    {
+        return $query->with(['genres', 'media', 'mediaStat', 'themes', 'translations', 'tv_rating'])
+            ->when(auth()->user(), function ($query, $user) {
+                $query->with(['library' => function ($query) use ($user) {
+                    $query->where('user_id', '=', $user->id);
+                }]);
+            });
+    }
+
+    /**
+     * Build a 'search' query for the given resource.
+     *
+     * @param ScoutBuilder $query
+     * @return ScoutBuilder
+     */
+    public function searchQuery(ScoutBuilder $query): ScoutBuilder
+    {
+        return $query->query(function (EloquentBuilder $query) {
+            $query->with(['genres', 'media', 'mediaStat', 'themes', 'translations', 'tv_rating'])->when(auth()->user(), function ($query, $user) {
+                $query->with(['library' => function ($query) use ($user) {
+                    $query->where('user_id', '=', $user->id);
+                }]);
+            });
+        });
     }
 
     /**
