c0ee52d5d4ebc68691fb865612d46d4eddf28cbf
kiritokatklian <casillaskhoren1@gmail.com>
2025-01-03 23:56:25 +0100

    [Update] Select Preferred Language - Updated `SelectPreferredLanguageForm`'s `mount` method to accept a user model

--- app/Livewire/Profile/SelectPreferredLanguageForm.php
+++ app/Livewire/Profile/SelectPreferredLanguageForm.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Profile;
 
 use App\Contracts\Web\Profile\UpdatesUserPreferredLanguage;
 use App\Models\Language;
+use App\Models\User;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Support\Collection;
@@ -12,6 +13,13 @@ use Livewire\Component;
 
 class SelectPreferredLanguageForm extends Component
 {
+    /**
+     * The user instance.
+     *
+     * @var User
+     */
+    public User $user;
+
     /**
      * The component's state.
      *
@@ -29,12 +37,15 @@ class SelectPreferredLanguageForm extends Component
     /**
      * Prepare the component.
      *
+     * @param User $user
+     *
      * @return void
      */
-    public function mount(): void
+    public function mount(User $user): void
     {
+        $this->user = $user;
         $this->state = [
-            'language' => auth()->user()->language_id
+            'language' => $user->language_id
         ];
     }
 
@@ -47,7 +58,7 @@ class SelectPreferredLanguageForm extends Component
     {
         $this->resetErrorBag();
 
-        $updater->update(auth()->user(), $this->state);
+        $updater->update($this->user, $this->state);
 
         $this->dispatch('saved');
     }
