5a41a922f2e2d57607ebcc731083b529af3e5438
kiritokatklian <casillaskhoren1@gmail.com>
2025-01-04 01:05:20 +0100

    [Update] Settings View - Added a check around the fields that shouldn't show when viewing a child account's settings

--- resources/views/profile/settings.blade.php
+++ resources/views/profile/settings.blade.php
@@ -10,78 +10,82 @@
     </x-slot:header>
 
     <div class="max-w-7xl mx-auto pl-4 pr-4 py-6 space-y-10 sm:px-6">
-        <livewire:profile.update-account-information-form :user="$user" />
+        @if ($user === auth()->user())
+            <livewire:profile.update-account-information-form />
 
-        <x-hr class="hidden sm:block" />
+            <x-hr class="hidden sm:block" />
+
+            <div class="sm:mt-0">
+                <livewire:profile.update-profile-information-form />
+            </div>
+
+            <x-hr class="hidden sm:block" />
+        @endif
 
         <div class="sm:mt-0">
-            <livewire:profile.update-profile-information-form :user="$user" />
+            <livewire:profile.select-preferred-language-form :user="$user" />
         </div>
 
         <x-hr class="hidden sm:block" />
 
         <div class="sm:mt-0">
-            <livewire:profile.select-preferred-language-form  />
+            <livewire:profile.select-preferred-timezone-form :user="$user" />
         </div>
 
         <x-hr class="hidden sm:block" />
 
         <div class="sm:mt-0">
-            <livewire:profile.select-preferred-timezone-form  />
+            <livewire:profile.select-preferred-tv-rating-form :user="$user" />
         </div>
 
-        <x-hr class="hidden sm:block" />
+        @if ($user === auth()->user())
+            <x-hr class="hidden sm:block" />
 
-        <div class="sm:mt-0">
-            <livewire:profile.select-preferred-tv-rating-form />
-        </div>
+            <div class="sm:mt-0">
+                <livewire:profile.import-library-form />
+            </div>
 
-        <x-hr class="hidden sm:block" />
+            <x-hr class="hidden sm:block" />
 
-        <div class="sm:mt-0">
-            <livewire:profile.import-library-form />
-        </div>
+            <div class="sm:mt-0">
+                <livewire:profile.subscribe-to-reminders />
+            </div>
 
-        <x-hr class="hidden sm:block" />
+            <x-hr class="hidden sm:block" />
 
-        <div class="sm:mt-0">
-            <livewire:profile.subscribe-to-reminders />
-        </div>
+            <div class="sm:mt-0">
+                <livewire:profile.update-password-form />
+            </div>
 
-        <x-hr class="hidden sm:block" />
+            <x-hr class="hidden sm:block" />
 
-        <div class="sm:mt-0">
-            <livewire:profile.update-password-form />
-        </div>
+            <div class="sm:mt-0">
+                <livewire:profile.two-factor-authentication-form />
+            </div>
 
-        <x-hr class="hidden sm:block" />
+            <x-hr class="hidden sm:block" />
 
-        <div class="sm:mt-0">
-            <livewire:profile.two-factor-authentication-form />
-        </div>
+            <div class="sm:mt-0">
+                <livewire:profile.sign-out-app-sessions-form />
+            </div>
 
-        <x-hr class="hidden sm:block" />
+            <x-hr class="hidden sm:block" />
 
-        <div class="sm:mt-0">
-            <livewire:profile.sign-out-app-sessions-form />
-        </div>
+            <div class="sm:mt-0">
+                <livewire:profile.sign-out-other-sessions-form />
+            </div>
 
-        <x-hr class="hidden sm:block" />
+            <x-hr class="hidden sm:block" />
 
-        <div class="sm:mt-0">
-            <livewire:profile.sign-out-other-sessions-form />
-        </div>
+            <div class="sm:mt-0">
+                <livewire:profile.delete-library-form />
+            </div>
 
-        <x-hr class="hidden sm:block" />
+            <x-hr class="hidden sm:block" />
 
-        <div class="sm:mt-0">
-            <livewire:profile.delete-library-form />
-        </div>
-
-        <x-hr class="hidden sm:block" />
-
-        <div class="sm:mt-0">
-            <livewire:profile.delete-user-form />
-        </div>
+            <div class="sm:mt-0">
+                <livewire:profile.delete-user-form />
+            </div>
+        @endif
     </div>
 </x-app-layout>
