efcca74d445167715051241afc9904f6dae50459
kiritokatklian <casillaskhoren1@gmail.com>
2025-01-07 22:37:10 +0100

    [Add] Nova User Block - Added `UserBlock` Nova model

---
+++ app/Nova/UserBlock.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Heading;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class UserBlock extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\UserBlock>
+     */
+    public static string $model = \App\Models\UserBlock::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * The logical group associated with the resource.
+     *
+     * @var string
+     */
+    public static $group = 'Users';
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param NovaRequest $request
+     *
+     * @return array
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [
+            Heading::make('Identification'),
+
+            ID::make()->sortable(),
+
+            Heading::make('Meta information'),
+
+            BelongsTo::make('User')
+                ->searchable()
+                ->sortable(),
+
+            BelongsTo::make('Blocked', 'blocked', User::class)
+                ->searchable()
+                ->sortable()
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param NovaRequest $request
+     *
+     * @return array
+     */
+    public function cards(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param NovaRequest $request
+     *
+     * @return array
+     */
+    public function filters(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param NovaRequest $request
+     *
+     * @return array
+     */
+    public function lenses(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param NovaRequest $request
+     *
+     * @return array
+     */
+    public function actions(NovaRequest $request): array
+    {
+        return [];
+    }
+}
