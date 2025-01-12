d1a3370d949851258f999c4266c221a82746d63d
kiritokatklian <casillaskhoren1@gmail.com>
2025-01-12 23:11:30 +0100

    [Fix] ResmushIt - Updated API endpoint

--- app/Helpers/ResmushIt.php
+++ app/Helpers/ResmushIt.php
@@ -51,7 +51,7 @@ class ResmushIt {
 
         // Send file to the API
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'http://api.resmush.it/?qlty=60');
+        curl_setopt($ch, CURLOPT_URL, 'https://api.resmush.it/ws.php?qlty=60');
         curl_setopt($ch, CURLOPT_POST,1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
