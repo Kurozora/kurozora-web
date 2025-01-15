f028d507939b2091982bdb4c5cbc323f99f91573
kiritokatklian <casillaskhoren1@gmail.com>
2025-01-16 00:55:13 +0100

    [Update] Manga View Themes - Added theming to "cast" views - Added theming to "details" views - Added theming to "index" views - Added theming to "related-games" views - Added theming to "related-mangas" views - Added theming to "related-shows" views - Added theming to "reviews" views - Added theming to "staff" views - Added theming to "studios" views

--- resources/views/livewire/manga/reviews.blade.php
+++ resources/views/livewire/manga/reviews.blade.php
@@ -39,19 +39,19 @@
             <div class="flex flex-row flex-wrap justify-between gap-4">
                 <div class="flex flex-col justify-end text-center">
                     <p class="font-bold text-6xl">{{ number_format($this->mediaStat->rating_average, 1) }}</p>
-                    <p class="font-bold text-sm text-gray-500">{{ __('out of') }} 5</p>
+                    <p class="font-bold text-sm text-secondary">{{ __('out of') }} 5</p>
                 </div>
 
                 <div class="flex flex-col justify-end items-center text-center">
                     @svg('star_fill', 'fill-current', ['width' => 32])
                     <p class="font-bold text-2xl">{{ number_format($this->mediaStat->highestRatingPercentage) }}%</p>
-                    <p class="text-sm text-gray-500">{{ $this->mediaStat->sentiment }}</p>
+                    <p class="text-sm text-secondary">{{ $this->mediaStat->sentiment }}</p>
                 </div>
 
                 <div class="flex flex-col w-full justify-end text-right sm:w-auto">
                     <x-star-rating-bar :media-stat="$this->mediaStat" />
 
-                    <p class="text-sm text-gray-500">{{ trans_choice('[0,1] Not enough ratings|[2,*] :x Ratings', $this->mediaStat->rating_count, ['x' => number_format($this->mediaStat->rating_count)]) }}</p>
+                    <p class="text-sm text-secondary">{{ trans_choice('[0,1] Not enough ratings|[2,*] :x Ratings', $this->mediaStat->rating_count, ['x' => number_format($this->mediaStat->rating_count)]) }}</p>
                 </div>
             </div>
         </section>
@@ -85,7 +85,7 @@
             <section>
                 <div class="flex gap-4 justify-between flex-wrap">
                     @foreach (range(1,25) as $range)
-                        <div class="bg-gray-200 w-64 md:w-80 flex-grow" style="height: 168px;"></div>
+                        <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow" style="height: 168px;"></div>
                     @endforeach
                     <div class="w-64 md:w-80 flex-grow"></div>
                     <div class="w-64 md:w-80 flex-grow"></div>
