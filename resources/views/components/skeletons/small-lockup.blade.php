7d5f87f17e7816fc8bca022e99e48804e028a615
kiritokatklian <casillaskhoren1@gmail.com>
2025-01-16 00:49:49 +0100

    [Update] Skeleton Theme - Updated theme of `skeletons/small-lockup` component

--- resources/views/components/skeletons/small-lockup.blade.php
+++ resources/views/components/skeletons/small-lockup.blade.php
@@ -9,10 +9,10 @@
     </div>
 
     <div class="flex gap-4 justify-between snap-mandatory snap-x overflow-x-scroll no-scrollbar">
-        <div class="bg-secondary w-64 md:w-80 flex-grow pb-2 shrink-0 rounded-lg snap-normal snap-center" style="height: 168px;"></div>
-        <div class="bg-secondary w-64 md:w-80 flex-grow pb-2 shrink-0 rounded-lg snap-normal snap-center" style="height: 168px;"></div>
-        <div class="bg-secondary w-64 md:w-80 flex-grow pb-2 shrink-0 rounded-lg snap-normal snap-center" style="height: 168px;"></div>
-        <div class="bg-secondary w-64 md:w-80 flex-grow pb-2 shrink-0 rounded-lg snap-normal snap-center" style="height: 168px;"></div>
-        <div class="bg-secondary w-64 md:w-80 flex-grow pb-2 shrink-0 rounded-lg snap-normal snap-center" style="height: 168px;"></div>
+        <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow pb-2 shrink-0 snap-normal snap-center" style="height: 168px;"></div>
+        <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow pb-2 shrink-0 snap-normal snap-center" style="height: 168px;"></div>
+        <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow pb-2 shrink-0 snap-normal snap-center" style="height: 168px;"></div>
+        <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow pb-2 shrink-0 snap-normal snap-center" style="height: 168px;"></div>
+        <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow pb-2 shrink-0 snap-normal snap-center" style="height: 168px;"></div>
     </div>
 </section>
