05250ecb0bdb20f04dc8179f29bb9aaeed740074
kiritokatklian <casillaskhoren1@gmail.com>
2025-01-17 22:28:04 +0100

    [Update] Tailwind Config - Removed dark mode support - Removed `grayBlue` color` - Simplified `orange` colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,6 @@ module.exports = {
         optimizeUniversalDefaults: true,
     },
 
-    darkMode: 'class',
-
     content: [
         './app/**/*.php',
         './public/**/*.html',
@@ -16,33 +14,11 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                grayBlue: {
-                    DEFAULT: '#353A50',
-                    '100': '#EBEBEE',
-                    '200': '#CDCED3',
-                    '300': '#AEB0B9',
-                    '400': '#878EAF',
-                    '500': '#353A50',
-                    '600': '#2A2F48',
-                    '700': '#23273A',
-                    '800': '#202330',
-                    '900': '#101118',
-                },
-                neutral: {
-                    '750': '#333333',
-                },
                 orange: {
                     DEFAULT: '#FF9300',
-                    '50': '#FFF4E5',
-                    '100': '#FFE9CC',
-                    '200': '#FFD499',
-                    '300': '#FFBE66',
                     '400': '#FFA933',
                     '500': '#FF9300',
                     '600': '#E68400',
-                    '700': '#CC7600',
-                    '800': '#B36700',
-                    '900': '#995800'
                 },
             },
 
