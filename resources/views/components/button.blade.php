993c2f6ca36c3f7cd5f336518fa9215ffbc5d322
kiritokatklian <casillaskhoren1@gmail.com>
2025-01-20 23:41:56 +0100

    [Update] Button Hover Theming - Improved theming of hover states for `button` and `tinted-pill-button` components

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,5 @@
 @props(['disabled' => false])
 
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center pl-2 pr-2 pt-1 pb-1 bg-tint btn-text-tinted border border-transparent rounded-md text-xs font-semibold uppercase tracking-widest transition ease-in-out duration-150 hover:bg-tint-800 active:bg-tint focus:outline-none active:border-tint active:ring-tint disabled:bg-gray-200 disabled:border-gray-300 disabled:text-gray-400 disabled:cursor-default sm:px-4 sm:py-2']) }} {{ $disabled ? 'disabled' : '' }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center pl-2 pr-2 pt-1 pb-1 bg-tint btn-text-tinted border border-transparent rounded-md text-xs font-semibold uppercase tracking-widest transition ease-in-out duration-150 hover:bg-tint-800 active:bg-tint active:border-tint active:ring-tint disabled:bg-gray-200 disabled:border-gray-300 disabled:text-gray-400 disabled:cursor-default sm:px-4 sm:py-2']) }} {{ $disabled ? 'disabled' : '' }}>
     {{ $slot }}
 </button>
--- resources/views/components/tinted-pill-button.blade.php
+++ resources/views/components/tinted-pill-button.blade.php
@@ -2,12 +2,12 @@
 
 @php
     $colorCSS = match ($color) {
-        'orange' => 'bg-tint btn-text-tinted active:bg-tint active:border-tint active:ring-tint',
+        'orange' => 'bg-tint btn-text-tinted hover:bg-tint-800 active:bg-tint active:border-tint active:ring-tint',
         'transparent' => '',
         default => 'bg-secondary hover:bg-tertiary active:bg-secondary active:border-tint active:ring-tint',
     };
 @endphp
 
-<button {{ $attributes->merge(['class' => 'inline-flex items-center justify-center gap-1 pl-2 pr-2 pt-1 pb-1 h-8 border border-transparent rounded-full text-xs font-semibold uppercase tracking-widest shadow-md transition ease-in-out duration-150 focus:outline-none disabled:bg-gray-200 disabled:border-gray-300 disabled:text-gray-400 disabled:cursor-default ' . $colorCSS ]) }} {{ $disabled ? 'disabled' : '' }}>
+<button {{ $attributes->merge(['class' => 'inline-flex items-center justify-center gap-1 pl-2 pr-2 pt-1 pb-1 h-8 border border-transparent rounded-full text-xs font-semibold uppercase tracking-widest shadow-md transition ease-in-out duration-150 disabled:bg-gray-200 disabled:border-gray-300 disabled:text-gray-400 disabled:cursor-default ' . $colorCSS ]) }} {{ $disabled ? 'disabled' : '' }}>
     {{ $slot }}
 </button>
