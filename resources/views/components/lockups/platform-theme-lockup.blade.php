e53a9cecaedf91d4e457d59e4b6298ee118b1e31
kiritokatklian <casillaskhoren1@gmail.com>
2025-02-03 00:00:39 +0100

    feat: update platform theme lockup UI - Updated design of `platform-theme-lockup`

--- resources/views/components/lockups/platform-theme-lockup.blade.php
+++ resources/views/components/lockups/platform-theme-lockup.blade.php
@@ -1,18 +1,60 @@
 @props(['theme'])
 
-<div class="max-w-lg sm:max-w-lg md:max-w-xs lg:max-w-sm bg-secondary rounded overflow-hidden shadow-lg">
-    <x-picture
-        style="background-color: {{ $theme->getFirstMedia(\App\Enums\MediaCollection::Screenshot)?->custom_properties['background_color'] ?? 'var(--bg-secondary-color)' }};"
-    >
-        <img class="w-full aspect-square object-cover object-bottom lazyload" data-sizes="auto" data-src="{{ $theme->getFirstMediaFullUrl(\App\Enums\MediaCollection::Screenshot()) }}" alt="{{ $theme->name }}" title="{{ $theme->name }}" style="background-color: {{ $theme->getFirstMedia(\App\Enums\MediaCollection::Screenshot)?->custom_properties['background_color'] ?? '#FFFFFF' }};">
-    </x-picture>
+<div class="relative flex-grow w-64 md:w-80">
+    <div class="flex items-end gap-2 pt-4 pr-2 pb-2 pl-2">
+        <div class="w-full">
+            <p class="leading-tight line-clamp-2">{{ $theme->name }}</p>
+            <p class="text-xs leading-tight opacity-75 line-clamp-2">{{ trans_choice('{1} :x Download|:x Downloads', $theme->download_count, ['x' => $theme->download_count]) }}</p>
+        </div>
 
-    <div class="pt-4 pr-2 pb-4 pl-2">
-        <div class="font-bold text-xl mb-2">{{ $theme->name }}</div>
-        <p class="text-secondary text-base">{{ trans_choice('{1} :x Download|:x Downloads', $theme->download_count, ['x' => $theme->download_count]) }}</p>
+        <div class="flex gap-2">
+            <x-button>{{ __('GET') }}</x-button>
+
+            @hasrole('superAdmin')
+                <x-nova-link :href="route('theme-store.edit', $theme)">
+                    @svg('pencil', 'fill-current', ['width' => '44'])
+                </x-nova-link>
+            @endhasrole
+
+            {{-- More Options --}}
+            <x-dropdown align="right" width="48">
+                <x-slot:trigger>
+                    <x-circle-button
+                        title="{{ __('More') }}"
+                    >
+                        @svg('ellipsis', 'fill-current', ['width' => '28'])
+                    </x-circle-button>
+                </x-slot:trigger>
+
+                <x-slot:content>
+                    <button
+                        class="block w-full pl-4 pr-4 pt-2 pb-2 text-primary text-xs text-center font-semibold hover:bg-tertiary focus:bg-secondary"
+                    >
+                        {{ __('Re-download') }}
+                    </button>
+
+                    <button
+                        class="block w-full pl-4 pr-4 pt-2 pb-2 text-red-500 text-xs text-center font-semibold hover:bg-tertiary focus:bg-secondary"
+                    >
+                        {{ __('Delete') }}
+                    </button>
+                </x-slot:content>
+            </x-dropdown>
+        </div>
     </div>
 
-    <div class="pt-4 pr-2 pb-2 pl-2">
-        <span class="inline-block bg-tertiary rounded-full px-3 pt-1 pb-1 text-sm font-semibold text-secondary mr-2 mb-2">{{ __('Premium') }}</span>
+    <div class="flex gap-2 justify-between">
+        @foreach($theme->media as $key => $media)
+{{--            style="background-color: {{ $theme->getFirstMedia(\App\Enums\MediaCollection::Screenshot)?->custom_properties['background_color'] ?? 'var(--bg-secondary-color)' }};"--}}
+            <picture
+                class="relative rounded-lg overflow-hidden"
+                style="background-color: {{ $media?->custom_properties['background_color'] ?? 'var(--bg-secondary-color)' }};"
+                wire:key="wire:key="{{ uniqid($media->id, true) }}"
+            >
+                <img class="w-full h-full object-cover lazyload" data-sizes="auto" data-src="{{ $media->getFullUrl() }}" alt="{{ $theme->name }} Screenshot {{ $key + 1 }}" title="{{ $theme->name }} Screenshot {{ $key + 1 }}" style="background-color: {{ $media?->custom_properties['background_color'] ?? 'var(--bg-secondary-color)' }}; aspect-ratio: 9 / 19.5;">
+
+                <div class="absolute top-0 left-0 h-full w-full border border-solid border-black/20 rounded-lg"></div>
+            </picture>
+        @endforeach
     </div>
 </div>
