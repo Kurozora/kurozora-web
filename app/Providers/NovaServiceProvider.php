430d23c091e9ea96d756063520f08d1d50794dcd
kiritokatklian <casillaskhoren1@gmail.com>
2025-02-12 22:59:09 +0100

    feat: update Nova sidebar - Added custom side bar menu - Added links to user menu

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,16 +2,76 @@
 
 namespace App\Providers;
 
-use App\Models\User;
+use App\Nova\Achievement;
+use App\Nova\Activity;
+use App\Nova\Anime;
+use App\Nova\AnimeCast;
+use App\Nova\AnimeTranslation;
+use App\Nova\AppTheme;
+use App\Nova\CastRole;
+use App\Nova\Character;
+use App\Nova\CharacterTranslation;
+use App\Nova\Comment;
+use App\Nova\Country;
 use App\Nova\Dashboards\Main;
 use App\Nova\Dashboards\UserInsights;
+use App\Nova\Episode;
+use App\Nova\EpisodeTranslation;
+use App\Nova\ExploreCategory;
+use App\Nova\ExploreCategoryItem;
+use App\Nova\Game;
+use App\Nova\GameCast;
+use App\Nova\GameTranslation;
+use App\Nova\Genre;
+use App\Nova\Language;
+use App\Nova\Manga;
+use App\Nova\MangaCast;
+use App\Nova\MangaTranslation;
+use App\Nova\Media;
+use App\Nova\MediaGenre;
+use App\Nova\MediaRelation;
+use App\Nova\MediaSong;
+use App\Nova\MediaStaff;
+use App\Nova\MediaStat;
+use App\Nova\MediaStudio;
+use App\Nova\MediaTag;
+use App\Nova\MediaTheme;
+use App\Nova\MediaType;
+use App\Nova\Notification;
 use App\Nova\Permission;
+use App\Nova\Person;
+use App\Nova\PersonalAccessToken;
+use App\Nova\Platform;
+use App\Nova\Relation;
 use App\Nova\Role;
+use App\Nova\Season;
+use App\Nova\SeasonTranslation;
+use App\Nova\Session;
+use App\Nova\SessionAttribute;
+use App\Nova\Song;
+use App\Nova\SongTranslation;
+use App\Nova\Source;
+use App\Nova\StaffRole;
+use App\Nova\Status;
+use App\Nova\Studio;
+use App\Nova\Tag;
+use App\Nova\Theme;
+use App\Nova\TvRating;
+use App\Nova\User;
+use App\Nova\UserBlock;
+use App\Nova\UserFavorite;
+use App\Nova\UserLibrary;
+use App\Nova\UserReminder;
+use App\Nova\Video;
+use App\Nova\View;
 use App\Policies\PermissionPolicy;
 use App\Policies\RolePolicy;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Nova\Actions\ActionEvent;
+use Laravel\Nova\Menu\Menu;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
 use Stepanenko3\LogsTool\LogsTool;
@@ -37,10 +97,230 @@ if (class_exists('Laravel\Nova\NovaApplicationServiceProvider')) {
 //            Nova::enableRTL(function (Request $request) {
 //                return $request->user()->wantsRTL();
 //            });
-
+            Nova::withoutNotificationCenter();
             // Set timezone to JST
             Nova::userTimezone(function (Request $request) {
                 return 'Asia/Tokyo';
+            });
+
+            // Main Menu
+            Nova::mainMenu(function (Request $request, Menu $menu) {
+                return [
+                    MenuSection::make(
+                        Nova::__('Dashboards'),
+                        collect($this->dashboards())
+                            ->map(fn($dashboard) => $dashboard->menu($request))
+                    )
+                        ->collapsable()
+                        ->icon('squares-2-x-2'),
+
+                    MenuSection::make(
+                        __('Explore'),
+                        collect([
+                            ExploreCategory::class,
+                            ExploreCategoryItem::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('magnifying-glass'),
+
+                    MenuSection::make(
+                        __('Anime'),
+                        collect([
+                            Anime::class,
+                            AnimeTranslation::class,
+                            AnimeCast::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('tv'),
+
+                    MenuSection::make(
+                        __('Manga'),
+                        collect([
+                            Manga::class,
+                            MangaTranslation::class,
+                            MangaCast::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('book-open'),
+
+                    MenuSection::make(
+                        __('Games'),
+                        collect([
+                            Game::class,
+                            GameTranslation::class,
+                            GameCast::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('puzzle-piece'),
+
+                    MenuSection::make(
+                        __('Songs'),
+                        collect([
+                            Song::class,
+                            SongTranslation::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('musical-note'),
+
+                    MenuSection::make(
+                        __('Seasons & Episodes'),
+                        collect([
+                            Season::class,
+                            SeasonTranslation::class,
+                            Episode::class,
+                            EpisodeTranslation::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('film'),
+
+                    MenuSection::make(
+                        __('Characters & People'),
+                        collect([
+                            Character::class,
+                            CharacterTranslation::class,
+                            Person::class,
+                            StaffRole::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('face-smile'),
+
+                    MenuSection::make(
+                        __('Studios'),
+                        collect([
+                            Studio::class,
+                            StaffRole::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('building-office-2'),
+
+                    MenuSection::make(
+                        __('Platforms'),
+                        collect([
+                            Platform::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('device-phone-mobile'),
+
+                    MenuSection::make(
+                        __('Media'),
+                        collect([
+                            Media::class,
+                            MediaGenre::class,
+                            MediaRelation::class,
+                            MediaSong::class,
+                            MediaStaff::class,
+                            MediaStat::class,
+                            MediaStudio::class,
+                            MediaTag::class,
+                            MediaTheme::class,
+                            MediaType::class,
+                            Video::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('squares-2x2'),
+
+                    MenuSection::make(
+                        __('Attributes'),
+                        collect([
+                            CastRole::class,
+                            Genre::class,
+                            Relation::class,
+                            Source::class,
+                            Status::class,
+                            Tag::class,
+                            Theme::class,
+                            TvRating::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('squares-plus'),
+
+                    MenuSection::make(
+                        __('Localization'),
+                        collect([
+                            Country::class,
+                            Language::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('language'),
+
+                    MenuSection::make(
+                        __('Cosmetics'),
+                        collect([
+                            Achievement::class,
+                            AppTheme::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('sparkles'),
+
+                    MenuSection::make(
+                        __('User Activity'),
+                        collect([
+                            Activity::class,
+                            View::class,
+                            Comment::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('chat-bubble-left-right'),
+
+                    MenuSection::make(
+                        __('Users & Permissions'),
+                        collect([
+                            User::class,
+                            UserBlock::class,
+                            UserFavorite::class,
+                            UserLibrary::class,
+                            UserReminder::class,
+                            Notification::class,
+                            Permission::class,
+                            Role::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('user'),
+
+                    MenuSection::make(
+                        __('Sessions'),
+                        collect([
+                            PersonalAccessToken::class,
+                            Session::class,
+                            SessionAttribute::class,
+                        ])->map(fn($resource) => MenuItem::resource($resource))
+                    )
+                        ->collapsable()
+                        ->icon('arrow-left-end-on-rectangle'),
+
+                    collect($this->tools())
+                        ->map(fn($tool) => $tool->menu($request)),
+                ];
+            });
+
+            // User Menu
+            Nova::userMenu(function (Request $request, Menu $menu) {
+                $menu->append([
+                    MenuItem::link('Profile', '/resources/' . User::uriKey() . '/' . $request->user()->getKey())
+                ]);
+
+                $menu->append([
+                    MenuItem::make(__('Settings'))
+                        ->path(route('settings'))
+                        ->external(),
+                ]);
+
+                return $menu;
             });
         }
 
@@ -53,7 +333,7 @@ if (class_exists('Laravel\Nova\NovaApplicationServiceProvider')) {
          */
         protected function gate(): void
         {
-            Gate::define('viewNova', function (User $user) {
+            Gate::define('viewNova', function (\App\Models\User $user) {
                 if (!$user->hasRole(['superAdmin', 'admin', 'mod', 'editor'])) {
                     abort(redirect('https://discord.com/channels/449250093623934977/1021013118408724480/1021150221054521344'));
                 }
