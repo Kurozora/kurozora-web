5b4593d13f619e156b3df4f84cafd2269e834651
kiritokatklian <casillaskhoren1@gmail.com>
2025-02-28 23:16:38 +0100

    feat: add api client tokens model - Added `APIClientToken` model

---
+++ app/Models/APIClientToken.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Str;
+
+class APIClientToken extends KModel
+{
+    use HasUuids,
+        SoftDeletes;
+
+    // Table name
+    const string TABLE_NAME = 'api_client_tokens';
+    protected $table = self::TABLE_NAME;
+
+    /**
+     * Bootstrap the model and its traits.
+     *
+     * @return void
+     */
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(function ($client) {
+            $client->token = Str::random(40);
+        });
+    }
+
+    /**
+     * Get the columns that should receive a unique identifier.
+     *
+     * @return array
+     */
+    public function uniqueIds(): array
+    {
+        return [
+            'key_id',
+        ];
+    }
+
+    /**
+     * The user the API token belongs to.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'uuid');
+    }
+}
