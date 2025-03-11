e6a2287240c43d041e1b16cce0afb8fadefba2a6
kiritokatklian <casillaskhoren1@gmail.com>
2025-03-11 22:56:12 +0100

    feat: update navigation sidebar - Added support for displaying signed-in user details - Added support for searching

--- app/Livewire/NavigationSidebar.php
+++ app/Livewire/NavigationSidebar.php
@@ -2,11 +2,53 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class NavigationSidebar extends Component
 {
-    public function render()
+    /**
+     * Whether search is enabled.
+     *
+     * @var bool $isSearchEnabled
+     */
+    public bool $isSearchEnabled = false;
+
+    /**
+     * The object containing the user data.
+     *
+     * @var User|null $user
+     */
+    public ?User $user;
+
+    /**
+     * Prepare the component.
+     *
+     * @return void
+     */
+    public function mount(): void
+    {
+        $this->user = auth()->user()?->load(['media']);
+    }
+
+    /**
+     * The component's listeners.
+     *
+     * @var array
+     */
+    protected $listeners = [
+        'refresh-navigation-dropdown' => '$refresh',
+    ];
+
+    /**
+     * Render the component.
+     *
+     * @return Application|Factory|View
+     */
+    public function render(): Application|Factory|View
     {
         return view('livewire.navigation-sidebar');
     }
