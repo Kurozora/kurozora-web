3163772a3ee9141ebf411ee1f010f37f501f75ce
kiritokatklian <casillaskhoren1@gmail.com>
2025-03-12 23:24:09 +0100

    feat: update skeleton component - Updated layout of `skeleton/small-lockup`

--- resources/views/components/skeletons/small-lockup.blade.php
+++ resources/views/components/skeletons/small-lockup.blade.php
@@ -1,5 +1,7 @@
-<section class="pt-5 pb-8 border-t border-primary">
-    <div class="flex gap-2 justify-between mb-5">
+<section class="pb-8">
+    <x-hr class="ml-4 mr-4 pb-5" />
+
+    <div class="flex gap-2 justify-between mb-5 pl-4 pr-4">
         <div>
             <p class="bg-secondary rounded-md" style="width: 168px; height: 28px"></p>
             <p class="bg-secondary rounded-md" style="width: 228px; height: 22px"></p>
@@ -8,7 +10,7 @@
         <div class="flex flex-wrap gap-2 justify-end"></div>
     </div>
 
-    <div class="flex gap-4 justify-between snap-mandatory snap-x overflow-x-scroll no-scrollbar">
+    <div class="flex gap-4 justify-between pl-4 pr-4 snap-mandatory snap-x overflow-x-scroll no-scrollbar">
         <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow pb-2 shrink-0 snap-normal snap-center" style="height: 168px;"></div>
         <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow pb-2 shrink-0 snap-normal snap-center" style="height: 168px;"></div>
         <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow pb-2 shrink-0 snap-normal snap-center" style="height: 168px;"></div>
