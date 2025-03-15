a6de13e1997388604410e018f135df1ee18e519d
kiritokatklian <casillaskhoren1@gmail.com>
2025-03-15 23:27:08 +0100

    feat: update studio pages - Updated layout of `studio/anime` - Updated layout of `studio/details` - Updated layout of `studio/games` - Updated layout of `studio/index` - Updated layout of `studio/manga` - Updated layout of `studio/reviews`

--- resources/views/livewire/studio/index.blade.php
+++ resources/views/livewire/studio/index.blade.php
@@ -19,10 +19,10 @@
         studios
     </x-slot:appArgument>
 
-    <div class="max-w-7xl mx-auto pl-4 pr-4 py-6 sm:px-6" wire:init="loadPage">
+    <div class="py-6" wire:init="loadPage">
         <section class="mb-4">
             <div>
-                <div class="flex gap-1">
+                <div class="flex gap-1 pl-4 pr-4">
                     <div class="flex flex-wrap items-center w-full">
                         <h1 class="text-2xl font-bold">{{ __('Studios') }}</h1>
                     </div>
@@ -44,12 +44,12 @@
         @if ($this->searchResults->count())
             <x-rows.studio-lockup :studios="$this->searchResults" :is-row="false" />
 
-            <section class="mt-4">
+            <div class="mt-4 pl-4 pr-4">
                 {{ $this->searchResults->links() }}
-            </section>
+            </div>
         @elseif (!$readyToLoad)
             <section class="mt-4">
-                <div class="flex gap-4 justify-between flex-wrap">
+                <div class="flex flex-wrap gap-4 justify-between pl-4 pr-4">
                     @foreach (range(1,25) as $range)
                         <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow" style="height: 168px;"></div>
                     @endforeach
