65222e90080b255ecba8321b0d09ba492709d681
kiritokatklian <casillaskhoren1@gmail.com>
2025-03-16 23:41:47 +0100

    feat: update character pages - Updated layout of character pages

--- resources/views/livewire/character/anime.blade.php
+++ resources/views/livewire/character/anime.blade.php
@@ -20,10 +20,10 @@
         characters/{{ $character->id }}/shows
     </x-slot:appArgument>
 
-    <div class="max-w-7xl mx-auto pl-4 pr-4 py-6 sm:px-6" wire:init="loadPage">
+    <div class="py-6" wire:init="loadPage">
         <section class="mb-4">
             <div>
-                <div class="flex gap-1">
+                <div class="flex gap-1 pl-4 pr-4">
                     <div class="flex flex-wrap items-center w-full">
                         <h1 class="text-2xl font-bold">{{ __(':x’s Anime', ['x' => $character->name]) }}</h1>
                     </div>
@@ -37,12 +37,12 @@
         @if ($readyToLoad)
             <x-rows.small-lockup :animes="$this->anime" :is-row="false" />
 
-            <section class="mt-4">
+            <div class="mt-4 pl-4 pr-4">
                 {{ $this->anime->links() }}
-            </section>
+            </div>
         @else
             <section>
-                <div class="flex gap-4 justify-between flex-wrap">
+                <div class="flex flex-wrap gap-4 justify-between pl-4 pr-4">
                     @foreach (range(1,25) as $range)
                         <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow" style="height: 168px;"></div>
                     @endforeach
