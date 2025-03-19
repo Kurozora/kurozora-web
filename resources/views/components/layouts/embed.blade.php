40b179e4c6667d0c0c48d1604a72dde8c5f1f495
kiritokatklian <casillaskhoren1@gmail.com>
2025-03-19 02:14:33 +0100

    feat: add embed page theming - Added theming to embed pages

--- resources/views/components/layouts/embed.blade.php
+++ resources/views/components/layouts/embed.blade.php
@@ -55,7 +55,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
     </head>
 
-    <body>
+    <body class="app-theme bg-primary text-primary">
         {{ $slot }}
 
         @livewireScripts
