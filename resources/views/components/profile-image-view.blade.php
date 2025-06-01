94f3608c1644f23d36c23f9a9645ffc5d7ed8225
kiritokatklian <casillaskhoren1@gmail.com>
2025-06-01 22:00:47 +0200

    fix: enhance profile image component with default image, link, and `title` attributes - Added default placeholder image for guests - Included `title` attributes for improved accessibility - Wrapped profile image in a navigable link

--- resources/views/components/profile-image-view.blade.php
+++ resources/views/components/profile-image-view.blade.php
@@ -1,12 +1,23 @@
 @props(['user'])
 
-<div {{ $attributes->merge(['class' => 'flex aspect-square']) }}>
+<a
+    {{ $attributes->merge(['class' => 'flex aspect-square']) }}
+    href="{{ $user ? route('profile.details', $user) : route('sign-in') }}"
+    wire:navigate
+>
     <picture
         class="relative bg-primary border-2 border-black/5 rounded-full overflow-hidden"
-        style="background-color: {{ $user->getFirstMedia(\App\Enums\MediaCollection::Profile)?->custom_properties['background_color'] ?? 'var(--bg-secondary-color)' }};"
+        style="background-color: {{ $user?->getFirstMedia(\App\Enums\MediaCollection::Profile)?->custom_properties['background_color'] ?? 'var(--bg-secondary-color)' }};"
     >
-        <img class="w-full h-full object-cover" src="{{ $user->getFirstMediaFullUrl(\App\Enums\MediaCollection::Profile()) }}" alt="{{ $user->username }} Profile Image" width="{{ $user->getFirstMedia(\App\Enums\MediaCollection::Profile)?->custom_properties['width'] ?? 96 }}" height="{{ $user->getFirstMedia(\App\Enums\MediaCollection::Profile)?->custom_properties['height'] ?? 96 }}">
+        <img
+            class="w-full h-full object-cover"
+            src="{{ $user?->getFirstMediaFullUrl(\App\Enums\MediaCollection::Profile()) ?? asset('images/static/placeholders/user_profile.webp') }}"
+            alt="{{ $user?->username ?? __('Guest') }} {{ __('Profile') }}"
+            title="{{ $user?->username ?? __('Guest') }}"
+            width="{{ $user?->getFirstMedia(\App\Enums\MediaCollection::Profile)?->custom_properties['width'] ?? 96 }}"
+            height="{{ $user?->getFirstMedia(\App\Enums\MediaCollection::Profile)?->custom_properties['height'] ?? 96 }}"
+        >
 
         <div class="absolute top-0 left-0 h-full w-full"></div>
     </picture>
-</div>
+</a>
