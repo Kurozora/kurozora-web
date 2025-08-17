94c748fd337d33ae10e6f8f221845c5581f8d152
kiritokatklian <casillaskhoren1@gmail.com>
2025-08-17 22:38:05 +0200

    feat: enhance parental guide UI - Add sticky header with blur effect for improved usability - Display summary section including TV rating and category evaluations - Update looping logic to utilize `ParentalGuideCategory::getInstances` for better readability - Refactor category sections to include average rating details - Introduce placeholder for missing category evaluations

--- resources/views/livewire/anime/parental-guide.blade.php
+++ resources/views/livewire/anime/parental-guide.blade.php
@@ -1,4 +1,3 @@
-@php use App\Enums\ParentalGuideCategory; @endphp
 <main>
     <x-slot:title>
         {{ __('Parents Guide') }} | {!! $anime->title !!}
@@ -22,8 +21,8 @@
         anime/{{ $anime->id }}/parentalguide
     </x-slot:appArgument>
 
-    <div class="pt-4 pb-6" wire:init="loadPage">
-        <section class="mb-4">
+    <div class="pb-6">
+        <section class="sticky top-0 mb-4 pt-4 pb-4 backdrop-blur bg-blur z-10">
             <div>
                 <div class="flex gap-1 pl-4 pr-4">
                     <div class="flex flex-wrap items-center w-full">
@@ -31,82 +30,112 @@
                     </div>
 
                     <div class="flex flex-wrap flex-1 justify-end items-center w-full">
+                        @hasrole('superAdmin')
+                            <x-button wire:click="">{{ __('Vote') }}</x-button>
+                        @endhasrole
                     </div>
                 </div>
             </div>
         </section>
 
-        @if ($readyToLoad)
-            <section class="mb-4">
-                <div class="flex flex-col gap-6 pl-4 pr-4">
-                    @foreach (ParentalGuideCategory::asSelectArray() as $value => $category)
-                        <div class="flex flex-col gap-4 pb-6">
-                            <h3 id="{{ str($category)->slug() }}" class="text-xl font-bold">
-                                <a href="#{{ str($category)->slug() }}">{{ $category }}</a>
+        <section class="mb-16">
+            <div class="flex flex-col gap-4 pb-6 pl-4 pr-4">
+                <h3 class="text-xl font-bold">{{ __('Summary') }}</h3>
+
+                <div class="w-full max-w-prose bg-secondary rounded-md pt-4 pb-4 pl-4 pr-4">
+                    <ul class="m-0 space-y-4 list-none">
+                        <li>
+                            <div class="flex gap-1 items-center">
+                                <h4 class="font-bold">{{ __('Rating') }}:</h4>
+
+                                <p class="text-secondary">{{ $anime->tv_rating->name }} ({{ $anime->tv_rating->description }})</p>
+                            </div>
+                        </li>
+
+                        @foreach (App\Enums\ParentalGuideCategory::getInstances() as $category)
+                            <li>
+                                <a href="#{{ str($category->key)->slug() }}" class="flex gap-1 items-center">
+                                    <h4 class="font-bold">{{ $category->description }}:</h4>
+                                    <p class="text-secondary">{{ $anime->parental_guide_stat->getAverageRating($category)->description }}</p>
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </section>
+
+        <section class="mb-4">
+            <div class="flex flex-col gap-6 pl-4 pr-4">
+                @foreach (App\Enums\ParentalGuideCategory::getInstances() as $category)
+                    <div class="flex flex-col gap-4 pb-6">
+                        <div class="flex flex-col">
+                            <h3 id="{{ str($category->key)->slug() }}" class="text-xl font-bold" style="scroll-margin-top: 4rem;">
+                                <a href="#{{ str($category->key)->slug() }}">{{ $category->description }}</a>
                             </h3>
 
-                            @if ($this->parentalGuideEntries->has($value))
-                                <div class="flex flex-wrap gap-4">
-                                    @foreach ($this->parentalGuideEntries->get($value) as $entry)
-                                        <div
-                                            class="relative w-full max-w-prose bg-secondary rounded-md"
-                                            x-data="{
-                                                isDisabled: @js($entry->is_spoiler),
-                                                hideBlur(el) {
-                                                    el.classList.add('hidden')
-                                                    this.isDisabled = false
-                                                    this.configureHeight()
-                                                },
-                                                configureHeight() {
-                                                    if ($refs.button.offsetHeight) {
-                                                        $refs.button.style.height = $refs.content.offsetHeight + 'px'
-                                                    } else {
-                                                        $refs.button.style.height = null
-                                                    }
-                                                },
-                                            }"
-                                            x-init="configureHeight()"
-                                            x-resize="configureHeight()"
-                                        >
-                                            <button
-                                                class="absolute w-full pl-2 pr-2 pt-2 pb-2 backdrop-blur bg-tertiary text-sm rounded-md text-center {{ $entry->is_spoiler ? '' : 'hidden' }}"
-                                                x-on:click="hideBlur($el)"
-                                                x-ref="button"
-                                            >
-                                                @if ($entry->is_spoiler)
-                                                    <p>{{ __('This reason contains spoilers — click to view') }}</p>
-                                                @endif
-                                            </button>
+                            @php
+                                $averageRating = $anime->parental_guide_stat->getAverageRating($category);
+                                [$averageRatingCount, $totalRatingCount] = $anime->parental_guide_stat->getAverageRatingCount($category);
+                            @endphp
 
-                                            <div
-                                                class="pt-4 pb-4 pl-4 pr-4"
-                                                x-bind:class="{'invisible' : isDisabled}"
-                                                x-ref="content"
-                                            >
-                                                <p style="white-space: pre-wrap; overflow-wrap: break-word;">{{ $entry->reason }}</p>
-                                            </div>
-                                        </div>
-                                    @endforeach
-                                </div>
-                            @else
-                                <div class="bg-secondary rounded-md p-4">
-                                    <x-link href="#">{{ __('Be the first to evaluate this category.') }}</x-link>
-                                </div>
+                            @if($averageRatingCount !== 0 && $totalRatingCount !== 0)
+                                <p class="text-secondary">{{ trans_choice('{0} :x of :y found this to have :z|[1,*] :x of :y found this :z', $averageRating->value, ['x' => $averageRatingCount, 'y' => $totalRatingCount, 'z' => strtolower($averageRating->description)]) }}</p>
                             @endif
                         </div>
-                    @endforeach
-                </div>
-            </section>
-        @elseif (!$readyToLoad)
-            <section>
-                <div class="flex flex-wrap gap-4 justify-between pl-4 pr-4">
-                    @foreach (range(1,25) as $range)
-                        <div class="bg-secondary w-64 rounded-md md:w-80 flex-grow" style="height: 168px;"></div>
-                    @endforeach
-                    <div class="w-64 md:w-80 flex-grow"></div>
-                    <div class="w-64 md:w-80 flex-grow"></div>
-                </div>
-            </section>
-        @endif
+
+                        @if ($this->parentalGuideEntries->has($category->value))
+                            <div class="flex flex-wrap gap-4">
+                                @foreach ($this->parentalGuideEntries->get($category->value) as $entry)
+                                    <div
+                                        class="relative w-full max-w-prose bg-secondary rounded-md"
+                                        x-data="{
+                                            isDisabled: @js($entry->is_spoiler),
+                                            hideBlur(el) {
+                                                el.classList.add('hidden')
+                                                this.isDisabled = false
+                                                this.configureHeight()
+                                            },
+                                            configureHeight() {
+                                                if ($refs.button.offsetHeight) {
+                                                    $refs.button.style.height = $refs.content.offsetHeight + 'px'
+                                                } else {
+                                                    $refs.button.style.height = null
+                                                }
+                                            },
+                                        }"
+                                        x-init="configureHeight()"
+                                        x-resize="configureHeight()"
+                                    >
+                                        <button
+                                            class="absolute w-full pl-2 pr-2 pt-2 pb-2 backdrop-blur bg-tertiary text-sm rounded-md text-center {{ $entry->is_spoiler ? '' : 'hidden' }}"
+                                            x-on:click="hideBlur($el)"
+                                            x-ref="button"
+                                        >
+                                            @if ($entry->is_spoiler)
+                                                <p>{{ __('This reason contains spoilers — click to view') }}</p>
+                                            @endif
+                                        </button>
+
+                                        <div
+                                            class="pt-4 pb-4 pl-4 pr-4"
+                                            x-bind:class="{'invisible' : isDisabled}"
+                                            x-ref="content"
+                                        >
+                                            <p style="white-space: pre-wrap; overflow-wrap: break-word;">{{ $entry->reason }}</p>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        @else
+                            <div class="bg-secondary rounded-md p-4">
+{{--                                <x-link href="#">{{ __('Be the first to evaluate this category.') }}</x-link>--}}
+                                <x-link href="#">{{ __('It looks like we don’t have an evaluation for this category yet.') }}</x-link>
+                            </div>
+                        @endif
+                    </div>
+                @endforeach
+            </div>
+        </section>
     </div>
 </main>
