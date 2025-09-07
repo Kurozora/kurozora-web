eb3df186c773fe92f36a4a0c10e3f3072f22808b
kiritokatklian <casillaskhoren1@gmail.com>
2025-09-07 22:03:40 +0200

    feat: add `tvRating` to EpisodeResourceBasic - Add `tvRating` property to `attributes`

--- app/Http/Resources/EpisodeResourceBasic.php
+++ app/Http/Resources/EpisodeResourceBasic.php
@@ -21,41 +21,43 @@ class EpisodeResourceBasic extends JsonResource
      * Transform the resource into an array.
      *
      * @param Request $request
+     *
      * @return array
      */
     public function toArray(Request $request): array
     {
         $resource = EpisodeResourceIdentity::make($this->resource)->toArray($request);
         $resource = array_merge($resource, [
-            'attributes'    => [
-                'poster'        => MediaResource::make(
+            'attributes' => [
+                'poster' => MediaResource::make(
                     $this->resource->season->media->firstWhere('collection_name', '=', MediaCollection::Poster) ??
                     $this->resource->anime->media->firstWhere('collection_name', '=', MediaCollection::Poster)
                 ),
-                'banner'        => MediaResource::make(
+                'banner' => MediaResource::make(
                     $this->resource->media->firstWhere('collection_name', '=', MediaCollection::Banner) ??
                     $this->resource->anime->media->firstWhere('collection_name', '=', MediaCollection::Banner) ??
                     $this->resource->anime->media->firstWhere('collection_name', '=', MediaCollection::Poster)
                 ),
-                'number'        => $this->resource->number,
-                'numberTotal'   => $this->resource->number_total,
-                'seasonNumber'  => $this->resource->season->number,
-                'showTitle'     => $this->resource->anime->title,
-                'title'         => $this->resource->title,
-                'synopsis'      => $this->resource->synopsis,
-                'duration'      => $this->resource->duration_string,
-                'stats'         => MediaStatsResource::make($this->resource->mediaStat),
-                'videos'        => VideoResource::collection($this->resource->videos),
-                'isFiller'      => $this->resource->is_filler,
-                'isNsfw'        => $this->resource->is_nsfw,
-                'isPremiere'    => $this->resource->is_premiere,
-                'isFinale'      => $this->resource->is_finale,
-                'isSpecial'     => $this->resource->is_special,
-                'isVerified'    => $this->resource->is_verified,
-                'viewCount'     => $this->resource->view_count,
-                'startedAt'     => $this->resource->started_at?->timestamp,
-                'firstAired'    => $this->resource->started_at?->timestamp,
-                'endedAt'       => $this->resource->ended_at?->timestamp,
+                'number' => $this->resource->number,
+                'numberTotal' => $this->resource->number_total,
+                'seasonNumber' => $this->resource->season->number,
+                'showTitle' => $this->resource->anime->title,
+                'title' => $this->resource->title,
+                'synopsis' => $this->resource->synopsis,
+                'tvRating' => $this->resource->tv_rating->only(['name', 'description']),
+                'duration' => $this->resource->duration_string,
+                'stats' => MediaStatsResource::make($this->resource->mediaStat),
+                'videos' => VideoResource::collection($this->resource->videos),
+                'isFiller' => $this->resource->is_filler,
+                'isNsfw' => $this->resource->is_nsfw,
+                'isPremiere' => $this->resource->is_premiere,
+                'isFinale' => $this->resource->is_finale,
+                'isSpecial' => $this->resource->is_special,
+                'isVerified' => $this->resource->is_verified,
+                'viewCount' => $this->resource->view_count,
+                'startedAt' => $this->resource->started_at?->timestamp,
+                'firstAired' => $this->resource->started_at?->timestamp,
+                'endedAt' => $this->resource->ended_at?->timestamp,
             ]
         ]);
 
