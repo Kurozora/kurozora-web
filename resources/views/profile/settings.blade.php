ee37a95945796d552692a73b938400a68a2ee4be
kiritokatklian <casillaskhoren1@gmail.com>
2026-01-21 00:19:03 +0100

    feat: enhance profile settings view with safe area insets - Updated classes to include `xl:safe-area-inset` where necessary to avoid sidebar overlay

--- resources/views/profile/settings.blade.php
+++ resources/views/profile/settings.blade.php
@@ -9,69 +9,71 @@
         </h2>
     </x-slot:header>
 
-    <div class="pl-4 pr-4 py-6">
-        <livewire:profile.update-account-information-form :user="$user" />
+    <div class="xl:safe-area-inset">
+        <div class="pl-4 pr-4 py-6">
+            <livewire:profile.update-account-information-form :user="$user" />
 
-        @if ($user === auth()->user())
+            @if ($user === auth()->user())
+                <div class="pt-10">
+                    <livewire:profile.update-profile-information-form />
+                </div>
+            @endif
+
+            @if ($user->parent_id === null)
+                <div class="pt-10">
+                    <livewire:profile.select-preferred-child-form :user="$user" />
+                </div>
+            @endif
+
             <div class="pt-10">
-                <livewire:profile.update-profile-information-form />
+                <livewire:profile.select-preferred-language-form :user="$user" />
             </div>
-        @endif
 
-        @if ($user->parent_id === null)
             <div class="pt-10">
-                <livewire:profile.select-preferred-child-form :user="$user" />
+                <livewire:profile.select-preferred-timezone-form :user="$user" />
             </div>
-        @endif
 
-        <div class="pt-10">
-            <livewire:profile.select-preferred-language-form :user="$user" />
+            <div class="pt-10">
+                <livewire:profile.select-preferred-tv-rating-form :user="$user" />
+            </div>
+
+            @if ($user === auth()->user())
+                <div class="pt-10">
+                    <livewire:profile.import-library-form />
+                </div>
+
+                <div class="pt-10">
+                    <livewire:profile.subscribe-to-reminders />
+                </div>
+
+                <div class="pt-10">
+                    <livewire:profile.update-password-form />
+                </div>
+
+                <div class="pt-10">
+                    <livewire:profile.two-factor-authentication-form />
+                </div>
+
+                <div class="pt-10">
+                    <livewire:profile.sign-out-app-sessions-form />
+                </div>
+
+                <div class="pt-10">
+                    <livewire:profile.sign-out-other-sessions-form />
+                </div>
+
+                <div class="pt-10">
+                    <livewire:profile.delete-library-form />
+                </div>
+
+                <div class="pt-10">
+                    <livewire:profile.delete-user-form />
+                </div>
+            @else
+                <div class="pt-10">
+                    <livewire:profile.unlink-user-form :user="$user" />
+                </div>
+            @endif
         </div>
-
-        <div class="pt-10">
-            <livewire:profile.select-preferred-timezone-form :user="$user" />
-        </div>
-
-        <div class="pt-10">
-            <livewire:profile.select-preferred-tv-rating-form :user="$user" />
-        </div>
-
-        @if ($user === auth()->user())
-            <div class="pt-10">
-                <livewire:profile.import-library-form />
-            </div>
-
-            <div class="pt-10">
-                <livewire:profile.subscribe-to-reminders />
-            </div>
-
-            <div class="pt-10">
-                <livewire:profile.update-password-form />
-            </div>
-
-            <div class="pt-10">
-                <livewire:profile.two-factor-authentication-form />
-            </div>
-
-            <div class="pt-10">
-                <livewire:profile.sign-out-app-sessions-form />
-            </div>
-
-            <div class="pt-10">
-                <livewire:profile.sign-out-other-sessions-form />
-            </div>
-
-            <div class="pt-10">
-                <livewire:profile.delete-library-form />
-            </div>
-
-            <div class="pt-10">
-                <livewire:profile.delete-user-form />
-            </div>
-        @else
-            <div class="pt-10">
-                <livewire:profile.unlink-user-form :user="$user" />
-            </div>
-        @endif
     </div>
 </x-app-layout>
