c013bc22397b706c02d83e5c5281af31707caede
selori <selmancinar2131@gmail.com>
2026-04-07 21:48:46 +0300

    feat: add Turkish localization

---
+++ lang/tr/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed'   => 'Girdiğiniz bilgiler kayıtlarımızla eşleşmiyor.',
+    'password' => 'Girdiğiniz şifre hatalı.',
+    'throttle' => 'Çok fazla giriş denemesi yapıldı. Lütfen :segundos saniye sonra tekrar deneyin.',
+];
