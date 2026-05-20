840870632bf253a39f3297132bc74e4a26a79324
kiritokatklian <casillaskhoren1@gmail.com>
2026-05-20 23:32:18 +0200

    fix: ensure proper casting Redis env variables on PHP 8.5

--- config/database.php
+++ config/database.php
@@ -157,8 +157,8 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
+            'port' => (int) env('REDIS_PORT', 6379),
+            'database' => (int) env('REDIS_DB', 0),
         ],
 
         'cache' => [
@@ -166,8 +166,8 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CACHE_DB', 1),
+            'port' => (int) env('REDIS_PORT', 6379),
+            'database' => (int) env('REDIS_CACHE_DB', 1),
         ],
 
     ],
--- config/reverb.php
+++ config/reverb.php
@@ -43,10 +43,10 @@ return [
                 'server' => [
                     'url' => env('REDIS_URL'),
                     'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'port' => env('REDIS_PORT', 6379),
+                    'port' => (int) env('REDIS_PORT', 6379),
                     'username' => env('REDIS_USERNAME'),
                     'password' => env('REDIS_PASSWORD'),
-                    'database' => env('REDIS_DB', 0),
+                    'database' => (int) env('REDIS_DB', 0),
                     'timeout' => env('REDIS_TIMEOUT', 60),
                 ],
             ],
