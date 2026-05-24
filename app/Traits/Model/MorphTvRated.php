a25f290f7f6dce3fc4198c075664bae6187e57cf
kiritokatklian <casillaskhoren1@gmail.com>
2026-05-24 11:21:40 +0200

    refactor: decouple `MorphTvRated` from `TvRated`

--- app/Traits/Model/MorphTvRated.php
+++ app/Traits/Model/MorphTvRated.php
@@ -11,20 +11,28 @@ use Illuminate\Database\Query\Builder;
  */
 trait MorphTvRated
 {
-    use TvRated;
-
     /**
      * Boot the soft deleting trait for a model.
      *
      * @return void
      */
-    public static function bootTvRated(): void
+    public static function bootMorphTvRated(): void
     {
         static::addGlobalScope(new MorphTvRatingScope);
     }
 
     /**
-     * Get the fully qualified "tv rating" column.
+     * Get the name of the "tv rating" column resolved against the morph target.
+     *
+     * @return string
+     */
+    public function getTvRatingColumn(): string
+    {
+        return defined(static::class . '::TV_RATING_ID') ? static::TV_RATING_ID : 'tv_rating_id';
+    }
+
+    /**
+     * Get the unqualified "tv rating" column so the morph scope can apply it against the related table.
      *
      * @return string
      */
