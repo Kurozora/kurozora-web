bad39d86cddb33e3db82828b09840c3d83c1743b
kiritokatklian <casillaskhoren1@gmail.com>
2026-06-29 00:35:20 +0200

    fix: handle invalid schema encoding in `schema` component - Safeguard against invalid JSON schema by encoding only valid payloads

--- resources/views/components/misc/schema.blade.php
+++ resources/views/components/misc/schema.blade.php
@@ -3,6 +3,9 @@
 @php
     // Concatenate to dodge Blade's @context directive parser.
     $schemaPayload = ['@' . 'context' => 'https://schema.org/'] + $data;
+    $encodedSchema = json_encode($schemaPayload, JSON_HEX_TAG | JSON_HEX_AMP | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_INVALID_UTF8_SUBSTITUTE);
 @endphp
 
-<script type="application/ld+json">{!! json_encode($schemaPayload, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_INVALID_UTF8_SUBSTITUTE) !!}</script>
+@if ($encodedSchema !== false)
+    <script type="application/ld+json">{!! $encodedSchema !!}</script>
+@endif
